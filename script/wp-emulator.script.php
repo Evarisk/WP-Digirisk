15c9599f2329a559f48e32f52723cbca2b594c58
Adrien Thierry <adrien.thierry@seraum.com>
2016-04-24 21:07:19 +0200

    Updating unit testing

---
+++ script/wp-emulator.script.php
@@ -0,0 +1,49 @@
+<?php 
+
+	function load_plugin_textdomain($name, $bool, $path)
+	{
+		echo "[+] Loading plugin -> " . $path . $name . PHP_EOL;
+	}
+	
+	function register_activation_hook( $file, $array )
+	{
+		$length = 0;
+		$hooked = "[";
+		foreach($array as $hook)
+		{
+			$length++;
+			$hooked .= $hook . ",";
+		}
+		if($length > 0) rtrim($hooked, ",");
+		$hooked .= "]";
+		echo "[+] Registering hook -> " . $file . " in " . $name . PHP_EOL;
+	}
+	
+	function add_filter( $name, $fn, $nbr)
+	{
+		echo "[+] Adding plugin -> " . $name . " X" . $nbr . PHP_EOL;	
+	}
+	
+	function sanitize_text_field($str)
+	{
+		echo "[+] Sanitizing -> " . $str . PHP_EOL;	
+	}
+	
+	function plugin_dir_path($path)
+	{
+		echo "[+] Plugin path -> " . $path . PHP_EOL;	
+	}
+	
+	function site_url()
+	{
+		$url = "http://testunitaire.dom/";
+		echo "[+] Requesting site_url -> " . $url . PHP_EOL;	
+		return $url;
+	}
+	
+	function plugin_basename()
+	{
+		$basename = dirname(__FILE__); 
+		echo "[+] Requesting plugin_basename -> " . $basename . PHP_EOL;	
+		return $basename;
+	}
---
+++ test/root.test.php
@@ -0,0 +1,3 @@
+<?php
+
+include("digirisk.php");
