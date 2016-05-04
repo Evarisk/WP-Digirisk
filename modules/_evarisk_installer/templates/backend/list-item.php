4d773ce112fa8eb3aac87526b8b2a13f8dce5a31
Jimmy Latour <jimmy.eoxia@gmail.com>
2016-05-04 11:47:46 +0200

    3843 - Installeur : Le générateur de mail
    Ne met plus d'antislash devant les caractères spéciaux

--- modules/_evarisk_installer/templates/backend/list-item.php
+++ modules/_evarisk_installer/templates/backend/list-item.php
@@ -2,7 +2,7 @@
 <li class="wp-digi-list-item">
 	<span class="wp-avatar" style="background: #<?php echo $user->option['user_info']['avatar_color']; ?>;" ><?php echo $user->option['user_info']['initial']; ?></span>
 	<span><strong>U<?php echo $user->id; ?></strong></span>
-	<span><?php echo $user->option['user_info']['lastname']; ?></span>
-	<span><?php echo $user->option['user_info']['firstname']; ?></span>
+	<span><?php echo stripslashes( $user->option['user_info']['lastname'] ); ?></span>
+	<span><?php echo stripslashes( $user->option['user_info']['firstname'] ); ?></span>
 	<span><?php echo $user->email; ?></span>
 </li>
