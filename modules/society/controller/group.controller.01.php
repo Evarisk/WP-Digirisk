702291516867ea36249f459f5bf4b5e7266a2bc8
Jimmy Latour <jimmy.eoxia@gmail.com>
2016-05-11 11:33:39 +0200

    Fix warning.

--- modules/society/controller/group.controller.01.php
+++ modules/society/controller/group.controller.01.php
@@ -104,37 +104,24 @@ class wpdigi_group_ctr_01 extends post_ctr_01 {
 		/**	Get existing groups for main selector display	*/
 		$group_list = $this->index( array( 'posts_per_page' => -1, 'post_parent' => 0, 'post_status' => array( 'publish', 'draft', ), ), false );
 		$default_selected_group_id = ( $default_selected_group_id == null ) && ( !empty( $group_list ) ) ? $group_list[0]->id : $default_selected_group_id;
-		// //global $default_selected_group_id;
-		// global $wpdigi_workunit_ctr;
 
-		//
-		// $workunit_id = 0;
-		// global $wpdb;
-		// $workunit_id = $wpdb->get_var( $wpdb->prepare( "SELECT P.ID FROM {$wpdb->posts} as P JOIN {$wpdb->postmeta} as PM ON P.ID=PM.post_id WHERE P.post_type=%s AND PM.meta_key=%s AND PM.meta_value=%s", array( $wpdigi_workunit_ctr->get_post_type(), '_wpdigi_unique_identifier', 'UT1' ) ) );
-		//
-		// if ( !empty( $_REQUEST['current_workunit_id'] ) ) {
-		// 	$workunit_id = $_REQUEST['current_workunit_id'];
-		// }
-		//
-		// if ( !empty( $_REQUEST['current_group_id'] ) ) {
-		// 	$default_selected_group_id = $_REQUEST['current_group_id'];
-		// }
+		if (!empty( $_REQUEST['current_workunit_id'] ) ) {
+			$workunit_id = (int) $_REQUEST['current_workunit_id'];
+		}
 
-		add_filter( 'wpdigi_default_dashboard_content', function( $default ){
-			$group_list = $this->index( array( 'posts_per_page' => -1, 'post_parent' => 0, 'post_status' => array( 'publish', 'draft', ), ), false );
-			$default_selected_group_id = ( $default_selected_group_id == null ) && ( !empty( $group_list ) ) ? $group_list[0]->id : $default_selected_group_id;
-			// global $wpdigi_workunit_ctr;
-			//
-			// global $wpdb;
-			// $workunit_id = $wpdb->get_var( $wpdb->prepare( "SELECT P.ID FROM {$wpdb->posts} as P JOIN {$wpdb->postmeta} as PM ON P.ID=PM.post_id WHERE P.post_type=%s AND PM.meta_key=%s AND PM.meta_value=%s", array( $wpdigi_workunit_ctr->get_post_type(), '_wpdigi_unique_identifier', 'UT1' ) ) );
-			//
-			// if ( !empty( $_REQUEST['current_workunit_id'] ) ) {
-			// 	$workunit_id = $_REQUEST['current_workunit_id'];
-			// }
+		add_filter( 'wpdigi_default_dashboard_content', function( $default ) {
+			if (!empty( $_REQUEST['current_workunit_id'] ) ) {
+				global $wpdigi_workunit_ctr;
+				$wpdigi_workunit_ctr->display( $_REQUEST['current_workunit_id'] );
+			}
+			else {
+				$group_list = $this->index( array( 'posts_per_page' => -1, 'post_parent' => 0, 'post_status' => array( 'publish', 'draft', ), ), false );
+				$default_selected_group_id = !empty( $group_list ) ? $group_list[0]->id : 0;
 
-			ob_start();
-			$this->display( $default_selected_group_id );
-			$default = ob_get_clean();
+				ob_start();
+				$this->display( $default_selected_group_id );
+				$default = ob_get_clean();
+			}
 
 			return $default;
 		}, 12);
--- modules/society/templates/simple/society/society-tree.php
+++ modules/society/templates/simple/society/society-tree.php
@@ -3,9 +3,8 @@
 require_once( wpdigi_utils::get_template_part( WPDIGI_STES_DIR, WPDIGI_STES_TEMPLATES_MAIN_DIR, 'group', 'list' ) );
 
 ?>
-
 <?php if ( !empty( $default_selected_group_id ) ) : ?>
-<?php echo do_shortcode( '[wpdigi-workunit-list group_id="' . $default_selected_group_id . '" workunit_id="' . $workunit_id . '" mode="' . $mode . '" ]' ); ?>
+<?php echo do_shortcode( '[wpdigi-workunit-list group_id="' . $default_selected_group_id . '" workunit_id="' . (!empty($workunit_id) ? $workunit_id : 0) . '" mode="' . $mode . '" ]' ); ?>
 <?php endif; ?>
 
 <?php apply_filters( 'wpdigi_society_tree_footer', $default_selected_group_id, $mode ); ?>
