5a0b91a432b21863ce16595fc2227e729025a9e7
Alexandre Techer <alexandre.techer@eoxia.com>
2016-06-01 11:07:54 +0200

    Fix transfer warning with logs

--- modules/document/templates/common/printed-list.php
+++ modules/document/templates/common/printed-list.php
@@ -3,11 +3,10 @@
 <ul class="wp-digi-list wp-digi-table wp-digi-list-document" >
 <?php foreach ( $list_document_id as $document_id ) : ?>
 <?php if ( has_term( 'printed', 'attachment_category', $document_id ) ) : ?>
-<?php 
+<?php
 	$document = $this->show( $document_id );
 	$document->option['document_meta'] = json_decode( $document->option['document_meta' ] );
 	$document_full_path = null;
-
 
 	if ( is_file( $this->get_document_path( 'basedir' ) . '/' . $element->type . '/' . $element->id . '/' . $document->title . '_merged.zip' )  && !empty( $document->option['document_meta']->zip ) ) {
 		$document_full_path = $this->get_document_path( 'baseurl' ) . '/' . $element->type . '/' . $element->id . '/' . $document->title . '_merged.zip';
@@ -15,7 +14,6 @@
 	else if ( is_file( $this->get_document_path( 'basedir' ) . '/' . $element->type . '/' . $element->id . '/' . $document->title . '.odt' ) ) {
 		$document_full_path = $this->get_document_path( 'baseurl' ) . '/' . $element->type . '/' . $element->id . '/' . $document->title . '.odt';
 	}
-
 
 	require( wpdigi_utils::get_template_part( WPDIGI_DOC_DIR, WPDIGI_DOC_TEMPLATES_MAIN_DIR, 'common', 'printed-list', 'item' ) );
 ?>
