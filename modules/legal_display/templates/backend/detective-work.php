2006a599621a84eed48e25bef02a100f403d840c
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-24 17:54:17 +0200

    Ajout des dernières données enregistrées dans le template detective-work.php de l'affichage légal

--- modules/legal_display/templates/backend/detective-work.php
+++ modules/legal_display/templates/backend/detective-work.php
@@ -16,37 +16,37 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
   <li>
     <label>
       <?php _e( 'Last name and first name', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[full_name]" type="text" />
+      <input name="detective_work[full_name]" type="text" value="<?php echo $data['detective_work']->option['full_name']; ?>" />
     </label>
   </li>
   <li>
     <label>
       <?php _e( 'Address', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[address][address]" type="text" />
+      <input name="detective_work[address][address]" type="text" value="<?php echo $data['detective_work']->address->option['address']; ?>" />
     </label>
   </li>
   <li>
     <label>
       <?php _e( 'Postcode', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[address][postcode]" type="text" />
+      <input name="detective_work[address][postcode]" type="text" value="<?php echo $data['detective_work']->address->option['postcode']; ?>" />
     </label>
   </li>
   <li>
     <label>
       <?php _e( 'City', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[address][town]" type="text" />
+      <input name="detective_work[address][town]" type="text" value="<?php echo $data['detective_work']->address->option['town']; ?>" />
     </label>
   </li>
   <li>
     <label>
       <?php _e( 'Phone', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[contact][phone]" type="text" />
+      <input name="detective_work[contact][phone]" type="text" value="<?php echo $data['detective_work']->option['contact']['phone'][0]; ?>" />
     </label>
   </li>
   <li>
     <label>
       <?php _e( 'Horaires', 'wpdigi-i18n' ); ?>
-      <input name="detective_work[opening_time]" type="text" />
+      <input name="detective_work[opening_time]" type="text" value="<?php echo $data['detective_work']->option['opening_hour']; ?>"/>
     </label>
   </li>
 </ul>
