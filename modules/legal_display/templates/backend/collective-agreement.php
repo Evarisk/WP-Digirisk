e60a1141a6cca8ad994095e26f72d073aa857e41
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-28 09:26:36 +0200

    Ajout du formulaire et des données par défaut

---
+++ modules/legal_display/templates/backend/collective-agreement.php
@@ -0,0 +1,29 @@
+<?php
+/**
+* Documents
+*
+* @author Jimmy Latour <jimmy.latour@gmail.com>
+* @version 0.1
+* @copyright 2015-2016 Eoxia
+* @package legal_display
+* @subpackage templates
+*/
+
+if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+
+<ul class="wp-digi-form">
+  <li><h2><?php _e( 'Collective agreement', 'wpdigi-i18n' ); ?></h2></li>
+  <li>
+    <label>
+      <?php _e( 'Title of the applicable collective agreement', 'wpdigi-i18n' ); ?>
+      <input name="collective_agreement[title_of_the_applicable_collective_agreement]" type="text" value="<?php echo $data['legal_display']->option['collective_agreement']['title_of_the_applicable_collective_agreement']; ?>" />
+    </label>
+  </li>
+  <li>
+    <label>
+      <?php _e( 'Location and access terms of the agreement', 'wpdigi-i18n' ); ?>
+      <input name="collective_agreement[location_and_access_terms_of_the_agreement]" type="text" value="<?php echo $data['legal_display']->option['collective_agreement']['location_and_access_terms_of_the_agreement']; ?>" />
+    </label>
+  </li>
+</ul>
