d573447be4605af5fd7d2f3c92355fd7af1e85bc
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-28 10:35:07 +0200

    Toutes les données sont générés dans le ODT

--- modules/legal_display/action/legal_display.action.01.php
+++ modules/legal_display/action/legal_display.action.01.php
@@ -7,6 +7,7 @@ class legal_display_action_01 {
 
   public function callback_save_legal_display( $detective_work_third, $occupational_health_service_third ) {
     global $legal_display_ctr;
+		global $wpdigi_group_ctr;
 
     // Récupère les tableaux
     $emergency_service = !empty( $_POST['emergency_service'] ) ? (array) $_POST['emergency_service'] : array();
@@ -39,23 +40,16 @@ class legal_display_action_01 {
 
     $legal_display = $legal_display_ctr->save_data( $legal_display_data );
 
-    $this->generate_sheet( $legal_display );
+		// Toutes les données de l'affichage légal
+		$all_data = $legal_display_ctr->load_data( $parent_id );
+		$element_parent = $wpdigi_group_ctr->show( $parent_id );
+
+    $this->generate_sheet( $all_data, $element_parent );
 
     wp_send_json_success();
   }
 
-  public function generate_sheet( $legal_display ) {
-    global $legal_display_ctr;
-    global $wpdigi_group_ctr;
-    global $third_class;
-    global $wpdigi_address_ctr;
-
-    $group = $wpdigi_group_ctr->show( $parent_id );
-    $response = array(
-			'status' 	=> false,
-			'message'	=> __( 'An error occured while getting element to generate sheet for.', 'wpdigi-i18n' ),
-		);
-
+  public function generate_sheet( $data, $element_parent ) {
 		/**	Début de la génération du document / Start document generation	*/
 		$document_controller = new document_controller_01();
 
@@ -65,7 +59,7 @@ class legal_display_action_01 {
 
 		/**	Récupération de la liste des modèles existants pour l'élément actuel / Get model list for current group	*/
 		if ( null === $group_model_id_to_use ) {
-			$response = $document_controller->get_model_for_element( array( 'affichage_legal', ) );
+			$response = $document_controller->get_model_for_element( array( 'affichage_legal_A4', ) );
 			if ( false === $response[ 'status' ] ) {
 				wp_send_json_error( $response );
 			}
@@ -74,47 +68,67 @@ class legal_display_action_01 {
     }
 
 		/**	Définition de la révision du document / Define the document version	*/
-		$document_revision = $document_controller->get_document_type_next_revision( array( 'affichage_legal' ), $legal_display->id );
+		$document_revision = $document_controller->get_document_type_next_revision( array( 'affichage_legal_A4' ), $data['legal_display']->id );
 
-    $third_inspector = $third_class->show( $legal_display->option['detective_work_id'] );
-    $third_inspector_address = $wpdigi_address_ctr->show( $third_inspector->option['contact']['address_id'] );
-
-    $third_doctor = $third_class->show( $legal_display->option['occupational_health_service_id'] );
-    $third_doctor_address = $wpdigi_address_ctr->show( $third_doctor->option['contact']['address_id'] );
+		ini_set("display_errors", true);
+		error_reporting(E_ALL);
 
 		/**	Définition finale de l'affichage légal	*/
 		$legal_display_sheet_details = array(
-      'inspection_du_travail_nom' => $third_inspector->option['full_name'],
-      'inspection_du_travail_adresse' => $third_inspector_address->option['address'],
-      'inspection_du_travail_code_postal' => $third_inspector_address->option['postcode'],
-      'inspection_du_travail_ville' => $third_inspector_address->option['town'],
-      'inspection_du_travail_telephone' => max( $third_inspector->option['contact']['phone'] ),
-      'service_de_sante_au_travail_nom' => $third_doctor->option['full_name'],
-      'service_de_sante_au_travail_adresse' => $third_doctor_address->option['address'],
-      'service_de_sante_au_travail_code_postal' => $third_doctor_address->option['postcode'],
-      'service_de_sante_au_travail_ville' => $third_doctor_address->option['town'],
-      'service_de_sante_au_travail_telephone' => max( $third_doctor->option['contact']['phone'] ),
-      'samu' => $legal_display->option['emergency_service']['samu'],
-      'police' => $legal_display->option['emergency_service']['police'],
-      'pompiers' => $legal_display->option['emergency_service']['pompier'],
-      'toute_urgence' => $legal_display->option['emergency_service']['emergency'],
-      'defenseur_des_droits' => $legal_display->option['emergency_service']['right_defender'],
-      'anti_poison' => $legal_display->option['emergency_service']['poison_control_center'],
-      'responsable_a_prevenir' => $legal_display->option['safety_rule']['responsible_for_preventing'],
-      'telephone' => $legal_display->option['safety_rule']['phone'],
-      'emplacement_des_consignes_detaillees' => $legal_display->option['safety_rule']['location_of_detailed_instruction'],
-      'permanente' => $legal_display->option['derogation_schedule']['permanent'],
-      'occasionnelle' => $legal_display->option['derogation_schedule']['occasional'],
-      'intitule' => $legal_display->option['document']['title_of_the_applicable_collective_agreement'],
-      'lieu_et_modalites' => $legal_display->option['document']['location_and_access_terms_of_the_agreement'],
-      'lieu_affichage' => $legal_display->option['document']['the_rule_of_procedure_display_location'],
-      'modalite_access' => $legal_display->option['document']['how_access_to_duer'],
+      'inspection_du_travail_nom' => $data['detective_work']->option['full_name'],
+      'inspection_du_travail_adresse' => $data['detective_work']->address->option['address'],
+      'inspection_du_travail_code_postal' => $data['detective_work']->address->option['postcode'],
+      'inspection_du_travail_ville' => $data['detective_work']->address->option['town'],
+      'inspection_du_travail_telephone' => max( $data['detective_work']->option['contact']['phone'] ),
+      'inspection_du_travail_horaire' => $data['detective_work']->option['opening_time'],
+
+      'service_de_sante_au_travail_nom' => $data['occupational_health_service']->option['full_name'],
+      'service_de_sante_au_travail_adresse' => $data['occupational_health_service']->address->option['address'],
+      'service_de_sante_au_travail_code_postal' => $data['occupational_health_service']->address->option['postcode'],
+      'service_de_sante_au_travail_ville' => $data['occupational_health_service']->address->option['town'],
+      'service_de_sante_au_travail_telephone' => max( $data['occupational_health_service']->option['contact']['phone'] ),
+			'service_de_sante_au_travail_horaire' => $data['occupational_health_service']->option['opening_time'],
+
+      'samu' => $data['legal_display']->option['emergency_service']['samu'],
+      'police' => $data['legal_display']->option['emergency_service']['police'],
+      'pompiers' => $data['legal_display']->option['emergency_service']['pompier'],
+      'toute_urgence' => $data['legal_display']->option['emergency_service']['emergency'],
+      'defenseur_des_droits' => $data['legal_display']->option['emergency_service']['right_defender'],
+      'anti_poison' => $data['legal_display']->option['emergency_service']['poison_control_center'],
+
+      'responsable_a_prevenir' => $data['legal_display']->option['safety_rule']['responsible_for_preventing'],
+      'telephone' => $data['legal_display']->option['safety_rule']['phone'],
+      'emplacement_des_consignes_detaillees' => $data['legal_display']->option['safety_rule']['location_of_detailed_instruction'],
+
+      'permanentes' => $data['legal_display']->option['derogation_schedule']['permanent'],
+      'occasionnelles' => $data['legal_display']->option['derogation_schedule']['occasional'],
+
+      'intitule' => $data['legal_display']->option['collective_agreement']['title_of_the_applicable_collective_agreement'],
+      'lieu_modalite' => $data['legal_display']->option['collective_agreement']['location_and_access_terms_of_the_agreement'],
+
+      'lieu_affichage' =>$data['legal_display']->option['rules']['location'],
+      'modalite_access' =>$data['legal_display']->option['DUER']['how_access_to_duer'],
+
+			'lundi_matin' => $data['legal_display']->option['working_hour']['monday_morning'],
+			'mardi_matin' => $data['legal_display']->option['working_hour']['tuesday_morning'],
+			'mercredi_matin' => $data['legal_display']->option['working_hour']['wednesday_morning'],
+			'jeudi_matin' => $data['legal_display']->option['working_hour']['thursday_morning'],
+			'vendredi_matin' => $data['legal_display']->option['working_hour']['friday_morning'],
+			'samedi_matin' => $data['legal_display']->option['working_hour']['saturday_morning'],
+			'dimanche_matin' => $data['legal_display']->option['working_hour']['sunday_morning'],
+
+			'lundi_aprem' => $data['legal_display']->option['working_hour']['monday_afternoon'],
+			'mardi_aprem' => $data['legal_display']->option['working_hour']['tuesday_afternoon'],
+			'mercredi_aprem' => $data['legal_display']->option['working_hour']['wednesday_afternoon'],
+			'jeudi_aprem' => $data['legal_display']->option['working_hour']['thursday_afternoon'],
+			'vendredi_aprem' => $data['legal_display']->option['working_hour']['friday_afternoon'],
+			'samedi_aprem' => $data['legal_display']->option['working_hour']['saturday_afternoon'],
+			'dimanche_aprem' => $data['legal_display']->option['working_hour']['sunday_afternoon'],
 		);
 
-
-    $document_creation = $document_controller->create_document( $group_model_to_use, $legal_display_sheet_details, $group->type. '/' . $group->id . '/affichage_legal.odt' , null );
+    $document_creation = $document_controller->create_document( $group_model_to_use, $legal_display_sheet_details, $element_parent->type. '/' . $element_parent->id . '/affichage_legal_A4.odt' , null );
     $upload_dir = wp_upload_dir();
-    $response['path'] = $upload_dir['baseurl'] . '/digirisk/' . $group->type. '/' . $group->id . '/affichage_legal.odt';
+    $response['path'] = $upload_dir['baseurl'] . '/digirisk/' . $element_parent->type. '/' . $element_parent->id . '/affichage_legal_A4.odt';
 		wp_send_json_success( $response );
 	}
 
