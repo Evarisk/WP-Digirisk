b99b3f4eb214994a5c364abbf764b8155151427c
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-29 16:54:43 +0200

    Mise à jour de l'affichage compelexe d'evarisk pour les risques

--- modules/evaluation_method/view/popup/popup.view.php
+++ modules/evaluation_method/view/popup/popup.view.php
@@ -11,7 +11,7 @@
 
 if ( !defined( 'ABSPATH' ) ) exit; ?>
 
-<div class="wpdigi-method-evaluation-render digi-popup hidden">
+<div class="wpdigi-method-evaluation-render digi-popup">
   <!-- Utile pour retenir la méthode d'evaluation utilisée -->
   <input type="hidden" class="digi-method-evaluation-id" value="<?php echo $term_evarisk->term_id; ?>" />
 
