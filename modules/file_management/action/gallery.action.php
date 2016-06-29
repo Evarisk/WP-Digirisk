4d61a1b9c31eb847ff7747a5fc7f1e5467b38340
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-29 15:14:56 +0200

    Ajout de la méthode callback_set_thumbnail

--- modules/file_management/action/gallery.action.php
+++ modules/file_management/action/gallery.action.php
@@ -13,8 +13,30 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 class gallery_action {
   public function __construct() {
-    add_action( 'wp_ajax_eo_set_thumbnail', array( gallery_class::get(), 'callback_set_thumbnail' ) );
+    add_action( 'wp_ajax_eo_set_thumbnail', array( $this, 'callback_set_thumbnail' ) );
   }
+
+	public function callback_set_thumbnail() {
+		if ( 0 === (int) $_POST['element_id'] )
+			wp_send_json_error();
+		else {
+			$element_id = (int) $_POST['element_id'];
+		}
+
+		if ( 0 === (int) $_POST['thumbnail_id'] )
+			wp_send_json_error();
+		else {
+			$thumbnail_id = (int) $_POST['thumbnail_id'];
+		}
+
+		set_post_thumbnail( $element_id, $thumbnail_id );
+
+		ob_start();
+		echo get_the_post_thumbnail( $element_id, 'digirisk-element-miniature' );
+		$template = ob_get_clean();
+
+		wp_send_json_success( array( 'template' => $template ) );
+	}
 }
 
 new gallery_action();
