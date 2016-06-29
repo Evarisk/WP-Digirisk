5a229cc1fdd91c3ab08a81b8ccc40368953f5351
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-29 10:09:40 +0200

    Inclus le dossier core

--- script/nonce.test.php
+++ script/nonce.test.php
@@ -11,6 +11,7 @@ DEFINE( 'EVA_PLUGIN_VERSION', 'test' );
 include_once( WPDIGI_PATH . '/core/module_management/module_management.php');
 
 /**	Appel automatique des modules présent dans le plugin / Install automatically modules into module directory	*/
+digi_module_management::core_util();
 digi_module_management::extra_modules();
 
 
