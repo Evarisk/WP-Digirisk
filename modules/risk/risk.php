8f6797f08d7b80bf19f9a62c2a5ef60df4c72faf
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-06-29 11:32:19 +0200

    Inclus risk_evaluation.action.php

--- modules/risk/risk.php
+++ modules/risk/risk.php
@@ -25,5 +25,6 @@ include( WPDIGI_RISKS_PATH . '/controller/risk.controller.01.php' );
 include( WPDIGI_RISKS_PATH . '/controller/risk.action.01.php' );
 include( WPDIGI_RISKS_PATH . '/controller/risk_evaluation.controller.01.php' );
 include( WPDIGI_RISKS_PATH . '/controller/risk_evaluation_comment.controller.01.php' );
+include( WPDIGI_RISKS_PATH . '/action/risk_evaluation.action.php' );
 
 ?>
