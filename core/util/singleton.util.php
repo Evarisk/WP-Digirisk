5d3d260d28fcb1e44fd3ff69fd1532014f784a71
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-08 11:43:15 +0200

    Ajout des commentaires

--- core/util/singleton.util.php
+++ core/util/singleton.util.php
@@ -1,17 +1,17 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit;
 
-/**
- * @author Jimmy Latour <jimmy.eoxia@gmail.com>
- * @version 1.0
- * Abstract Singleton class for PHP.  Extending classes must define a protected static $instance member.
- */
-
 abstract class singleton_util {
   protected static $instance;
 
+	/**
+	* Appelle le constructeur parent
+	*/
   protected final function __construct() { $this->construct(); }
   abstract protected function construct();
 
+	/**
+	* Récupères l'instance courante
+	*/
   final public static function get() {
     if ( !isset( self::$instance ) || get_called_class() != get_class( self::$instance ) ) {
       $class_name = get_called_class();
