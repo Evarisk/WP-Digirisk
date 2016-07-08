f5a576762d34247e3bdc769f0870f87e2fefcdc6
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-08 11:41:41 +0200

    Ajout des commentiares

--- modules/address/class/address.class.php
+++ modules/address/class/address.class.php
@@ -1,28 +1,13 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit;
-/**
- * Classe du controlleur principal pour les adresses dans Digirisk / Controller class for addresses for Digirisk
- *
- * @author Evarisk development team <dev@evarisk.com>
- * @version 6.0
- */
+
 class address_class extends comment_class {
-	/**
-	 * Nom du modèle à utiliser / Model name to use
-	 * @var string
-	 */
+
 	protected $model_name   = 'wpdigi_address_mdl_01';
-	/**
-	 * Type de l'élément dans wordpress / Wordpress element type
-	 * @var string
-	 */
+
 	protected $comment_type  = 'digi-address';
-	/**
-	 * Nom du champs (meta) de stockage des données liées / Name of field (meta) for linked datas storage
-	 * @var string
-	 */
+
 	protected $meta_key    	= '_wpdigi_address';
 
-	/**	Défini la route par défaut permettant d'accèder à l'élément depuis WP Rest API  / Define the default route for accessing to element from WP Rest API	*/
 	protected $base = 'address';
 	protected $version = '0.1';
 
@@ -33,6 +18,13 @@ class address_class extends comment_class {
 		include_once( ADDRESS_PATH . 'model/address.model.01.php' );
 	}
 
+	/**
+	* Créer une addresse
+	*
+	* @param array $data (test: postcode => 34130, address => 10 rue du clavier, town => Montpellier ) Les données a sauvegarder dans l'addresse
+	*
+	* @return object The address_model object
+	*/
 	public function save_data( $data ) {
 		$data['postcode'] = sanitize_text_field( $data['postcode'] );
 		$data['address'] = sanitize_text_field( $data['address'] );
@@ -40,5 +32,4 @@ class address_class extends comment_class {
 
 		return $this->create( $data );
 	}
-
 }
