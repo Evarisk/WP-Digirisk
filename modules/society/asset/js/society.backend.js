7d7240b2405b69b33b2abfc405def7c73f3d936c
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-18 17:56:44 +0200

    Modification pour bitHound

--- modules/society/asset/js/society.backend.js
+++ modules/society/asset/js/society.backend.js
@@ -4,15 +4,21 @@ var digi_society = {
 	$: undefined,
 	event: function( $ ) {
 		digi_society.$ = $,
-
-		digi_society.$( document ).on( 'click', '.wp-digi-societytree-left-container .wp-digi-global-name', function( event ) { digi_society.load_society( event, digi_society.$( this ) ); } );
-
-		/**	Quand on commence a modifier le nom ou la description, on affiche le bouton de sauvgarde	*/
 		digi_society.$( document ).on( 'keyup', 'input[name="establishment_name"]', function( event ) { digi_society.identity_edition_mode( event, digi_society.$( this ) ); } );
-		/**	Quand on clique sur le bouton de sauvegarde des informations d'une unité	*/
-		digi_society.$( document ).on( 'click', '.wp-digi-global-sheet-header button.wp-digi-save-identity-button', function( event ) { digi_society.save_identity( event, digi_society.$( this ) ); } );
-
+		digi_society.$( document ).on( 'click', '.wp-digi-societytree-left-container .wp-digi-global-name', function( event ) { digi_society.load_society( event, digi_society.$( this ) ); } );
+		digi_society.$( document ).on( 'click', '.wp-digi-global-sheet-header button.wp-digi-save-identity-button', function( event ) { digi_society.save_society( event, digi_society.$( this ) ); } );
 		digi_society.$( document ).on( 'click', '.wp-digi-global-sheet-header .wp-digi-delete-action', function ( event ) { digi_society.delete_society( event, digi_society.$( this ) ); } );
+	},
+
+	/**
+	 * Affichage du bouton d'enregistrement pour les informations principales d'une unité de travail. Mise en avant des champs a sauvegarder
+	 *
+	 * @param event Evenement appelé pour le lancement de l'action
+	 * @param element L'élément cliqué
+	 */
+	identity_edition_mode: function( event, element ) {
+		digi_society.$( element ).closest( '.wp-digi-global-sheet-header' ).find( ".wp-digi-global-action-container" ).removeClass( "hidden" );
+		digi_society.$( element ).addClass( "active" );
 	},
 
 	load_society: function( event, element ) {
@@ -27,7 +33,7 @@ var digi_society = {
 			tab_to_display: 'digi-risk',
 		};
 
-		digi_society.$.post( ajaxurl, data, function( response ) {
+		digi_society.$.post( window.ajaxurl, data, function( response ) {
 			digi_society.$( '.wp-digi-societytree-right-container' ).removeClass( "wp-digi-bloc-loading" );
 			digi_society.$( '.wp-digi-societytree-right-container' ).html( response.data.template );
 			digi_society.$( '.wp-digi-societytree-left-container' ).html( response.data.template_left );
@@ -36,23 +42,12 @@ var digi_society = {
 	},
 
 	/**
-	 * Affichage du bouton d'enregistrement pour les informations principales d'une unité de travail. Mise en avant des champs a sauvegarder
-	 *
-	 * @param event Evenement appelé pour le lancement de l'action
-	 * @param element L'élément cliqué
-	 */
-	identity_edition_mode: function( event, element ) {
-		digi_society.$( element ).closest( '.wp-digi-global-sheet-header' ).find( ".wp-digi-global-action-container" ).removeClass( "hidden" );
-		digi_society.$( element ).addClass( "active" );
-	},
-
-	/**
 	 * Enregistrement des informations principales d'une unité de travail
 	 *
 	 * @param event Evenement appelé pour le lancement de l'action
 	 * @param element L'élément cliqué
 	 */
-	save_identity: function( event, element ) {
+	save_society: function( event, element ) {
 		digi_society.$( element ).addClass( "wp-digi-loading" );
 
 		var element_id = digi_society.$( element ).closest( '.wp-digi-sheet' ).data( 'id' );
