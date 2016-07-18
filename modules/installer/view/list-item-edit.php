3072f0a84e697acce569f514b14c7849efbd1cb8
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-18 11:25:47 +0200

    4548 - Page installeur/utilisateur: Mis à jour du template niveau CSS

--- modules/installer/view/list-item-edit.php
+++ modules/installer/view/list-item-edit.php
@@ -9,7 +9,7 @@
 	<span><input type="text" placeholder="Name" name="user[option][user_info][lastname]" value="<?php echo stripslashes($user->option['user_info']['lastname']); ?>" /></span>
 	<span><input type="text" placeholder="Firstname" name="user[option][user_info][firstname]" value="<?php echo stripslashes($user->option['user_info']['firstname']); ?>" /></span>
 	<span><input type="text" placeholder="email" name="user[email]" value="<?php echo stripslashes($user->email); ?>" /></span>
-	<span class="wp-digi-user wp-digi-user-action">
+	<span class="wp-digi-action wp-digi-user wp-digi-user-action">
 		<a href="#" data-id="<?php echo $user->id; ?>" class="wp-digi-action wp-digi-action-edit dashicons dashicons-edit"></a>
 	</span>
 </form>
--- modules/installer/view/list-item.php
+++ modules/installer/view/list-item.php
@@ -1,12 +1,12 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit; ?>
 
-<li class="wp-digi-list-item" data-id="<?php echo $user->id; ?>">
+<li class="wp-digi-risk-item wp-digi-list-item" data-id="<?php echo $user->id; ?>">
 	<span class="wp-avatar" style="background: #<?php echo $user->option['user_info']['avatar_color']; ?>;" ><?php echo $user->option['user_info']['initial']; ?></span>
 	<span><strong>U<?php echo $user->id; ?></strong></span>
 	<span><?php echo stripslashes( $user->option['user_info']['lastname'] ); ?></span>
 	<span><?php echo stripslashes( $user->option['user_info']['firstname'] ); ?></span>
 	<span><?php echo $user->email; ?></span>
-	<span class="wp-digi-user wp-digi-user-action">
+	<span class="wp-digi-action wp-digi-user wp-digi-user-action">
 		<a href="#" data-id="<?php echo $user->id; ?>" data-nonce="<?php echo wp_create_nonce( 'ajax_load_user_' . $user->id ); ?>" class="wp-digi-action wp-digi-action-load dashicons dashicons-edit"></a>
 		<a href="#" data-id="<?php echo $user->id; ?>" data-nonce="<?php echo wp_create_nonce( 'ajax_delete_user_' . $user->id ); ?>" class="wp-digi-action wp-digi-action-delete dashicons dashicons-no-alt"></a>
 	</span>
