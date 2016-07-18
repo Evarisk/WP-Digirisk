9a8699f24ec902415a05bd51840ab872c11d4135
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-18 09:44:40 +0200

    Ajout du shortcode wpdigi-workunit-list

--- modules/society/shortcode/workunit.shortcode.php
+++ modules/society/shortcode/workunit.shortcode.php
@@ -15,8 +15,38 @@ class workunit_shortcode {
 	* Le constructeur
 	*/
   public function __construct() {
+		add_shortcode( 'wpdigi-workunit-list', array( &$this, 'shortcode_workunit_list' ) );
     add_shortcode( 'digi-sheet-workunit', array( $this, 'callback_digi_sheet_workunit' ) );
   }
+
+	/**
+	 * Affiche la liste des groupements existant sous forme de liste déroulante si il en existe plusieurs / Display a dropdown with all groups if there are several existing
+	 *
+	 * @param array $args Les paramètres passés au travers du shortcode / Parameters list passed thrgough shortcode
+	 *
+	 * @return string Le code html permettant d'afficher la liste déroulante contenant les groupements existant / The HTML code allowing to display existing groups
+	 */
+	public function shortcode_workunit_list( $args ) {
+		$output = '';
+
+		/**	Get existing groups for display	*/
+		$list = $this->index( array( 'posts_per_page' => -1, 'parent_id' => 0, 'post_status' => array( 'publish', ), 'post_parent' => $args[ 'group_id' ] ), false );
+
+		/**	Define a nonce for display sheet using ajax	*/
+		$workunit_display_nonce = wp_create_nonce( 'wpdigi_workunit_sheet_display' );
+
+		/**	Affichage de la liste des unités de travail pour le groupement actuellement sélectionné / Display the work unit list for current selected group	*/
+		$path = wpdigi_utils::get_template_part( WPDIGI_STES_DIR, WPDIGI_STES_TEMPLATES_MAIN_DIR, 'workunit', 'list' );
+
+		if ( $path ) {
+			ob_start();
+			require_once( wpdigi_utils::get_template_part( WPDIGI_STES_DIR, WPDIGI_STES_TEMPLATES_MAIN_DIR, 'workunit', 'list' ) );
+			$output = ob_get_contents();
+			ob_end_clean();
+		}
+
+		return  $output;
+	}
 
   /**
 	* Affiches le contenu d'un établissement
