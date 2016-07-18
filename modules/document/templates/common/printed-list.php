337b5108dff9a19f596621560388c0b8445717f6
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-18 14:43:49 +0200

    Ajout du message "There is no document yet"

--- modules/document/templates/common/printed-list.php
+++ modules/document/templates/common/printed-list.php
@@ -1,11 +1,16 @@
-<?php if ( !defined( 'ABSPATH' ) ) exit; ?>
+<?php if ( !defined( 'ABSPATH' ) ) exit;
 
-<ul class="wp-digi-list wp-digi-table wp-digi-list-document">
-	<?php
-	if ( !empty( $list_document ) ):
-	  foreach ( $list_document as $element ):
-			require( wpdigi_utils::get_template_part( WPDIGI_DOC_DIR, WPDIGI_DOC_TEMPLATES_MAIN_DIR, 'common', 'printed-list', 'item' ) );
-	  endforeach;
-	endif;
+if ( !empty( $list_document ) ):
 	?>
-</ul>
+	<ul class="wp-digi-list wp-digi-table wp-digi-list-document">
+	<?php>
+  foreach ( $list_document as $element ):
+		require( wpdigi_utils::get_template_part( WPDIGI_DOC_DIR, WPDIGI_DOC_TEMPLATES_MAIN_DIR, 'common', 'printed-list', 'item' ) );
+  endforeach;
+	?>
+	</ul>
+	<?php
+else:
+	_e( 'There is no document yet', 'digirisk' );
+endif;
+?>
