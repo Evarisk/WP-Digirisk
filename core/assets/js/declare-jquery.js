479b6b07f5fde36c3854e84a855757be9cdbc4f2
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-18 15:19:34 +0200

    Passes $ en paramètre

--- core/assets/js/declare-jquery.js
+++ core/assets/js/declare-jquery.js
@@ -1,20 +1,20 @@
 "use strict";
 
 jQuery(document).ready(function( $ ) {
-	digi_global.init();
-	legal_display.init();
-	digi_recommendation.init();
-	digi_risk.init();
+	digi_global.init( $ );
+	legal_display.init( $ );
+	digi_recommendation.init( $ );
+	digi_risk.init( $ );
 
-	digi_installer.event();
-	digi_global.event();
-	digi_evaluation_method.event();
-	digi_evaluator.event();
-	file_management.event();
-	wpeo_gallery.event();
-	digi_group.event();
-	digi_society.event();
-	digi_workunit.event();
-	digi_tab.event();
-	digi_user.event();
+	digi_installer.event( $ );
+	digi_global.event( $ );
+	digi_evaluation_method.event( $ );
+	digi_evaluator.event( $ );
+	file_management.event( $ );
+	wpeo_gallery.event( $ );
+	digi_group.event( $ );
+	digi_society.event( $ );
+	digi_workunit.event( $ );
+	digi_tab.event( $ );
+	digi_user.event( $ );
 } );
