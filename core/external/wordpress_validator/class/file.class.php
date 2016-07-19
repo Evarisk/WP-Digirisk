81ff98785b49251316e7be636d0eba5ccd6327d8
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-19 09:24:17 +0200

    Travis-CI

--- core/external/wordpress_validator/class/file.class.php
+++ core/external/wordpress_validator/class/file.class.php
@@ -2,6 +2,7 @@
 
 class file_class {
 	public function search_file( $folder, $pattern ) {
+		echo $folder . PHP_EOL;
 		$dir = new RecursiveDirectoryIterator( $folder );
 		$ite = new RecursiveIteratorIterator( $dir );
 		$files = new RegexIterator( $ite, $pattern, RegexIterator::GET_MATCH );
