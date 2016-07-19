27200e7a9b2aa3f94ca81a523f0c80ace3865d7a
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-19 15:25:37 +0200

    Traductions

--- core/assets/js/define-string.js
+++
@@ -1,3 +0,0 @@
-var digi_confirm_delete = "Confirm delete ? ";
-var digi_tools_in_progress = "In progress...";
-var digi_tools_done = "Done!";
---
+++ core/assets/js/define-string.js.php
@@ -0,0 +1,9 @@
+<script type="text/javascript" >
+	var digi_current_date = "<?php echo mysql2date( 'Y-m-d', current_time( 'mysql', 0 ), false ); ?>";
+	var digi_current_datetime = "<?php echo mysql2date( 'Y-m-d H:i', current_time( 'mysql', 0 ), false ); ?>";
+	var digi_confirm_delete = "<?php _e( 'Confirm delete ?', 'digirisk' ); ?>";
+
+	var digi_tools_in_progress = "<?php _e( 'En cours...', 'digirisk'); ?>";
+	var digi_tools_done = "<?php _e( 'Terminé', 'digirisk'); ?>";
+	var digi_tools_confirm = "<?php _e( 'Attention, avant cette opération, il est recommendé de faire une sauvegarde de votre base de donnée.', 'digirisk' ); ?>"
+</script>
