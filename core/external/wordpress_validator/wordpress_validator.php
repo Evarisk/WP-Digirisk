7d0f30bde8c3d82795a08693ec76438bf3fc2b39
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-19 09:03:06 +0200

    Mise à jour pour Travis-CI

--- core/external/wordpress_validator/wordpress_validator.php
+++ core/external/wordpress_validator/wordpress_validator.php
@@ -1,8 +1,8 @@
 <?php
 
-require './wordpress_validator.config.php';
-require './class/file.class.php';
-require './test/request.test.php';
+require 'wordpress_validator.config.php';
+require 'class/file.class.php';
+require 'test/request.test.php';
 
 $file_class = new file_class();
 $list_file = $file_class->search_file( PLUGIN_PATH, "/^.*\.php$/" );
