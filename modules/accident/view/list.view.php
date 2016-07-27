9f527a520d722f9ae78b43327a08c5dbbd733713
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-27 17:36:35 +0200

    Ajout du header

--- modules/accident/view/list.view.php
+++ modules/accident/view/list.view.php
@@ -3,14 +3,13 @@
 <ul class="wp-digi-list wp-digi-risk wp-digi-table" >
 	<li class="wp-digi-risk-list-header wp-digi-table-header">
 		<span class="wp-digi-risk-list-column-thumbnail" >&nbsp;</span>
-		<span class="wp-digi-risk-list-column-id"><?php _e( 'ID', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-name"><?php _e( 'nom', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-description"><?php _e( 'descr.', 'digirisk' ); ?> </span>
-		<span class="wp-digi-risk-list-column-serial-number"><?php _e('n° serie', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-fabrication-date"><?php _e('date fab.', 'digirisk'); ?></span>
-		<span class="wp-digi-risk-list-column-shelf-life"><?php _e('durée de vie', 'digirisk'); ?></span>
-		<span class="wp-digi-risk-list-column-periodicity-of-controls"><?php _e('per. de contrôles', 'digirisk'); ?></span>
-
+		<span><?php _e( 'Ref.', 'digirisk' ); ?></span>
+		<span><?php _e( 'Risque associé', 'digirisk' ); ?></span>
+		<span><?php _e( 'Date accident', 'digirisk' ); ?> </span>
+		<span><?php _e('Personne accidentée', 'digirisk' ); ?></span>
+		<span><?php _e('Description', 'digirisk'); ?></span>
+		<span><?php _e('Nb. de jour arrêt', 'digirisk'); ?></span>
+		<span><?php _e('Enquête accident', 'digirisk'); ?></span>
 		<span class="wp-digi-risk-list-column-actions" >&nbsp;</span>
 	</li>
 
