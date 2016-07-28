d17bb21d8d1bb6e7d608fbdce248bd751c014857
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-07-28 10:18:30 +0200

    = Version 6.1.5.4 =
    Améliorations
    
    * 4674 - Minify le JavaScript

--- digirisk.config.php
+++ digirisk.config.php
@@ -16,7 +16,7 @@
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-DEFINE( 'EVA_PLUGIN_VERSION', '6.1.5.3' );
+DEFINE( 'EVA_PLUGIN_VERSION', '6.1.5.4' );
 
 DEFINE( 'WPDIGI_VERSION', EVA_PLUGIN_VERSION );
 DEFINE( 'WPDIGI_DIR', basename( dirname( __FILE__ ) ) );
