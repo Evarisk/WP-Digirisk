27257d93e18a2d4876ac9d27fd1ebfbe21fae4a1
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-05 17:35:26 +0200

    Initialisation

---
+++ modules/export_import/view/progress-table.view.php
@@ -0,0 +1,11 @@
+<?php if ( !defined( 'ABSPATH' ) ) exit;
+/**
+ * Le tableau de progression de l'import
+ *
+ * @author Jimmy Latour <dev@evarisk.com>
+ * @version 6.1.5.6
+ * @copyright 2015-2016 Evarisk
+ * @package export_import
+ * @subpackage view
+ */
+?>
