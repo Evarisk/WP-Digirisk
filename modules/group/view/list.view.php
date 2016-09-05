7f21b4ed82b1d31fb144e2ae78d50555f1312882
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-05 17:23:59 +0200

    Récursivité sur les groupements enfantes

--- modules/group/view/list.view.php
+++ modules/group/view/list.view.php
@@ -1,26 +1,26 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit;
 
-if ( !empty( $groupment ) ):
-	?>
-	<ul class="<?php echo isset( $key ) ? 'sub-menu': 'parent'; ?>">
-		<li data-id="<?php echo $groupment->id; ?>" class="<?php echo $groupment->id === $selected_group->id ? 'active' : ''; ?>">
-			<div>
-				<span data-id="<?php echo $groupment->id; ?>" class="wp-digi-global-name"><?php echo $groupment->unique_identifier . ' - ' . $groupment->title; ?></span>
-				<span class="wp-digi-new-group-action">
-					<a data-id="<?php echo $groupment->id; ?>" href="#" class="wp-digi-action dashicons dashicons-plus"></a>
-				</span>
-			</div>
+if ( !empty( $list_groupment ) ):
+	foreach( $list_groupment as $key => $groupment ):
+		?>
+		<ul class="<?php echo isset( $key ) ? 'sub-menu': 'parent'; ?>">
+			<li data-id="<?php echo $groupment->id; ?>" class="<?php echo $groupment->id === $selected_group->id ? 'active' : ''; ?>">
+				<div>
+					<span data-id="<?php echo $groupment->id; ?>" class="wp-digi-global-name"><?php echo $groupment->unique_identifier . ' - ' . $groupment->title; ?></span>
+					<span class="wp-digi-new-group-action">
+						<a data-id="<?php echo $groupment->id; ?>" href="#" class="wp-digi-action dashicons dashicons-plus"></a>
+					</span>
+				</div>
 
-			<?php
-			if ( !empty( $groupment->list_group ) ) {
-			  foreach ( $groupment->list_group as $key => $sub_group ) {
-					$groupment = $sub_group;
+				<?php
+				if ( !empty( $groupment->list_group ) ) {
+					$list_groupment = $groupment->list_group;
 					require ( GROUP_VIEW_DIR . '/list.view.php' );
-			  }
-			}
-			?>
+				}
+				?>
 
-		</li>
-	</ul>
-	<?php
+			</li>
+		</ul>
+		<?php
+	endforeach;
 endif;
