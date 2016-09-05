b40f111a185e62609b0b9ae61e34aafc3297c69d
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-05 10:22:03 +0200

    La fonction unzip renvoie la liste de nom des documents extraits

--- core/util/zip.util.php
+++ core/util/zip.util.php
@@ -8,15 +8,39 @@
 class zip_util extends singleton_util {
 	protected function construct() {}
 
+	/**
+	 *
+	 * Dézippes l'archive $zip_path dans $destination_path
+	 * Retournes tous les noms des fichiers contenus dans l'archive
+	 *
+	 * @param  string $zip_path         Le chemin vers l'archive
+	 * @param  string $destination_path Le chemin d'extraction des fichiers
+	 * @return $data = array('state' => true|false, 'list_file' => array( 0 => filename ) );
+	 */
   public function unzip( $zip_path, $destination_path ) {
 		$zip = new ZipArchive;
+		$data = array( 'state' => true, 'list_file' => array() );
+
 		if ( $zip->open( $zip_path ) === TRUE ) {
-			$zip->extractTo( $destination_path );
+			if ( !$zip->extractTo( $destination_path ) ) {
+				$data['state'] = false;
+			}
+
+			// Récupérations de tous les fichiers
+			for ( $i = 0; $i < $zip->numFiles; $i++ ) {
+				$filename = $zip->getNameIndex(0);
+
+				if ( isset( $filename ) ) {
+					$data['list_file'][] = $filename;
+				}
+			}
+
 			$zip->close();
-			return true;
 		}
 		else {
-			return false;
+			$data['state'] = false;
 		}
+
+		return $data;
 	}
 }
