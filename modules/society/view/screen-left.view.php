e6522a654061c9b6409a257e3122e28a89e857aa
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-06 16:12:05 +0200

    Initialisation

---
+++ modules/society/view/screen-left.view.php
@@ -0,0 +1,7 @@
+<?php if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+<div class="wp-digi-societytree-left-container wp-digi-bloc-loader" >
+	<?php group_class::g()->display_toggle( $group_list, $society ); ?>
+	<?php workunit_class::g()->display_list( $element_id ); ?>
+	<button data-id="<?php echo $element_id; ?>" class="wp-digi-duer-form-display wp-digi-bton-fifth dashicons-before dashicons-share-alt2"><?php _e( 'Print DUER', 'digirisk' ); ?></button>
+</div>
