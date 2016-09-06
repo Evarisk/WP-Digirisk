8e8ed183b8a6c13a1fcfc3f022c70516b9555844
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-06 16:12:10 +0200

    Initialisation

---
+++ modules/society/view/screen-right.view.php
@@ -0,0 +1,5 @@
+<?php if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+<div class="wp-digi-societytree-right-container wp-digi-bloc-loader">
+	<?php do_shortcode( '[digi_dashboard id="' . $element_id . '"]' ); ?>
+</div>
