f64a4accf6a701fac5df8d51b4adee9c3b2f0842
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-07 16:57:21 +0200

    Version 6.1.5.6

--- digirisk.php
+++ digirisk.php
@@ -16,6 +16,7 @@ require_once( 'digirisk.config.php' );
 require_once( 'core/util/singleton.util.php' );
 require_once( 'core/util/include.util.php' );
 require_once( 'core/wpeo_log/wpeo_log.php' );
+require_once( 'core/wpeo_model/class/helper.class.php' );
 
 include_util::inc( WPDIGI_PATH . 'core/wpeo_model', array( 'config', 'helper', 'class', 'model' ) );
 include_util::inc( WPDIGI_PATH . 'core/wpeo_export', array( 'config', 'class' ) );
