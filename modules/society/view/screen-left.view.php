460d1619e91132e86e4f3aeaed933514e3e5aaee
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-09 09:56:06 +0200

    Vérifie l'existence d'un groupement

--- modules/society/view/screen-left.view.php
+++ modules/society/view/screen-left.view.php
@@ -1,7 +1,13 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <div class="wp-digi-societytree-left-container wp-digi-bloc-loader" >
-	<?php group_class::g()->display_toggle( $group_list, $society ); ?>
-	<?php workunit_class::g()->display_list( $element_id ); ?>
-	<button data-id="<?php echo $element_id; ?>" class="wp-digi-duer-form-display wp-digi-bton-fifth dashicons-before dashicons-share-alt2"><?php _e( 'Print DUER', 'digirisk' ); ?></button>
+	<?php
+	if ( !empty( $group_list ) ):
+		group_class::g()->display_toggle( $group_list, $society );
+		workunit_class::g()->display_list( $element_id );
+		?>
+		<button data-id="<?php echo $element_id; ?>" class="wp-digi-duer-form-display wp-digi-bton-fifth dashicons-before dashicons-share-alt2"><?php _e( 'Print DUER', 'digirisk' ); ?></button>
+		<?php
+	endif;
+	?>
 </div>
