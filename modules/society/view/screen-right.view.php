e7c295bfeb476bcd9bcf1dc73f33d1687a273adc
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-09 09:56:09 +0200

    Vérifie l'existence d'un groupement

--- modules/society/view/screen-right.view.php
+++ modules/society/view/screen-right.view.php
@@ -1,5 +1,9 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <div class="wp-digi-societytree-right-container wp-digi-bloc-loader">
-	<?php do_shortcode( '[digi_dashboard id="' . $element_id . '"]' ); ?>
+	<?php
+	if ( !empty( $group_list ) ):
+		do_shortcode( '[digi_dashboard id="' . $element_id . '"]' );
+	endif;
+	?>
 </div>
