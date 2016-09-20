95017c5bc147a5984c7bca0014b2a9d4fd0c3220
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-20 17:53:21 +0200

    Modification de $risk_list en $epi_list

--- modules/epi/view/list.view.php
+++ modules/epi/view/list.view.php
@@ -1,22 +1,22 @@
 <?php if ( !defined( 'ABSPATH' ) ) exit; ?>
 
-<ul class="wp-digi-list wp-digi-risk wp-digi-table" >
-	<li class="wp-digi-risk-list-header wp-digi-table-header">
-		<span class="wp-digi-risk-list-column-thumbnail" >&nbsp;</span>
-		<span class="wp-digi-risk-list-column-id"><?php _e( 'ID', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-name"><?php _e( 'nom', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-description"><?php _e( 'descr.', 'digirisk' ); ?> </span>
-		<span class="wp-digi-risk-list-column-serial-number"><?php _e('n° serie', 'digirisk' ); ?></span>
-		<span class="wp-digi-risk-list-column-fabrication-date"><?php _e('date prod.', 'digirisk'); ?></span>
-		<span class="wp-digi-risk-list-column-shelf-life"><?php _e('durée de vie', 'digirisk'); ?></span>
-		<span class="wp-digi-risk-list-column-periodicity-of-controls"><?php _e('per. de contrôles', 'digirisk'); ?></span>
+<ul class="wp-digi-list wp-digi-epi wp-digi-table" >
+	<li class="wp-digi-epi-list-header wp-digi-table-header">
+		<span class="wp-digi-epi-list-column-thumbnail" >&nbsp;</span>
+		<span class="wp-digi-epi-list-column-id"><?php _e( 'ID', 'digirisk' ); ?></span>
+		<span class="wp-digi-epi-list-column-name"><?php _e( 'nom', 'digirisk' ); ?></span>
+		<span class="wp-digi-epi-list-column-description"><?php _e( 'descr.', 'digirisk' ); ?> </span>
+		<span class="wp-digi-epi-list-column-serial-number"><?php _e('n° serie', 'digirisk' ); ?></span>
+		<span class="wp-digi-epi-list-column-fabrication-date"><?php _e('date prod.', 'digirisk'); ?></span>
+		<span class="wp-digi-epi-list-column-shelf-life"><?php _e('durée de vie', 'digirisk'); ?></span>
+		<span class="wp-digi-epi-list-column-periodicity-of-controls"><?php _e('per. de contrôles', 'digirisk'); ?></span>
 
 		<span class="wp-digi-risk-list-column-actions" >&nbsp;</span>
 	</li>
 
 	<?php $i = 1; ?>
-	<?php if ( !empty( $risk_list ) ) : ?>
-		<?php foreach ( $risk_list as $risk ) : ?>
+	<?php if ( !empty( $epi_list ) ) : ?>
+		<?php foreach ( $epi_list as $epi ) : ?>
 			<?php require( EPI_VIEW_DIR . '/list-item.php' ); ?>
 		<?php endforeach; ?>
 	<?php endif; ?>
