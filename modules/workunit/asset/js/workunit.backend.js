06a17e16164204e51c0d88bb81764ce457c41cd1
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-20 15:09:27 +0200

    Supprimes les méthodes inutiles

--- modules/workunit/asset/js/workunit.backend.js
+++ modules/workunit/asset/js/workunit.backend.js
@@ -12,11 +12,6 @@ var digi_workunit = {
 		/**	Quand on demande la suppression d'une unité de travail	*/
 		digi_workunit.$( document ).on( 'click', '.wp-digi-list-workunit .wp-digi-action-delete', function( event ) { digi_workunit.delete_workunit( event, digi_workunit.$( this ) ); } );
 
-		digi_workunit.$( ".wp-digi-societytree-right-container" ).on( "click", "#wpdigi-save-element-sheet", function( event ) { digi_workunit.save_element_sheet( event, digi_workunit.$( this ) ); } );
-
-		digi_workunit.$( ".wp-digi-societytree-right-container" ).on( "click", ".wp-digi-list-document .wp-digi-action-delete", function( event ) { digi_workunit.delete_element_sheet( event, digi_workunit.$( this ) ); } );
-		digi_workunit.$( ".wp-digi-societytree-right-container" ).on( "click", ".wp-digi-list-document .wp-digi-action-regenerate", function( event ) { digi_workunit.regenerate_document( event, digi_workunit.$( this ) ); } );
-
 		digi_workunit.$( document ).on( 'click', '.wp-digi-sheet-tab-toggle', function() { window.digi_global.responsive_menu_toggle( digi_workunit.$( this ) ); } );
 		digi_workunit.$( document ).on( 'click', '.wp-digi-sheet-tab-responsive-content > li', function() { window.digi_global.responsive_menu_toggle( digi_workunit.$( this ) ); } );
 	},
@@ -70,69 +65,5 @@ var digi_workunit = {
 			digi_workunit.$.post( window.ajaxurl, data, function( response ) {} );
 		}
 	},
-
-	regenerate_document: function ( event, element ) {
-		event.preventDefault();
-
-		var data = {
-			action: 'wpdigi_regenerate_document',
-			element_id: digi_workunit.$( element ).data( 'id' ),
-			parent_id: digi_workunit.$( element ).data( 'parent-id' ),
-			_wpnonce: digi_workunit.$( element ).data( 'nonce' ),
-		};
-		digi_workunit.$.post( window.ajaxurl, data, function() {
-
-		} );
-	},
-
-	delete_element_sheet: function( event, element ) {
-		event.preventDefault();
-
-		var data = {
-			action: 'wpdigi_delete_sheet',
-			parent_id: digi_workunit.$( element ).data( 'parent-id' ),
-			element_id: digi_workunit.$( element ).data( 'id' ),
-			global: digi_workunit.$( element ).data( 'global' ),
-		};
-
-		digi_workunit.$( element ).closest( 'li' ).fadeOut();
-
-		digi_workunit.$.post( window.ajaxurl, data, function() {
-
-		} );
- 	},
-
-	/**
-	 * Lancement de l'enregistrement de la fiche de l'unité de travail
-	 *
-	 * @param event Evenement appelé pour le lancement de l'action
-	 * @param element Element sur lequel on intervient
-	 */
-	save_element_sheet : function ( event, element ) {
-		event.preventDefault();
-
-		var options = {
-	        beforeSubmit:  function( formData, jqForm, options ) {
-	        	digi_workunit.$( element ).addClass( "wp-digi-loading" );
-	        },
-	        success:       function( responseText, statusText, xhr, $form ) {
-	        	digi_workunit.$( element ).removeClass( "wp-digi-loading" );
-	        	if ( responseText.success ) {
-	        		if ( undefined !== digi_workunit.$( ".wp-digi-global-sheet-content ul.wp-digi-list-document" ).html() ) {
-	        			digi_workunit.$( ".wp-digi-global-sheet-content ul.wp-digi-list-document" ).prepend( responseText.data.output );
-	        		}
-	        		else {
-	        			digi_workunit.$( ".wp-digi-global-sheet-tab li.wp-digi-sheet-generation-button" ).click();
-	        		}
-	        	}
-	        	else {
-
-	        	}
-	        },
-	        dataType: "json",
-	        resetForm: true,
-	    };
-		digi_workunit.$( "#wpdigi-save-element-form" ).ajaxSubmit( options );
-	}
 
 };
