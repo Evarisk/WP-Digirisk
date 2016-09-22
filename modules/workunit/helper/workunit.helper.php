f85f7597abcfb724a9d2d38fad5b67f965725668
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-22 17:35:00 +0200

    Ajout de la fonction filter_by_affected_user_id

---
+++ modules/workunit/helper/workunit.helper.php
@@ -0,0 +1,11 @@
+<?php
+
+function filter_by_affected_user_id( $data, $args ) {
+	if ( !empty( $data->user_info['affected_id']['user'][$args['user_id']] ) ) {
+		return $data;
+	}
+
+	return false;
+}
+
+?>
