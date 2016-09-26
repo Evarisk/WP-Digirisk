fcae165137027b353eb8f833cf410de93a697c20
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-09-26 15:09:23 +0200

    Mise à jour vers le nouveau modèle "wpeo_model"

--- modules/transfer_data/transfer_data.config.php
+++ modules/transfer_data/transfer_data.config.php
@@ -3,11 +3,11 @@
  * Bootstrap file
  *
  * @author Development team <dev@evarisk.com>
- * @version 1.0
+ * @version 1.1
  */
 if ( !defined( 'ABSPATH' ) ) exit;
 
-DEFINE( 'DIGI_DTRANS_VERSION', '1.0' );
+DEFINE( 'DIGI_DTRANS_VERSION', '1.1' );
 DEFINE( 'DIGI_DTRANS_DIR', basename( dirname( __FILE__ ) ) );
 DEFINE( 'DIGI_DTRANS_PATH', str_replace( DIGI_DTRANS_DIR, "", dirname( __FILE__ ) ) );
 DEFINE( 'DIGI_DTRANS_URL', str_replace( str_replace( "\\", "/", ABSPATH), site_url() . '/', str_replace( "\\", "/", DIGI_DTRANS_PATH ) ) );
@@ -15,4 +15,4 @@ DEFINE( 'DIGI_DTRANS_TEMPLATES_MAIN_DIR', DIGI_DTRANS_PATH . DIGI_DTRANS_DIR . '
 DEFINE( 'DIGI_DTRANS_NB_ELMT_PER_PAGE', 10 );
 DEFINE( 'DIGI_DTRANS_MEDIAN_MAX_STEP', 3 );
 DEFINE( 'DIGI_DTRANS_MAX_STEP', 4 );
-DEFINE( 'TRANSFER_DATA_STATE', false );
+DEFINE( 'TRANSFER_DATA_STATE', true );
