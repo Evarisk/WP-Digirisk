ea9118ee69349d7e83601b113ac9137650551919
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-10-05 11:17:57 +0200

    Résolution du problème des commentaires sur les risques

--- modules/comment/view/item-edit.view.php
+++ modules/comment/view/item-edit.view.php
@@ -9,6 +9,6 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
 	?>
 	<input type="hidden" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][<?php echo $comment->id; ?>][author_id]" value="<?php echo $comment->author_id; ?>" />
 	<input type="hidden" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][<?php echo $comment->id; ?>][id]" value="<?php echo $comment->id; ?>" />
-	<input type="text" class="wpdigi_date" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][0][date]" value="<?php echo $comment->date; ?>" /> :
-	<input type="text" class="wpdigi_comment" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][0][content]" value="<?php echo $comment->content; ?>" />
+	<input type="text" class="wpdigi_date" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][<?php echo $comment->id; ?>][date]" value="<?php echo $comment->date; ?>" /> :
+	<input type="text" class="wpdigi_comment" name="<?php echo $type; ?>[<?php echo $element->id ?>][list_comment][<?php echo $comment->id; ?>][content]" value="<?php echo $comment->content; ?>" />
 </li>
--- modules/comment/view/list.view.php
+++ modules/comment/view/list.view.php
@@ -2,15 +2,15 @@
 if ( !defined( 'ABSPATH' ) ) exit; ?>
 
 <?php if ( !empty( $element->comment ) ) : ?>
-	<?php foreach ( $element->comment as $comment ) : ?>
+	<?php foreach ( $element->comment as $key => $comment ) : ?>
 		<?php if ( $comment->status == '-34070' ): ?>
 			<?php if ( $display == 'edit' ): ?>
-				<?php view_util::exec( 'comment', 'item-edit', array('type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
+				<?php view_util::exec( 'comment', 'item-edit', array('key' => $key, 'type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
 			<?php else: ?>
-				<?php view_util::exec( 'comment', 'item', array('type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
+				<?php view_util::exec( 'comment', 'item', array('key' => $key, 'type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
 			<?php endif; ?>
 		<?php else: ?>
-			<?php view_util::exec( 'comment', 'item-edit', array('type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
+			<?php view_util::exec( 'comment', 'item-edit', array('key' => $key, 'type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
 		<?php endif; ?>
 	<?php endforeach; ?>
 <?php endif;?>
