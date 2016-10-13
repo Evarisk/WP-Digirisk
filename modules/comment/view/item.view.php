f0b95322c12a5eee826b5f51be1187aa6935c56a
Jimmy <jimmy@eoxia.com>
2016-10-13 15:58:57 +0200

    Modification de l'affichage de la date

--- modules/comment/view/item.view.php
+++ modules/comment/view/item.view.php
@@ -7,6 +7,6 @@ if ( !defined( 'ABSPATH' ) ) exit;
 	<?php
 	$userdata = get_userdata( $comment->author_id );
 	echo !empty( $userdata->display_name ) ? $userdata->display_name : ''; ?>
-	<strong><?php echo date( 'd/m/Y', strtotime( $comment->date ) ); ?></strong> :
+	<strong><?php echo $comment->date; ?></strong> :
 	<?php echo $comment->content; ?>
 </li>
