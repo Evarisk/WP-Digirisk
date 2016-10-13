c9b195922813e48638d3a280370ee688c34f0a4a
Jimmy <jimmy@eoxia.com>
2016-10-13 15:18:22 +0200

    Transforme en objet

--- modules/evaluation_method/asset/js/evaluation_method_evarisk.backend.js
+++ modules/evaluation_method/asset/js/evaluation_method_evarisk.backend.js
@@ -1,8 +1,8 @@
-window.digirisk.evaluation_method_evarisk = function() {
-	window.digirisk.evaluation_method_evarisk.init();
-	window.digirisk.evaluation_method_evarisk.event();
-}
+window.digirisk.evaluation_method_evarisk = {};
 
-window.digirisk.evaluation_method_evarisk.init = function() {};
+window.digirisk.evaluation_method_evarisk.init = function() {
+	window.digirisk.evaluation_method_evarisk.event();
+};
+
 window.digirisk.evaluation_method_evarisk.event = function() {
 };
