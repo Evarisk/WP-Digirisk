d861a30321380c3b06cbffb38aac652b1cf0c2b6
Jimmy <jimmy@eoxia.com>
2016-10-13 16:19:10 +0200

    initialisation

--- core/assets/js/lib/popup.backend.js
+++ core/assets/js/lib/popup.backend.js
@@ -4,4 +4,23 @@ window.digirisk.popup.init = function() {
 	window.digirisk.popup.event();
 };
 
-window.digirisk.popup.event = function() {};
+window.digirisk.popup.event = function() {
+  jQuery( document ).on( 'click', '.open-popup', window.digirisk.popup.open );
+  jQuery( document ).on( 'click', '.digi-popup-propagation', window.digirisk.popup.stop );
+  jQuery( document ).on( 'click', 'body', window.digirisk.popup.close );
+};
+
+window.digirisk.popup.open = function( event ) {
+  // Récupères la box de destination mis dans l'attribut du popup
+  var target = jQuery( "." + jQuery( this ).data( 'target' ) );
+	target.toggle();
+  event.stopPropagation();
+};
+
+window.digirisk.popup.stop = function( event ) {
+	event.stopPropagation();
+};
+
+window.digirisk.popup.close = function( event ) {
+  jQuery( '.digi-popup' ).hide();
+}
