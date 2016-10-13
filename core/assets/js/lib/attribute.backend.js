88478aecadc5cffeb8e3df1b435322ede7d69fe3
Jimmy <jimmy@eoxia.com>
2016-10-13 15:17:39 +0200

    Initialisation

---
+++ core/assets/js/lib/attribute.backend.js
@@ -0,0 +1,20 @@
+jQuery.fn.get_data = function( cb ) {
+  this.each( function() {
+    var data = {};
+
+    for ( var i = 0; i <  jQuery( this )[0].attributes.length; i++ ) {
+      var localName = jQuery( this )[0].attributes[i].localName;
+      if (  localName.substr(0, 4) == 'data' ||
+            localName == 'action') {
+
+        localName = localName.substr(5);
+
+        if ( localName == 'nonce' ) localName = '_wpnonce';
+        
+        data[localName] =  jQuery( this )[0].attributes[i].value;
+      }
+    }
+
+    cb( data );
+  } );
+};
