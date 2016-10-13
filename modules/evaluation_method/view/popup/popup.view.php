fde9e9f4140f23c1caf3ead8a6911278c39a6c5f
Jimmy <jimmy@eoxia.com>
2016-10-13 16:19:30 +0200

    Ajout de la class digi-popup-propagation

--- modules/evaluation_method/view/popup/popup.view.php
+++ modules/evaluation_method/view/popup/popup.view.php
@@ -16,7 +16,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
   <input type="hidden" class="digi-method-evaluation-id" value="<?php echo !empty( $term_evarisk->term_id ) ? $term_evarisk->term_id : 0; ?>" />
 
   <section class="wp-digi-eval-evarisk">
-    <div>
+    <div class="digi-popup-propagation">
 			<a href="#" class="close"><i class="dashicons dashicons-no-alt"></i></a>
 			<div class="wp-digi-eval-table">
 				<?php if ( !empty( $list_evaluation_method_variable ) ): ?>
