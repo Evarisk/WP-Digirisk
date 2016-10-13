cd7475bd445cefc5377c21687d718b514e9414f5
Jimmy <jimmy@eoxia.com>
2016-10-13 16:48:29 +0200

    L'évènement est déclenché par le clique sur action-edit

--- core/assets/js/lib/array-form.backend.js
+++ core/assets/js/lib/array-form.backend.js
@@ -5,7 +5,7 @@ window.eva_lib.array_form.init = function() {
 };
 
 window.eva_lib.array_form.event = function() {
-	jQuery( document ).on( 'click', '.wp-digi-table .wp-digi-list-item .wp-digi-action-edit', window.eva_lib.array_form.send_form );
+	jQuery( document ).on( 'click', '.wp-digi-action-edit', window.eva_lib.array_form.send_form );
 };
 
 window.eva_lib.array_form.get_input = function( parent ) {
@@ -13,6 +13,8 @@ window.eva_lib.array_form.get_input = function( parent ) {
 };
 
 window.eva_lib.array_form.send_form = function( event ) {
+	event.preventDefault();
+	
 	var element = jQuery( this );
 	var parent = element.closest( '.wp-digi-list-item' );
 	var list_input = window.eva_lib.array_form.get_input( parent );
