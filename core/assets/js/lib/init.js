ee2761887520e37aa2d04d7c2700990b5d17a261
Jimmy <jimmy@eoxia.com>
2016-10-13 15:17:51 +0200

    init au lieu de new

--- core/assets/js/lib/init.js
+++ core/assets/js/lib/init.js
@@ -11,13 +11,13 @@ window.eva_lib.init = function() {
 window.eva_lib.load_list_script = function() {
 	for ( var key in window.digirisk ) {
 		console.log('Initialisation de l\'objet: ' + key);
-		new window.digirisk[key]();
+		window.digirisk[key].init();
 	}
 }
 
 window.eva_lib.init_array_form = function() {
 	console.log('Init array form');
-	new window.eva_lib.array_form();
+	 window.eva_lib.array_form.init();
 }
 
 jQuery(document).ready(window.eva_lib.init);
