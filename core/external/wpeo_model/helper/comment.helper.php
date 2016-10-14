421542c05b9dc518bea4061b152818db739b6348
Jimmy <lelabodudev@gmail.com>
2016-10-14 15:57:43 +0200

    Date commentaire risque

--- core/external/wpeo_model/helper/comment.helper.php
+++ core/external/wpeo_model/helper/comment.helper.php
@@ -3,7 +3,7 @@ namespace digi;
 if ( !defined( 'ABSPATH' ) ) exit;
 
 function construct_current_date( $data ) {
-  $data->date = current_time( 'd/m/Y' );
+  $data->date = !empty( $data->date ) ? date( 'd/m/Y', strtotime( $data->date ) ) : current_time( 'd/m/Y' );
 
   return $data;
 }
