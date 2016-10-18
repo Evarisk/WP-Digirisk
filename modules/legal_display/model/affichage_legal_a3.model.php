d60c8b4a453483d48c9a17facc733b2d403aed55
Jimmy <lelabodudev@gmail.com>
2016-10-18 17:17:02 +0200

    Initialisation des modèles

--- modules/legal_display/model/affichage_legal_a3.model.php
+++ modules/legal_display/model/affichage_legal_a3.model.php
@@ -2,7 +2,7 @@
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-class affichage_legal_a4_model extends document_model {
+class affichage_legal_a3_model extends document_model {
 
 	/**
 	 * Construit le modèle / Fill the model
@@ -17,6 +17,129 @@ class affichage_legal_a4_model extends document_model {
 			'meta_type' 	=> 'single',
 			'field'				=> 'document_meta',
 			'child' => array(
+				'inspection_du_travail_nom' => array(
+					'type' => 'string'
+				),
+				'inspection_du_travail_adresse' => array(
+					'type' => 'string'
+				),
+				'inspection_du_travail_code_postal' => array(
+					'type' => 'string'
+				),
+				'inspection_du_travail_ville' => array(
+					'type' => 'string'
+				),
+				'inspection_du_travail_telephone' => array(
+					'type' => 'string'
+				),
+				'inspection_du_travail_horaire' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_nom' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_adresse' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_code_postal' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_ville' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_telephone' => array(
+					'type' => 'string'
+				),
+				'service_de_sante_horaire' => array(
+					'type' => 'string'
+				),
+				'samu' => array(
+					'type' => 'string'
+				),
+				'police' => array(
+					'type' => 'string'
+				),
+				'pompier' => array(
+					'type' => 'string'
+				),
+				'toute_urgence' => array(
+					'type' => 'string'
+				),
+				'defenseur_des_droits' => array(
+					'type' => 'string'
+				),
+				'anti_poison' => array(
+					'type' => 'string'
+				),
+				'responsable_a_prevenir' => array(
+					'type' => 'string'
+				),
+				'telephone' => array(
+					'type' => 'string'
+				),
+				'emplacement_des_consignes_detaillees' => array(
+					'type' => 'string'
+				),
+				'permanente' => array(
+					'type' => 'string'
+				),
+				'occasionnelle' => array(
+					'type' => 'string'
+				),
+				'intitule' => array(
+					'type' => 'string'
+				),
+				'lieu_modalite' => array(
+					'type' => 'string'
+				),
+				'lieu_affichage' => array(
+					'type' => 'string'
+				),
+				'modalite_access' => array(
+					'type' => 'string'
+				),
+				'lundi_matin' => array(
+					'type' => 'string'
+				),
+				'mardi_matin' => array(
+					'type' => 'string'
+				),
+				'mercredi_matin' => array(
+					'type' => 'string'
+				),
+				'jeudi_matin' => array(
+					'type' => 'string'
+				),
+				'vendredi_matin' => array(
+					'type' => 'string'
+				),
+				'samedi_matin' => array(
+					'type' => 'string'
+				),
+				'dimanche_matin' => array(
+					'type' => 'string'
+				),
+				'lundi_aprem' => array(
+					'type' => 'string'
+				),
+				'mardi_aprem' => array(
+					'type' => 'string'
+				),
+				'mercredi_aprem' => array(
+					'type' => 'string'
+				),
+				'jeudi_aprem' => array(
+					'type' => 'string'
+				),
+				'vendredi_aprem' => array(
+					'type' => 'string'
+				),
+				'samedi_aprem' => array(
+					'type' => 'string'
+				),
+				'dimanche_aprem' => array(
+					'type' => 'string'
+				)
 			)
 		);
 
