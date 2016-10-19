b59a4715fd7a152df7dd8dcb177804f91e617c5e
Jimmy <lelabodudev@gmail.com>
2016-10-19 14:31:14 +0200

    Mise à jour des commentaires

--- modules/comment/view/list.view.php
+++ modules/comment/view/list.view.php
@@ -8,8 +8,6 @@ if ( !defined( 'ABSPATH' ) ) exit;
 		<?php if ( $comment->status == '-34070' ): ?>
 			<?php if ( $display == 'edit' ): ?>
 				<?php view_util::exec( 'comment', 'item-edit', array('key' => $key, 'type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
-				<?php view_util::exec( 'comment', 'item-edit', array('key' => $key, 'type' => $type, 'comment' => $comment_new, 'element' => $element ) ); ?>
-
 			<?php else: ?>
 				<?php view_util::exec( 'comment', 'item', array('key' => $key, 'type' => $type, 'comment' => $comment, 'element' => $element ) ); ?>
 			<?php endif; ?>
@@ -22,3 +20,7 @@ if ( !defined( 'ABSPATH' ) ) exit;
 		<?php endif; ?>
 	<?php endforeach; ?>
 <?php endif;?>
+
+<?php if ($element->id > 0 && $display == 'edit'): ?>
+	<?php view_util::exec( 'comment', 'item-edit', array('key' => 0, 'type' => $type, 'comment' => $comment_new, 'element' => $element ) ); ?>
+<?php endif; ?>
