701bd01b3ae4281024f1aa010be65f911015470f
Jimmy <lelabodudev@gmail.com>
2016-10-20 11:12:23 +0200

    Mise à jour pagination utilisateur/evaluateur

--- modules/evaluator/asset/js/evaluator.backend.js
+++ modules/evaluator/asset/js/evaluator.backend.js
@@ -6,6 +6,7 @@ window.digirisk.evaluator.init = function() {
 
 window.digirisk.evaluator.event = function() {
 	jQuery( document ).on( 'click', '.wp-digi-evaluator-list input[type="checkbox"]', window.digirisk.evaluator.set_time );
+	jQuery( document ).on( 'click', '.wp-form-evaluator-to-assign .wp-digi-pagination a', window.digirisk.evaluator.pagination );
 };
 
 window.digirisk.evaluator.set_time = function(event) {
@@ -19,4 +20,22 @@ window.digirisk.evaluator.callback_edit_evaluator_assign_success = function( ele
 
 window.digirisk.evaluator.callback_detach_evaluator_success = function( element, response ) {
 	jQuery( '.wp-digi-list-evaluator' ).replaceWith( response.data.template );
+};
+
+window.digirisk.evaluator.pagination = function( event ) {
+	event.preventDefault();
+
+	jQuery( this ).closest( '.wp-digi-bloc-loader' ).addClass( 'wp-digi-bloc-loading' );
+
+	var href = jQuery( this ).attr( 'href' ).split( '&' );
+	var next_page = href[1].replace('current_page=', '');
+	var element_id = href[2].replace('element_id=', '');
+
+	var data = {
+		action: 'paginate_evaluator',
+		element_id: element_id,
+		next_page: next_page
+	};
+
+	jQuery( '.wp-digi-content' ).load( window.ajaxurl, data, function() {} );
 };
