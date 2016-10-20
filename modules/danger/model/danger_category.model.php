3cdb88de9d79c0b38f22c8f6e7f27f7b747f016d
Alexandre Techer <alexandre.techer@eoxia.com>
2016-10-20 14:20:48 +0200

    Ajout des champs manquants dans les catégories de danger

--- modules/danger/model/danger_category.model.php
+++ modules/danger/model/danger_category.model.php
@@ -6,6 +6,36 @@ class category_danger_model extends term_model {
 
 	public function __construct( $object, $children_wanted = array() ) {
 		$this->model = array_merge( $this->model, array(
+			'status' => array(
+				'export'			=> true,
+				'type' 				=> 'string',
+				'meta_type'		=> 'single',
+				'field'				=> '_wpdigi_status',
+				'bydefault'			=> '',
+			),
+			'unique_key' => array(
+				'type' 		=> 'string',
+				'meta_type'		=> 'single',
+				'field'		=> '_wpdigi_unique_key',
+				'bydefault'	=> '',
+			),
+			'unique_identifier' => array(
+				'type' 		=> 'string',
+				'meta_type' => 'multiple',
+				'bydefault'	=> '',
+			),
+			'thumbnail_id' => array(
+				'type' 		=> 'integer',
+				'meta_type'		=> 'single',
+				'field'		=> '_thumbnail_id',
+				'bydefault'	=> 0,
+			),
+			'position'	=> array(
+				'type'	=> 'integer',
+				'meta_type'		=> 'single',
+				'field'		=> '_position',
+				'bydefault'	=> 1,
+			),
 			'child' => array(
 				'danger'	=> array(
 					'export'			=> true,
