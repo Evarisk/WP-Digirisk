ea89313eec8de4a345d603b5342e6209a7722808
Alexandre Techer <alexandre.techer@eoxia.com>
2016-10-20 15:32:00 +0200

    Instanciation automatique pour les commentaires des evaluations des risques

--- modules/risk/class/risk_evaluation_comment.class.php
+++ modules/risk/class/risk_evaluation_comment.class.php
@@ -7,7 +7,7 @@ class risk_evaluation_comment_class extends comment_class {
 	protected $model_name   = '\digi\risk_evaluation_comment_model';
 	protected $meta_key     = '_wpdigi_risk_evaluation_comment';
 	protected $comment_type	= 'digi-riskevalcomment';
-	
+
 	protected $base					= 'digirisk/risk-evaluation-comment';
 	protected $version			= '0.1';
 
@@ -16,3 +16,5 @@ class risk_evaluation_comment_class extends comment_class {
 	}
 
 }
+
+risk_evaluation_comment_class::g();
