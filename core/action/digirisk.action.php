f20f2a60733b27a1418e3234e6a5637f833e1d95
eoxia-jimmy <jimmy.eoxia@gmail.com>
2016-10-26 17:36:31 +0200

    Mise à jour vers la norme de WordPress

--- core/action/digirisk.action.php
+++ core/action/digirisk.action.php
@@ -1,27 +1,28 @@
 <?php
+/**
+ * Initialise les fichiers .config.json
+ *
+ * @package Evarisk\Plugin
+ */
 
 namespace digi;
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
 /**
-* @TODO : A détailler
-*
-* @author Jimmy Latour <jimmy.latour@gmail.com>
-* @version 0.1
-* @copyright 2015-2016 Eoxia
-* @package core
-* @subpackage action
-*/
-
-if ( !defined( 'ABSPATH' ) ) exit;
-
-class digirisk_action {
+ * Initialise les scripts JS et CSS du Plugin
+ * Ainsi que le fichier MO
+ */
+class Digirisk_action {
 
 	/**
-	* Le constructeur ajoutes les actions WordPress suivantes:
-	* admin_enqueue_scripts (Pour appeller les scripts JS et CSS dans l'admin)
-	* admin_print_scripts (Pour appeler les scripts JS en bas du footer)
-	* plugins_loaded (Pour appeler le domaine de traduction)
-	*/
+	 * Le constructeur ajoutes les actions WordPress suivantes:
+	 * admin_enqueue_scripts (Pour appeller les scripts JS et CSS dans l'admin)
+	 * admin_print_scripts (Pour appeler les scripts JS en bas du footer)
+	 * plugins_loaded (Pour appeler le domaine de traduction)
+	 */
 	public function __construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'callback_before_admin_enqueue_scripts' ), 10 );
 		add_action( 'admin_enqueue_scripts', array( $this, 'callback_admin_enqueue_scripts' ), 11 );
@@ -30,6 +31,11 @@ class digirisk_action {
 		add_action( 'init', array( $this, 'callback_plugins_loaded' ) );
 	}
 
+	/**
+	 * Initialise les fichiers JS inclus dans WordPress (jQuery, wp.media et thickbox)
+	 *
+	 * @return void nothing
+	 */
 	public function callback_before_admin_enqueue_scripts() {
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_script( 'jquery-form' );
@@ -39,21 +45,32 @@ class digirisk_action {
 		add_thickbox();
 	}
 
+	/**
+	 * Initialise le fichier style.min.css et backend.min.js du plugin DigiRisk.
+	 *
+	 * @return void nothing
+	 */
 	public function callback_admin_enqueue_scripts() {
 		wp_register_style( 'digi-style', PLUGIN_DIGIRISK_URL . 'core/assets/css/style.min.css', array(), config_util::$init['digirisk']->version );
-	  wp_enqueue_style( 'digi-style' );
+		wp_enqueue_style( 'digi-style' );
 
 		wp_enqueue_script( 'digi-script', PLUGIN_DIGIRISK_URL . 'core/assets/js/backend.min.js', array(), config_util::$init['digirisk']->version, false );
 	}
 
+	/**
+	 * Initialise en php le fichier permettant la traduction des variables string JavaScript.
+	 *
+	 * @return void nothing
+	 */
 	public function callback_admin_print_scripts() {
 		require( PLUGIN_DIGIRISK_PATH . '/core/assets/js/define-string.js.php' );
 	}
+
 	/**
-	* Appelle le domaine de traduction
-	*/
+	 * Initialise le fichier MO
+	 */
 	public function callback_plugins_loaded() {
-		load_plugin_textdomain( "digirisk", false, PLUGIN_DIGIRISK_DIR . '/core/assets/languages/' );
+		load_plugin_textdomain( 'digirisk', false, PLUGIN_DIGIRISK_DIR . '/core/assets/languages/' );
 	}
 }
 
--- modules/risk/action/risk_save.action.php
+++ modules/risk/action/risk_save.action.php
@@ -9,61 +9,65 @@
 * @subpackage action
 */
 
-if ( !defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH') ) { exit;
+}
 
-class risk_save_action {
-	/**
-	* Le constructeur appelle la méthode personnalisé: save_risk
-	*/
-	public function __construct() {
-		add_action( 'save_risk', array( $this, 'callback_save_risk' ), 10, 1 );
-	}
+class risk_save_action
+{
+    /**
+    * Le constructeur appelle la méthode personnalisé: save_risk
+    */
+    public function __construct()
+    {
+        add_action('save_risk', array( $this, 'callback_save_risk' ), 10, 1);
+    }
 
-	/**
+    /**
   * Enregistres un risque.
-	* Ce callback est appelé après le callback callback_save_risk de risk_evaluation_action
+    * Ce callback est appelé après le callback callback_save_risk de risk_evaluation_action
   *
   * int $_POST['establishment']['establishment_id'] L'id de l'établissement
   * int $_POST['danger_id'] L'id du danger
-	*
-	* @param array $_POST Les données envoyées par le formulaire
+    *
+    * @param array $_POST Les données envoyées par le formulaire
   */
-	public function callback_save_risk( $risk ) {
-		$parent_id = !empty( $_POST['parent_id'] ) ? (int) $_POST['parent_id'] : 0;
+    public function callback_save_risk( $risk )
+    {
+        $parent_id = !empty($_POST['parent_id']) ? (int) $_POST['parent_id'] : 0;
 
-		if ( isset( $risk['id'] ) ) {
-			$danger = danger_class::g()->get( array( 'include' => $risk['danger_id'] ) );
-			$danger = $danger[0];
+        if (isset($risk['id']) ) {
+            $danger = danger_class::g()->get(array( 'include' => $risk['danger_id'] ));
+            $danger = $danger[0];
 
-			$image_id = 0;
+            $image_id = 0;
 
-			if ( !empty( $risk['associated_document_id'] ) ) {
-				$image_id = $risk['associated_document_id']['image'][0];
-			}
+            if (!empty($risk['associated_document_id']) ) {
+                $image_id = $risk['associated_document_id']['image'][0];
+            }
 
-			$risk['title'] = $danger->name;
-			$risk['parent_id'] = $parent_id;
-			$risk['taxonomy']['digi-danger'][] = $danger->id;
-			$risk['taxonomy']['digi-danger-category'][] = $danger->parent_id;
-			$risk_obj = risk_class::g()->update( $risk );
+            $risk['title'] = $danger->name;
+            $risk['parent_id'] = $parent_id;
+            $risk['taxonomy']['digi-danger'][] = $danger->id;
+            $risk['taxonomy']['digi-danger-category'][] = $danger->parent_id;
+            $risk_obj = risk_class::g()->update($risk);
 
-			if ( !$risk_obj ) {
-				wp_send_json_error();
-			}
+            if (!$risk_obj ) {
+                wp_send_json_error();
+            }
 
-			$risk_evaluation = risk_evaluation_class::g()->update( array( 'id' => $risk_obj->current_evaluation_id, 'post_id' => $risk_obj->id ) );
+            $risk_evaluation = risk_evaluation_class::g()->update(array( 'id' => $risk_obj->current_evaluation_id, 'post_id' => $risk_obj->id ));
 
-			if ( !$risk_evaluation ) {
-				wp_send_json_error();
-			}
+            if (!$risk_evaluation ) {
+                wp_send_json_error();
+            }
 
-			if ( !empty( $image_id ) ) {
-				file_management_class::g()->associate_file( $image_id, $risk_obj->id, 'risk_class' );
-			}
-		}
+            if (!empty($image_id) ) {
+                file_management_class::g()->associate_file($image_id, $risk_obj->id, 'risk_class');
+            }
+        }
 
-		do_action( 'save_risk_evaluation_comment', $risk_obj, $risk );
-	}
+        do_action('save_risk_evaluation_comment', $risk_obj, $risk);
+    }
 }
 
 new risk_save_action();
