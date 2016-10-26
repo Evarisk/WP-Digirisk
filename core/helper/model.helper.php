c4aca4b19d56de667e19575588e438bf775bfd1d
eoxia-jimmy <jimmy.eoxia@gmail.com>
2016-10-26 17:47:16 +0200

    Mise à la norme

--- core/helper/model.helper.php
+++ core/helper/model.helper.php
@@ -1,15 +1,28 @@
 <?php
+/**
+ * Les fonctions helpers des modèles
+ *
+ * @package Evarisk\Plugin
+ */
 
 namespace digi;
 
-if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
+/**
+ * Construit l'identifiant unique d'un modèle
+ *
+ * @param  object $data Les données du modèle.
+ * @return object       Les données du modèle avec l'identifiant
+ */
 function construct_identifier( $data ) {
 	$model_name = get_class( $data );
 	$controller_name = str_replace( 'model', 'class', $model_name );
 	$next_identifier = common_util::get_last_unique_key( $controller_name );
 	if ( empty( $data->unique_key ) ) {
-		$data->unique_key = (int)( $next_identifier + 1 );
+		$data->unique_key = (int) ( $next_identifier + 1 );
 	}
 
 	if ( empty( $data->unique_identifier ) ) {
@@ -19,6 +32,12 @@ function construct_identifier( $data ) {
 	return $data;
 }
 
+/**
+ * Remplaces l'identifiant du modèle par l'identifiant personnalisé qui se trouve dans la BDD
+ *
+ * @param  object $data Les données du modèle.
+ * @return object       Les données du modèle avec l'identifiant personnalisé
+ */
 function get_identifier( $data ) {
 	$list_accronym = get_option( config_util::$init['digirisk']->accronym_option );
 	$list_accronym = json_decode( $list_accronym, true );
@@ -26,15 +45,21 @@ function get_identifier( $data ) {
 	$controller_name = str_replace( 'model', 'class', $model_name );
 	$element_prefix = $controller_name::g()->element_prefix;
 
-	if ( !empty( $data->unique_identifier ) && !empty( $list_accronym[$element_prefix] ) ) {
-		$data->unique_identifier = str_replace( $element_prefix, $list_accronym[$element_prefix]['to'], $data->unique_identifier );
+	if ( ! empty( $data->unique_identifier ) && ! empty( $list_accronym[ $element_prefix ] ) ) {
+		$data->unique_identifier = str_replace( $element_prefix, $list_accronym[ $element_prefix ]['to'], $data->unique_identifier );
 	}
 
 	return $data;
 }
 
+/**
+ * Convertie la date au format français dd/mm/yy en format SQL
+ *
+ * @param  object $data Les donnnées du modèle.
+ * @return object       Les donnnées du modèle avec la date au format SQL
+ */
 function convert_date( $data ) {
-	if ( !empty( $data ) && !empty( $data->date ) ) {
+	if ( ! empty( $data ) && ! empty( $data->date ) ) {
 		$data->date = date( 'Y-m-d', strtotime( str_replace( '/', '-', $data->date ) ) );
 	}
 
@@ -42,24 +67,23 @@ function convert_date( $data ) {
 }
 
 /**
- * Construit les initiales d'un utilisateurs donné / Build initial for a given user
+ * Construit les initiales des utilisateurs
  *
- * @param object $user Les données de l'utilisateur courant / Current user data
- *
- * @return string Les initiales de l'utilisateur courant / Current user initial
+ * @param  \digi\User_class $user Les données de l'utilisateur.
+ * @return \digi\User_class       Les données de l'utilisateur avec les intiales
  */
-function build_user_initial( $user ){
+function build_user_initial( $user ) {
 	$initial = '';
 
-	if ( !empty( $user->firstname ) ) {
+	if ( ! empty( $user->firstname ) ) {
 		$initial .= substr( $user->firstname, 0, 1 );
 	}
-	if ( !empty( $user->lastname ) ) {
+	if ( ! empty( $user->lastname ) ) {
 		$initial .= substr( $user->lastname, 0, 1 );
 	}
 
 	if ( empty( $initial ) ) {
-		if ( !empty( $user->login ) ) {
+		if ( ! empty( $user->login ) ) {
 			$initial .= substr( $user->login, 0, 1 );
 		}
 	}
@@ -69,6 +93,12 @@ function build_user_initial( $user ){
 	return $user;
 }
 
+/**
+ * Initialise le tableau avec les couleurs possibles pour l'utilisateur
+ *
+ * @param  \digi\User_class $user Les données de l'utilisateur.
+ * @return \digi\User_class       Les données de l'utilisateur avec la couleur.
+ */
 function build_avatar_color( $user ) {
 	$avatar_color = array(
 		'e9ad4f',
@@ -79,9 +109,7 @@ function build_avatar_color( $user ) {
 		'734fe9',
 	);
 
-	$user->avatar_color = $avatar_color[array_rand( $avatar_color, 1 )];
+	$user->avatar_color = $avatar_color[ array_rand( $avatar_color, 1 ) ];
 
 	return $user;
 }
-
-?>
