06145bf613efac86230b526872f7d7f980c744a0
eoxia-jimmy <jimmy.eoxia@gmail.com>
2016-11-04 14:35:16 +0100

    Standard WordPress

--- modules/group/view/list.view.php
+++ modules/group/view/list.view.php
@@ -1,9 +1,11 @@
 <?php namespace digi;
 
- if ( !defined( 'ABSPATH' ) ) exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
-if ( !empty( $list_groupment ) ):
-	foreach( $list_groupment as $key => $groupment ):
+if ( ! empty( $list_groupment ) ) :
+	foreach ( $list_groupment as $key => $groupment ) :
 		?>
 		<ul class="<?php echo !empty( $groupment->parent_id ) ? 'sub-menu': 'parent'; ?>">
 			<li data-id="<?php echo $groupment->id; ?>">
@@ -14,7 +16,7 @@ if ( !empty( $list_groupment ) ):
 						class="action-attribute"><?php echo $groupment->unique_identifier . ' - ' . $groupment->title; ?></span>
 
 					<span class="wp-digi-new-group-action">
-						<?php if ( empty( $groupment->list_workunit) ): ?>
+						<?php if ( empty( $groupment->list_workunit ) ): ?>
 							<a
 								data-parent-id="<?php echo $groupment->id; ?>"
 								data-action="create_group"
