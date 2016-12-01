cb5583d6d4414c231e1410fa5f5542cb1151c984
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-12-01 14:49:55 +0100

    MAJ

--- modules/comment/view/item-edit.view.php
+++ modules/comment/view/item-edit.view.php
@@ -7,22 +7,24 @@ if ( !defined( 'ABSPATH' ) ) exit;
 	<?php
 	$author_id = !empty( $comment->author_id ) ? $comment->author_id : get_current_user_id();
 	$userdata = get_userdata( $author_id );
-	echo !empty( $userdata->display_name ) ? $userdata->display_name : '';
 	?>
+	<span><?php echo !empty( $userdata->display_name ) ? $userdata->display_name : ''; ?></span>
 	<input type="hidden" name="list_comment[<?php echo $comment->id; ?>][author_id]" value="<?php echo $author_id; ?>" />
 	<input type="hidden" name="list_comment[<?php echo $comment->id; ?>][id]" value="<?php echo $comment->id; ?>" />
 	<span class="wp-digi-risk-date padded">
 		<input type="text" class="eva-date" name="list_comment[<?php echo $comment->id; ?>][date]" value="<?php echo $comment->date; ?>" />
 	</span>
 	<span class="wp-digi-risk-comment padded">
-		<textarea name="list_comment[<?php echo $comment->id; ?>][content]"><?php echo $comment->content; ?></textarea>
+		<textarea rows="1" name="list_comment[<?php echo $comment->id; ?>][content]"><?php echo $comment->content; ?></textarea>
 	</span>
 
 	<?php if ($comment->id > 0): ?>
-		<a href="#"
-			data-id="<?php echo $comment->id; ?>"
-			data-nonce="<?php echo wp_create_nonce( 'ajax_delete_comment_' . $comment->id ); ?>"
-			data-action="delete_comment"
-			class="wp-digi-action wp-digi-action-delete dashicons dashicons-no-alt" ></a>
+		<span>
+			<a href="#"
+				data-id="<?php echo $comment->id; ?>"
+				data-nonce="<?php echo wp_create_nonce( 'ajax_delete_comment_' . $comment->id ); ?>"
+				data-action="delete_comment"
+				class="wp-digi-action wp-digi-action-delete dashicons dashicons-no-alt" ></a>
+		</span>
 	<?php endif; ?>
 </li>
