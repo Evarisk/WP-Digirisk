54a7b0ee80486a98e31d895f0a89719048ba51cc
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2016-12-01 09:47:19 +0100

    Gestion des modèles personnalisés indisponibles

--- modules/handle_model/view/main.view.php
+++ modules/handle_model/view/main.view.php
@@ -2,20 +2,4 @@
 
 if ( !defined( 'ABSPATH' ) ) exit;
 
-if ( !empty( $list_type_document ) ):
-	foreach ( $list_type_document as $key => $title ):
-		?>
-		<div class="block">
-			<div>
-				<h3><?php echo $title; ?></h3>
-				<div class="wp-digi-bton-first upload">
-					<?php do_shortcode( '[eo_upload_button action="eo_set_model" type="' . $key . '"]' ); ?>
-					<span>Envoyer votre modèle personnalisé</span>
-				</div>
-				<a href="<?php echo $list_document_default[$key]['model_url']; ?>" class="wp-digi-bton-second"><i class="dashicons-download dashicons"></i>Télécharger le modèle courant</a>
-			</div>
-		</div>
-		<?php
-	endforeach;
-endif;
-?>
+esc_html_e( 'Indisponible', 'digirisk' );
