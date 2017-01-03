3977b3224768743673382037dbe458ba2e74f47b
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-03 15:41:03 +0100

    Mise à jour du main header

--- core/assets/js/lib/toggle.backend.js
+++ core/assets/js/lib/toggle.backend.js
@@ -5,22 +5,27 @@ window.digirisk.toggle.init = function() {
 };
 
 window.digirisk.toggle.event = function() {
-  jQuery( document ).on( 'click', 'toggle', window.digirisk.toggle.open );
-  // jQuery( document ).on( 'click', 'body', window.digirisk.toggle.close );
+	jQuery( document ).on( 'click', '.toggle', window.digirisk.toggle.open );
+	jQuery( document ).on( 'click', 'body', window.digirisk.toggle.close );
 };
 
 window.digirisk.toggle.open = function( event ) {
 	var target = undefined;
-  // Récupères la box de destination mis dans l'attribut du toggle
-  if ( jQuery( this ).data( 'parent' ) ) {
-	  target = jQuery( this ).closest( '.' + jQuery( this ).data( 'parent' ) ).find( "." + jQuery( this ).data( 'target' ) );
-	}
-	else {
-		target = jQuery( "." + jQuery( this ).data( 'target' ) );
+
+	// Récupères la box de destination mis dans l'attribut du toggle
+	if ( jQuery( this ).data( 'parent' ) ) {
+		target = jQuery( this ).closest( '.' + jQuery( this ).data( 'parent' ) ).find( '.' + jQuery( this ).data( 'target' ) );
+	} else {
+		target = jQuery( '.' + jQuery( this ).data( 'target' ) );
 	}
 
 	if ( target ) {
-	  target.toggle();
-	  event.stopPropagation();
+		target.toggle();
+		event.stopPropagation();
 	}
+};
+
+window.digirisk.toggle.close = function( event ) {
+	jQuery( '.digi-toggle' ).hide();
+	event.stopPropagation();
 };
--- core/util/view.util.php
+++ core/util/view.util.php
@@ -28,12 +28,13 @@ class View_util extends Singleton_util {
 	/**
 	 * Appelle la vue avec les paramètres et calcule automatiquement les MicroSeconds
 	 *
-	 * @param  string $module_name           Le nom du module.
-	 * @param  string $view_path_without_ext Le chemin vers le fichier à partir du dossier "view" du module.
-	 * @param  array  $args                  Les données à transmettre à la vue.
+	 * @param  string  $module_name           Le nom du module.
+	 * @param  string  $view_path_without_ext Le chemin vers le fichier à partir du dossier "view" du module.
+	 * @param  array   $args                  Les données à transmettre à la vue.
+	 * @param  boolean $filter                Est ce qu'on utilise apply_fiters pour cette vue ou pas.
 	 * @return void                        	 nothing
 	 */
-	public static function exec( $module_name, $view_path_without_ext, $args = array() ) {
+	public static function exec( $module_name, $view_path_without_ext, $args = array(), $filter = true ) {
 		$path_to_view = PLUGIN_DIGIRISK_PATH . 'modules/' . $module_name . '/view/' . $view_path_without_ext . '.view.php';
 
 		log_class::g()->start_ms( 'view_util_exec' );
@@ -42,7 +43,10 @@ class View_util extends Singleton_util {
 			log_class::g()->exec( 'digi_view', 'view_util_exec', 'Impossible de charger la vue : ' . $path_to_view, $args, 2 );
 		}
 
-		$args = apply_filters( $module_name . '_' . $view_path_without_ext, $args, $module_name, $view_path_without_ext );
+		if ( $filter ) {
+			$args = apply_filters( $module_name . '_' . $view_path_without_ext, $args, $module_name, $view_path_without_ext );
+		}
+
 		extract( $args );
 		require( $path_to_view );
 		log_class::g()->exec( 'view_util_exec', 'view_util_exec', 'Chargement de la vue : ' . $path_to_view, $args );
