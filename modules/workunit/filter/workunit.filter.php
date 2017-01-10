8966785427a7ce636a142e130a3ffff2365fbfd7
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-10 11:32:07 +0100

    Mise à la norme de WordPress

--- modules/workunit/filter/workunit.filter.php
+++ modules/workunit/filter/workunit.filter.php
@@ -22,11 +22,11 @@ class Workunit_Filter {
 	 * @version 6.2.2.0
 	 */
 	public function __construct() {
-		add_filter( 'digi_tab', array( $this, 'callback_digi_tab' ), 2, 2 );
+		add_filter( 'digi_tab', array( $this, 'callback_digi_tab' ), 5, 2 );
 	}
 
 	/**
-	 * Ajoutes l'onglet Configuration aux unités de travail
+	 * Ajoutes les onglets "Configuration" et "Supprimer" aux unités de travail
 	 *
 	 * @since 6.2.2.0
 	 * @version 6.2.4.0
@@ -37,14 +37,22 @@ class Workunit_Filter {
 	 * @return array
 	 */
 	function callback_digi_tab( $tab_list, $id ) {
-		$tab_list['digi-workunit']['more']['configuration'] = array(
-			'type' => 'text',
-			'text' => __( 'Configuration', 'digirisk' ),
-		);
-
-		$tab_list['digi-workunit']['more']['delete'] = array(
-			'type' => 'text',
-			'text' => __( 'Supprimer', 'digirisk' ),
+		$tab_list['digi-workunit']['more'] = array(
+			'type' => 'toggle',
+			'text' => '<i class="action fa fa-ellipsis-v toggle"></i>',
+			'items' => array(
+				'configuration' => array(
+					'type' => 'text',
+					'text' => __( 'Configuration', 'digirisk' ),
+				),
+				'delete' => array(
+					'type' => 'text',
+					'text' => __( 'Supprimer', 'digirisk' ),
+					'class' => 'action-delete',
+					'attributes' => 'data-action=delete_society data-id=' . $id . '',
+					'nonce' => wp_create_nonce( 'delete_society' ),
+				),
+			),
 		);
 
 		return $tab_list;
