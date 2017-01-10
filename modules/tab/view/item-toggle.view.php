637d6309d2b429e34cf8c4c1764c671c8cc5f96b
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-10 11:18:57 +0100

    Ajout de l'action tab-element sur les élements du toggle

--- modules/tab/view/item-toggle.view.php
+++ modules/tab/view/item-toggle.view.php
@@ -17,14 +17,17 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 <li class="tab-element toggle option" data-parent="toggle" data-target="content">
 	<i class="action fa fa-ellipsis-v toggle"></i>
 	<ul class="content">
-		<?php
-		foreach ( $element['items'] as $key => $sub_element ) :
-			?><li
-				<?php echo 'class="item ' . ( ! empty( $sub_element['class'] ) ? esc_attr( $sub_element['class'] ) : '' ); ?>"
-				<?php echo ! empty( $sub_element['nonce'] ) ? 'data-nonce="' . esc_attr( $sub_element['nonce'] ) . '"' : ''; ?>
-				<?php echo ! empty( $sub_element['attributes'] ) ? esc_attr( $sub_element['attributes'] ) : ''; ?>
-				><?php echo esc_html( $sub_element['text'] ); ?></li><?php
-		endforeach;
-		?>
+		<?php foreach ( $element['items'] as $key => $sub_element ) : ?>
+			<li class="tab-element"
+					data-action="digi-<?php echo esc_attr( $key ); ?>"
+					data-nonce="<?php echo esc_attr( wp_create_nonce( 'load_content' ) ); ?>"
+					<?php echo ! empty( $sub_element['title'] ) ? 'data-title="' . $sub_element['title'] . '"' : ''; // no esc_attr is okay. ?>
+					<?php echo ! empty( $sub_element['nonce'] ) ? 'data-nonce="' . esc_attr( $sub_element['nonce'] ) . '"' : ''; ?>
+					<?php echo ! empty( $sub_element['attributes'] ) ? esc_attr( $sub_element['attributes'] ) : ''; ?>
+					data-id="<?php echo esc_attr( $id ); ?>">
+				<span <?php echo ! empty( $sub_element['class'] ) ? 'class="' . esc_attr( $sub_element['class'] ) . '"' : ''; ?>
+					><?php echo $sub_element['text']; ?></span> <!-- no esc_html is okay. -->
+			</li>
+		<?php endforeach; ?>
 	</ul>
 </li>
