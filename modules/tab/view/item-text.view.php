2ffe925557db6244ad3eabeb71a245d1809f1972
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-10 10:09:17 +0100

    Vérifie si l'index title existe

--- modules/tab/view/item-text.view.php
+++ modules/tab/view/item-text.view.php
@@ -17,7 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 <li class="tab-element"
 		data-action="digi-<?php echo esc_attr( $key ); ?>"
 		data-nonce="<?php echo esc_attr( wp_create_nonce( 'load_content' ) ); ?>"
-		data-title="<?php echo esc_attr( $element['title'] ); ?>"
+		<?php echo esc_attr( ! empty( $element['title'] ) ? 'data-title="' . $element['title'] . '"' : '' ); ?>
 		data-id="<?php echo esc_attr( $id ); ?>">
 	<span <?php echo ! empty( $element['class'] ) ? 'class="' . esc_attr( $element['class'] ) . '"' : ''; ?>
 		><?php echo $element['text']; ?></span> <!-- no esc_html is okay. -->
