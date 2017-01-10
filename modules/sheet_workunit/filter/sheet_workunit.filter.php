1e3360970bf20fe70f036bbbea803e6312361862
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-10 11:31:15 +0100

    Mise à la norme de WordPress

--- modules/sheet_workunit/filter/sheet_workunit.filter.php
+++ modules/sheet_workunit/filter/sheet_workunit.filter.php
@@ -1,8 +1,13 @@
 <?php
 /**
- * Ajoutes le filtre pour ajouter le bouton dans le contenu des onglets
+ * Les filtres relatifs aux fiches de poste
  *
- * @package Evarisk\Plugin
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 0.1
+ * @version 6.2.4.0
+ * @copyright 2015-2017 Evarisk
+ * @package sheet_workunit
+ * @subpackage filter
  */
 
 namespace digi;
@@ -10,24 +15,30 @@ namespace digi;
 if ( ! defined( 'ABSPATH' ) ) { exit; }
 
 /**
- * Ajoutes le filtre pour ajouter le bouton dans le contenu des onglets
+ * Les filtres relatifs aux fiches de poste
  */
 class Sheet_Workunit_Filter {
 
 	/**
-	 * Ajoutes le filtres
+	 * Ajoutes le filtre "digi_tab"
 	 *
-	 * @see add_filter
+	 * @since 0.1
+	 * @version 6.2.4.0
 	 */
 	public function __construct() {
-		add_filter( 'digi_tab', array( $this, 'callback_digi_tab' ), 16, 2 );
+		add_filter( 'digi_tab', array( $this, 'callback_digi_tab' ), 6, 2 );
 	}
 
 	/**
-	 * Ajoutes une entrée dans le tableau $list_tab
+	 * Ajoutes l'onglet "Fiche de poste"" aux unités de travail
 	 *
-	 * @param  array $list_tab  La liste des filtres.
-	 * @return array            La liste des filtres + le filtre ajouté par cette méthode.
+	 * @since 6.2.2.0
+	 * @version 6.2.4.0
+	 *
+	 * @param  array   $list_tab La liste des filtres.
+	 * @param  integer $id L'ID de la société.
+	 *
+	 * @return array
 	 */
 	public function callback_digi_tab( $list_tab, $id ) {
 		$list_tab['digi-workunit']['fiche-de-poste'] = array(
