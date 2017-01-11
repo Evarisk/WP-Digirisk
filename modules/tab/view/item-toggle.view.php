689ccbb2b44a4f3c4c96613d668ab8c21f3c1b64
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-11 10:47:48 +0100

    Ajout de parent_class et action

--- modules/tab/view/item-toggle.view.php
+++ modules/tab/view/item-toggle.view.php
@@ -18,15 +18,15 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 	<i class="action fa fa-ellipsis-v toggle"></i>
 	<ul class="content">
 		<?php foreach ( $element['items'] as $key => $sub_element ) : ?>
-			<li class="tab-element"
-					data-action="digi-<?php echo esc_attr( $key ); ?>"
+			<li class="tab-element <?php echo esc_attr( ! empty( $sub_element['parent_class'] ) ? $sub_element['parent_class'] : '' ); ?>"
+					data-action="<?php echo esc_attr( ! empty( $sub_element['action'] ) ? $sub_element['action'] : 'digi-' . esc_attr( $key ) ); ?>"
 					data-nonce="<?php echo esc_attr( wp_create_nonce( 'load_content' ) ); ?>"
-					<?php echo ! empty( $sub_element['title'] ) ? 'data-title="' . $sub_element['title'] . '"' : ''; // no esc_attr is okay. ?>
+					<?php echo ! empty( $sub_element['title'] ) ? 'data-title="' . $sub_element['title'] . '"' : ''; // WPCS: XSS ok. ?>
 					<?php echo ! empty( $sub_element['nonce'] ) ? 'data-nonce="' . esc_attr( $sub_element['nonce'] ) . '"' : ''; ?>
 					<?php echo ! empty( $sub_element['attributes'] ) ? esc_attr( $sub_element['attributes'] ) : ''; ?>
 					data-id="<?php echo esc_attr( $id ); ?>">
 				<span <?php echo ! empty( $sub_element['class'] ) ? 'class="' . esc_attr( $sub_element['class'] ) . '"' : ''; ?>
-					><?php echo $sub_element['text']; ?></span> <!-- no esc_html is okay. -->
+					><?php echo $sub_element['text']; // WPCS: XSS ok. ?></span>
 			</li>
 		<?php endforeach; ?>
 	</ul>
