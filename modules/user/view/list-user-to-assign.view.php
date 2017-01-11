130c403977ad8a7ae4f1c3f53ae591eb1c210c5c
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-11 09:45:07 +0100

    Refactorisation

--- modules/user/view/list-user-to-assign.view.php
+++ modules/user/view/list-user-to-assign.view.php
@@ -1,58 +1,53 @@
-<?php namespace digi;
+<?php
+/**
+ * Le tableau des utilisateurs qui peuvent être affecté.
+ *
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.2.3.0
+ * @version 6.2.4.0
+ * @copyright 2015-2017 Evarisk
+ * @package user
+ * @subpackage view
+ */
 
-if ( !defined( 'ABSPATH' ) ) exit; ?>
+namespace digi;
 
-<form class="wp-form-user-to-assign wp-digi-bloc-loader" method="POST" action="<?php echo admin_url( 'admin-ajax.php' ); ?>">
-	<ul class="wp-digi-list wp-digi-table">
-		<li class="wp-digi-table-header" >
-			<span></span>
-			<span><?php _e('ID', 'digirisk'); ?></span>
-			<span><?php _e('Lastname', 'digirisk'); ?></span>
-			<span><?php _e('Firtname', 'digirisk'); ?></span>
-			<span><?php _e('Hiring date', 'digirisk'); ?></span>
-			<span><?php _e('Assign', 'digirisk'); ?></span>
-		</li>
+if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 
-		<?php
-		if ( !empty( $list_user_to_assign ) ):
-			foreach ( $list_user_to_assign as $user_to_assign ):
-				?>
-				<li class="wp-digi-list-item">
-					<span class="wp-avatar" style="background: #<?php echo $user_to_assign->avatar_color; ?>;" ><?php echo $user_to_assign->initial; ?></span>
-					<span><strong>U<?php echo $user_to_assign->id; ?></strong></span>
-					<span><?php echo $user_to_assign->lastname; ?></span>
-					<span><?php echo $user_to_assign->firstname; ?></span>
-					<span><input type="text" class="eva-date" name="list_user[<?php echo $user_to_assign->id; ?>][on]" value="<?php echo date( 'd/m/Y', strtotime( $user_to_assign->hiring_date ) ); ?>" /></span>
-					<span><input type="checkbox" <?php echo in_array( $user_to_assign->id, !empty( $list_affected_id ) ? $list_affected_id : array() ) ? 'disabled="disabled";' : '';?> name="list_user[<?php echo $user_to_assign->id; ?>][affect]" /></span>
-				</li>
-				<?php
-			endforeach;
-		endif;
-		?>
-	</ul>
+<form method="POST" class="form-edit-user-assign" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>">
 
-	<input type="hidden" name="workunit_id" value="<?php echo $workunit->id; ?>" />
-	<input type="hidden" name="group_id" value="<?php echo $workunit->parent_id; ?>" />
+	<table class="table users">
+		<thead>
+			<tr>
+				<th></th>
+				<th class="padding"><?php esc_html_e( 'ID', 'digirisk' ); ?></th>
+				<th class="padding"><?php esc_html_e( 'Nom', 'digirisk' ); ?></th>
+				<th class="padding"><?php esc_html_e( 'Prénom', 'digirisk' ); ?></th>
+				<th><?php esc_html_e( 'Date d\'embauche', 'digirisk' ); ?></th>
+				<th><?php esc_html_e( 'Affecter', 'digirisk' ); ?></th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php if ( ! empty( $users ) ) : ?>
+				<?php foreach ( $users as $user ) : ?>
+					<tr>
+						<td><div class="avatar" style="background-color: #<?php echo esc_attr( $user->avatar_color ); ?>;"><span><?php echo esc_html( $user->initial ); ?></span></div></td>
+						<td class="padding"><span><strong><?php echo esc_html( User_Class::g()->element_prefix . $user->id ); ?><strong></span></td>
+						<td class="padding"><span><?php echo esc_html( $user->lastname ); ?></span></td>
+						<td class="padding"><span><?php echo esc_html( $user->firstname ); ?></span></td>
+						<td><input type="text" class="date" name="list_user[<?php echo esc_attr( $user->id ); ?>][on]" value="<?php echo esc_attr( date( 'd/m/Y', strtotime( $user->hiring_date ) ) ); ?>"></td>
+						<td><input <?php echo esc_attr( in_array( $user->id, ! empty( $list_affected_id ) ? $list_affected_id : array(), true ) ? 'disabled="disabled";' : '' ); ?> type="checkbox" name="list_user[<?php echo esc_attr( $user->id ); ?>][affect]"></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</tbody>
+	</table>
+
+	<?php wp_nonce_field( 'edit_user_assign' ); ?>
+	<input type="hidden" name="workunit_id" value="<?php echo esc_attr( $workunit->id ); ?>" />
+	<input type="hidden" name="group_id" value="<?php echo esc_attr( $workunit->parent_id ); ?>" />
 	<input type="hidden" name="action" value="edit_user_assign" />
-	<input type="submit" class="wp-digi-bton-fourth float right" value="<?php _e('Update', 'digirisk'); ?>" />
-
-	<!-- Pagination -->
-	<?php if ( !empty( $current_page ) && !empty( $number_page ) ): ?>
-		<div class="wp-digi-pagination">
-			<?php
-			$big = 999999999;
-			echo paginate_links( array(
-				'base' => admin_url( 'admin-ajax.php?action=paginate_user&current_page=%_%&element_id=' . $workunit->id ),
-				'format' => '%#%',
-				'current' => $current_page,
-				'total' => $number_page,
-				'before_page_number' => '<span class="screen-reader-text">'. __( 'Page', 'digirisk' ) .' </span>',
-				'type' => 'plain',
-				'next_text' => '<i class="dashicons dashicons-arrow-right"></i>',
-				'prev_text' => '<i class="dashicons dashicons-arrow-left"></i>'
-			) );
-			?>
-		</div>
-	<?php endif; ?>
+	<div class="button green uppercase strong float right margin submit-form"><span><?php esc_html_e( 'Mettre à jour', 'digirisk' ); ?></span></div>
 
 </form>
