7c024d1886a2580fa376982eaa3bde3c29c65003
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-11 11:02:40 +0100

    Ajout de commentaire

--- modules/society/asset/js/society.backend.js
+++ modules/society/asset/js/society.backend.js
@@ -8,7 +8,18 @@ window.digirisk.society.event = function() {
 	jQuery( document ).on( 'keyup', 'input[name="title"]', window.digirisk.society.display_save_button );
 };
 
-window.digirisk.society.callback_load_society = function( element, response ) {
+/**
+ * Callback en cas de réussite de la requête Ajax "load_society"
+ * Remplaces le template principale de l'application avec le template reçu dans la réponse de la requête Ajax.
+ *
+ * @param  {HTMLSpanElement} triggeredElement   L'élement HTML déclenchant la requête Ajax.
+ * @param  {Object}        response          Les données renvoyées par la requête Ajax.
+ * @return {void}
+ *
+ * @since 0.1
+ * @version 6.2.4.0
+ */
+window.digirisk.society.loadedSocietySuccess = function( element, response ) {
 	jQuery( '.digirisk-wrap' ).replaceWith( response.data.template );
 };
 
@@ -16,7 +27,7 @@ window.digirisk.society.display_save_button = function( event ) {
 	jQuery( this ).closest( '.wp-digi-global-sheet-header' ).find( ".wp-digi-global-action-container" ).removeClass( "hidden" );
 	jQuery( this ).addClass( "active" );
 	jQuery( '.wp-digi-group-action-container .wp-digi-bton-fourth' ).text( 'Enregistrer' );
-}
+};
 
 window.digirisk.society.update_society_success = function( element, response ) {
 	jQuery( element ).closest( '.wp-digi-global-sheet-header' ).find( '.wp-digi-global-action-container' ).addClass( 'hidden' );
@@ -28,8 +39,19 @@ window.digirisk.society.update_society_success = function( element, response ) {
 	else {
 		jQuery( '.wp-digi-societytree-left-container span[data-workunit-id="' + response.data.society.id + '"]' ).text( response.data.society.unique_identifier + ' - ' + response.data.society.title );
 	}
-}
+};
 
-window.digirisk.society.delete_success = function( event, response ) {
-	jQuery( '.wp-digi-societytree-main-container' ).replaceWith( response.data.template );
-}
+/**
+ * Callback en cas de réussite de la requête Ajax "delete_society"
+ * Remplaces le template principale de l'application avec le template reçu dans la réponse de la requête Ajax.
+ *
+ * @param  {HTMLLiElement} triggeredElement   L'élement HTML déclenchant la requête Ajax.
+ * @param  {Object}        response          Les données renvoyées par la requête Ajax.
+ * @return {void}
+ *
+ * @since 0.1
+ * @version 6.2.4.0
+ */
+window.digirisk.society.deletedSocietySuccess = function( triggeredElement, response ) {
+	jQuery( '.digirisk-wrap' ).replaceWith( response.data.template );
+};
