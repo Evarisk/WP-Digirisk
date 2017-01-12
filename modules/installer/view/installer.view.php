adf2e10ffd70db76272751ed58d400bc61bc5eda
amandine-eoxia <amandine@eoxia.com>
2017-01-12 17:26:34 +0100

    Arrangement de la page installer

--- modules/installer/view/installer.view.php
+++ modules/installer/view/installer.view.php
@@ -15,14 +15,14 @@ if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
 <div class="wpdigi-installer digirisk-wrap">
 	<h2><?php esc_html_e( 'Digirisk', 'digirisk' ); ?></h2>
 
-	<ul class="step">
-		<li class="step-create-society active"><span><?php esc_html_e( 'Votre société', 'digirisk' ); ?></span><i class="circle">1</i></li>
-		<li class="step-create-components"><span><?php esc_html_e( 'Composants', 'digirisk' ); ?></span><i class="circle">2</i></li>
-		<li class="step-create-users"><span><?php esc_html_e( 'Votre personnel', 'digirisk' ); ?></span><i class="circle">3</i></li>
+	<ul class="step-list">
+		<li class="step step-create-society active"><span class="title"><?php esc_html_e( 'Votre société', 'digirisk' ); ?></span><i class="circle">1</i></li>
+		<li class="step step-create-components"><span class="title"><?php esc_html_e( 'Composants', 'digirisk' ); ?></span><i class="circle">2</i></li>
+		<li class="step step-create-users"><span class="title"><?php esc_html_e( 'Votre personnel', 'digirisk' ); ?></span><i class="circle">3</i></li>
 	</ul>
 
-	<div>
-		<h3><?php esc_html_e( 'Votre société', 'digirisk' );?></h3>
+	<div class="main-content">
+		<h2><?php esc_html_e( 'Votre société', 'digirisk' );?></h2>
 
 		<form method="POST" class="form" action="<?php echo esc_attr( admin_url( 'admin-ajax.php' ) ); ?>">
 			<input type="hidden" name="action" value="installer_save_society" />
@@ -38,68 +38,63 @@ if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
 				</div>
 			</div>
 
-			<ul class="grid-layout w2">
-				<li class="form-element">
-					<input type="text" name="address[address]" />
-					<label><?php esc_html_e( 'Address', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text"
-								data-field="groupment[user_info][owner_id]"
-								data-type="user"
-								placeholder=""
-								class="digi-search"
-								value="<?php echo ! empty( $owner_user->id ) ? esc_attr( User_Digi_Class::g()->element_prefix . $owner_user->id . ' - ' . $owner_user->displayname ) : ''; ?>" />
-					<label><?php esc_html_e( 'Owner', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-					<input type="hidden" name="groupment[user_info][owner_id]" />
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="address[additional_address]" />
-					<label><?php esc_html_e( 'Additional address', 'digirisk' ); ?> </label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" class="eva-date" name="groupment[date]" value="<?php echo esc_attr( date( 'd/m/Y' ) ); ?>" />
-					<label><?php esc_html_e( 'Created date', 'digirisk' ); ?> </label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="address[postcode]" />
-					<label><?php esc_html_e( 'Postcode', 'digirisk' ); ?> </label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="groupment[identity][siren]" />
-					<label><?php esc_html_e( 'SIREN', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="address[town]" />
-					<label><?php esc_html_e( 'Town', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="groupment[identity][siret]" />
-					<label><?php esc_html_e( 'SIRET', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</li>
-
-				<li class="form-element">
-					<input type="text" name="groupment[contact][phone][]" />
-					<label><?php esc_html_e( 'Phone', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</li>
-
-			</ul>
+			<div class="grid-layout w2">
+				<ul>
+					<li class="form-element">
+						<input type="text" name="address[address]" />
+						<label><?php esc_html_e( 'Address', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="address[additional_address]" />
+						<label><?php esc_html_e( 'Additional address', 'digirisk' ); ?> </label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="address[postcode]" />
+						<label><?php esc_html_e( 'Postcode', 'digirisk' ); ?> </label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="address[town]" />
+						<label><?php esc_html_e( 'Town', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="groupment[contact][phone][]" />
+						<label><?php esc_html_e( 'Phone', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+					</li>
+				</ul>
+				<ul>
+					<li class="form-element">
+						<input type="text"
+									data-field="groupment[user_info][owner_id]"
+									data-type="user"
+									placeholder=""
+									class="digi-search"
+									value="<?php echo ! empty( $owner_user->id ) ? esc_attr( User_Digi_Class::g()->element_prefix . $owner_user->id . ' - ' . $owner_user->displayname ) : ''; ?>" />
+						<label><?php esc_html_e( 'Owner', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+						<input type="hidden" name="groupment[user_info][owner_id]" />
+					</li>
+					<li class="form-element active">
+						<input type="text" class="eva-date" name="groupment[date]" value="<?php echo esc_attr( date( 'd/m/Y' ) ); ?>" />
+						<label><?php esc_html_e( 'Created date', 'digirisk' ); ?> </label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="groupment[identity][siren]" />
+						<label><?php esc_html_e( 'SIREN', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+					</li>
+					<li class="form-element">
+						<input type="text" name="groupment[identity][siret]" />
+						<label><?php esc_html_e( 'SIRET', 'digirisk' ); ?></label>
+						<span class="bar"></span>
+					</li>
+				</ul>
+			</div>
 
 			<div class="form-element">
 				<textarea name="groupment[content]"></textarea>
@@ -107,7 +102,7 @@ if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
 				<span class="bar"></span>
 			</div>
 
-			<div class="float right submit-form button blue uppercase bold"><?php esc_html_e( 'Créer ma société', 'digirisk' ); ?></div>
+			<div class="float right submit-form button blue uppercase strong"><span><?php esc_html_e( 'Créer ma société', 'digirisk' ); ?></span></div>
 		</form>
 	</div>
 
@@ -140,7 +135,9 @@ if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
 
 	<div class="hidden wpdigi-staff">
 		<?php do_shortcode( '[digi_user_dashboard]' ); ?>
-		<a href="<?php echo esc_attr( admin_url( 'admin.php?page=digirisk-simple-risk-evaluation' ) ); ?>" type="button" class="float right wp-digi-bton-fourth"><?php esc_html_e( 'Aller sur l\'application', 'digirisk' ); ?></a>
+		<a href="<?php echo esc_attr( admin_url( 'admin.php?page=digirisk-simple-risk-evaluation' ) ); ?>" type="button" class="float right button blue uppercase strong wp-digi-bton-fourth">
+			<span><?php esc_html_e( 'Aller sur l\'application', 'digirisk' ); ?></span>
+		</a>
 	</div>
 
 </div>
