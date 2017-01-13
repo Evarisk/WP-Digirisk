1af5f6b3ca26d28e6b4fdda668eb62d08403bd25
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-01-13 15:35:08 +0100

    Mise à jour de l'affichage légal

--- modules/legal_display/view/list-item.view.php
+++ modules/legal_display/view/list-item.view.php
@@ -1,29 +1,26 @@
 <?php
 /**
- * Gestion de l'affichage d'un DUER
+ * Gestion de l'affichage d'un affichage légal
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
- * @version 6.1.9.0
- * @copyright 2015-2016 Evarisk
- * @package document
+ * @since 6.1.9.0
+ * @version 6.2.4.0
+ * @copyright 2015-2017 Evarisk
+ * @package legal_display
  * @subpackage view
  */
 
 namespace digi;
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 
-?>
-
-<li class='wp-digi-list-item wp-digi-risk-item'>
-	<span><?php echo esc_html( $element->unique_identifier ); ?></span>
-	<span><?php echo esc_html( $element->title ); ?></span>
-	<span class="padded flex-tmp">
+<tr>
+	<td><strong><?php echo esc_html( $element->unique_identifier ); ?></strong></td>
+	<td><?php echo esc_html( $element->title ); ?></td>
+	<td class="padded flex-tmp">
 		<a href="<?php echo esc_attr( Document_Class::g()->get_document_path( $element ) ); ?>">
 			<i class="fa fa-download" aria-hidden="true"></i>
 			<?php esc_html_e( 'Affichage légal ', 'digirisk' ); ?>
 			<?php echo esc_html( strtoupper( substr( $element->type, 16, 2 ) ) ); ?></a>
-	</span>
-</li>
+	</td>
+</tr>
