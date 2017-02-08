5e1068e33b230d3b01e2928b3e43e1595dfd8b22
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-02-08 10:10:45 +0100

    Mise à jour de la prévisualisation de la cotation dans la popup de méthode d'évaluation Evarisk

--- modules/evaluation_method/view/popup/popup.view.php
+++ modules/evaluation_method/view/popup/popup.view.php
@@ -17,7 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 <div class="popup popup-evaluation">
 	<div class="container">
 		<div class="header">
-			<h2 class="title"><?php echo esc_html_e( 'Édition de la méthode d\'évaluation Evarisk', 'digirisk' ); ?></h2>
+			<h2 class="title">
+				<?php echo esc_html_e( 'Édition de la méthode d\'évaluation Evarisk', 'digirisk' ); ?>
+				<?php echo esc_html( ! empty( $risk ) ? ' : ' . $risk->unique_identifier . ' - ' . $risk->evaluation->unique_identifier : '' ); ?>
+			</h2>
 			<i class="close fa fa-times"></i>
 		</div>
 		<div class="content">
@@ -39,7 +42,10 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 					endfor; ?>
 				</tbody>
 			</table>
-			<div class="preview">Prévisualisation: <span><?php echo esc_html( ! empty( $risk->evaluation ) ? $risk->evaluation->risk_level['equivalence'] : 0 ); ?></span></div>
+
+			<div class="cotation level<?php echo esc_attr( ! empty( $risk->evaluation ) ? $risk->evaluation->scale : '-1' ); ?>">
+				<span><?php echo esc_html( ! empty( $risk->evaluation ) ? $risk->evaluation->risk_level['equivalence'] : 0 ); ?></span>
+			</div>
 			<div data-nonce="<?php echo esc_attr( wp_create_nonce( 'get_scale' ) ); ?>" class="button green margin uppercase strong float right"><span>Enregistrer la cotation</span></div>
 		</div>
 	</div>
