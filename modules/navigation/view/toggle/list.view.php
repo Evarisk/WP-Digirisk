cb0cf2abb634d4092781ee5454034258713ef8a4
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-03-10 11:42:45 +0100

    7747 - L'action pour créer un nouveau groupement est prend tout le carré.

--- modules/navigation/view/toggle/list.view.php
+++ modules/navigation/view/toggle/list.view.php
@@ -26,15 +26,13 @@ if ( ! empty( $groupments ) ) :
 						data-loader="digirisk-wrap"
 						class="action-attribute"><?php echo esc_html( $groupment->unique_identifier . ' - ' . $groupment->title ); ?></span>
 
-					<span class="wp-digi-new-group-action">
-						<?php if ( empty( $groupment->count_workunit ) ) : ?>
-							<a
+					<span class="wp-digi-new-group-action <?php echo empty( $groupment->count_workunit ) ? 'action-attribute' : ''; ?>"
 								data-parent-id="<?php echo esc_attr( $groupment->id ); ?>"
 								data-action="create_group"
 								data-nonce="<?php echo esc_attr( wp_create_nonce( 'create_group' ) ); ?>"
-								data-loader="content"
-								href="#"
-								class="wp-digi-action fa fa-plus action-attribute"></a>
+								data-loader="navigation-container">
+						<?php if ( empty( $groupment->count_workunit ) ) : ?>
+							<a href="#"	class="wp-digi-action fa fa-plus"></a>
 						<?php endif; ?>
 					</span>
 				</div>
