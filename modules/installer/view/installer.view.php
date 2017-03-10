90a775597d46d9b11de98b80c59f0aeb3d02aedf
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-03-10 14:31:06 +0100

    Mise à jour de la vue de l'installation

--- modules/installer/view/installer.view.php
+++ modules/installer/view/installer.view.php
@@ -5,7 +5,7 @@
  * @package Evarisk\Plugin
  *
  * @since 0.1
- * @version 6.2.7.0
+ * @version 6.2.8.0
  */
 
 namespace digi;
@@ -15,60 +15,38 @@ if ( ! defined( 'ABSPATH' ) ) {	exit; } ?>
 <div class="wpdigi-installer digirisk-wrap">
 	<h2><?php esc_html_e( 'Digirisk', 'digirisk' ); ?></h2>
 
-	<ul class="step-list">
-		<li class="step step-create-society active"><span class="title"><?php esc_html_e( 'Votre société', 'digirisk' ); ?></span><i class="circle">1</i></li>
-		<li class="step step-create-components"><span class="title"><?php esc_html_e( 'Composants', 'digirisk' ); ?></span><i class="circle">2</i></li>
-		<li class="step step-create-users"><span class="title"><?php esc_html_e( 'Votre personnel', 'digirisk' ); ?></span><i class="circle">3</i></li>
-	</ul>
+	<div class="main-content">
 
-	<div class="main-content society form">
-		<h2><?php esc_html_e( 'Votre société', 'digirisk' );?></h2>
+		<div class="bloc-create-society">
+			<input type="hidden" name="action" value="installer_save_society" />
+			<?php wp_nonce_field( 'ajax_installer_save_society' ); ?>
+			<h2><?php esc_html_e( 'Bienvenue sur DigiRisk. Avant de commencer l\'installation, veuillez entrer le nom de votre société.', 'digirisk' );?></h2>
+			<input type="text" name="groupment[title]" value="<?php echo esc_attr( 'Nom de ma société *' ); ?>" />
+		</div>
 
-		<input type="hidden" name="action" value="installer_save_society" />
-		<?php wp_nonce_field( 'ajax_installer_save_society' ); ?>
-
-		<div class="grid-layout w2">
-			<div class="form-element">
-				<input type="text" name="groupment[title]" />
-				<label>
-					<?php esc_html_e( 'Nom de votre société', 'digirisk' ); ?>
-					<span class="required tooltip red" aria-label="Le nom de votre société est obligatoire.">*</span>
-				</label>
-				<span class="bar"></span>
-			</div>
-			<div>
+		<div class="hidden">
+			<div class="owl-carousel owl-theme">
+				<?php View_Util::exec( 'installer', 'bloc-1' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-2' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-3' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-4' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-5' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-6' ); ?>
+				<?php View_Util::exec( 'installer', 'bloc-7' ); ?>
 			</div>
 		</div>
 
-		<div data-module="installer" data-loader="form" data-parent="form" data-before-method="beforeCreateSociety" class="float right action-input button blue uppercase strong"><span><?php esc_html_e( 'Créer ma société', 'digirisk' ); ?></span></div>
-	</div>
 
-	<div class="main-content hidden wpdigi-components">
-
-		<!-- Le nonce pour la sécurité de la requête -->
-		<?php
-		echo '<input type="hidden" class="nonce-installer-components" value="' . esc_attr( wp_create_nonce( 'ajax_installer_components' ) ) . '" />';
-		?>
-
-		<div class="owl-carousel owl-theme">
-			<?php View_Util::exec( 'installer', 'bloc-1' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-2' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-3' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-4' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-5' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-6' ); ?>
-			<?php View_Util::exec( 'installer', 'bloc-7' ); ?>
+		<div class="bar">
+			<ul class="step-list">
+				<li class="step"><span class="title"><?php esc_html_e( 'Création société', 'digirisk' ); ?></span></li>
+				<li class="step"><span class="title"><?php esc_html_e( 'Installation des catégories de danger', 'digirisk' ); ?></span></li>
+				<li class="step"><span class="title"><?php esc_html_e( 'Installation des méthodes d\'évaluation', 'digirisk' ); ?></span></li>
+				<li class="step"><span class="title"><?php esc_html_e( 'Installation des préconisations', 'digirisk' ); ?></span></li>
+				<li class="step"><span class="title"><?php esc_html_e( 'Digirisk est prêt', 'digirisk' ); ?></span></li>
+			</ul>
 		</div>
-
-		<progress value="0" max="100">Création des dangers</progress>
-		<div class="valid margin"><button class="button blue next disabled"><span>Suivant</span></button></div>
 	</div>
 
-	<div class="hidden wpdigi-staff">
-		<?php do_shortcode( '[digi_user_dashboard]' ); ?>
-		<a href="<?php echo esc_attr( admin_url( 'admin.php?page=digirisk-simple-risk-evaluation' ) ); ?>" type="button" class="float right button blue uppercase strong">
-			<span><?php esc_html_e( 'Aller sur l\'application', 'digirisk' ); ?></span>
-		</a>
-	</div>
-
+	<div data-module="installer" data-loader="main-content" data-parent="main-content" data-before-method="beforeCreateSociety" class="float right action-input button blue uppercase strong"><span><?php esc_html_e( 'Installer', 'digirisk' ); ?></span></div>
 </div>
