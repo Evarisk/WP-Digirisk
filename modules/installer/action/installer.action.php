f1295d26ddc7ba5995d4a9a43cdeaf32266bb305
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-03-13 09:23:12 +0100

    Suppression de la fonction "last_step"

--- modules/installer/action/installer.action.php
+++ modules/installer/action/installer.action.php
@@ -5,7 +5,7 @@
  * @package Evarisk\Plugin
  *
  * @since 0.1
- * @version 6.2.6.0
+ * @version 6.2.8.0
  */
 
 namespace digi;
@@ -21,15 +21,13 @@ class Installer_Action {
 	 * Le constructeur appelle les méthodes admin_post et ajax suivantes:
 	 * save_society (Ajax)
 	 * installer_components (Ajax)
-	 * last_step (admin_post)
 	 *
 	 * @since 0.1
-	 * @version 6.2.4.0
+	 * @version 6.2.8.0
 	 */
 	public function __construct() {
 		add_action( 'wp_ajax_installer_save_society', array( $this, 'ajax_installer_save_society' ) );
 		add_action( 'wp_ajax_installer_components', array( $this, 'ajax_installer_components' ) );
-		add_action( 'admin_post_last_step', array( $this, 'admin_post_last_step' ) );
 	}
 
 
@@ -101,24 +99,6 @@ class Installer_Action {
 		) );
 	}
 
-	/**
-	 * C'est quoi cette méthode ?
-	 *
-	 * @return void
-	 */
-	public function admin_post_last_step() {
-		if ( empty( $_GET['_wpnonce'] ) ) {
-			wp_safe_redirect( wp_get_referer() );
-			die();
-		}
-		$wpnonce = sanitize_text_field( $_GET['_wpnonce'] );
-
-		if ( ! wp_verify_nonce( $wpnonce, 'last_step' ) ) {
-			wp_safe_redirect( admin_url( 'users.php?page=digirisk-users' ) );
-		}
-
-		wp_safe_redirect( admin_url( 'users.php?page=digirisk-users' ) );
-	}
 }
 
 new Installer_Action();
