aba33f1a5eaacab14ca733419d0f1ec73e15f79c
amandine-eoxia <amandine@eoxia.com>
2017-09-11 15:04:47 +0200

    maj css navigation

--- modules/navigation/view/main.view.php
+++ modules/navigation/view/main.view.php
@@ -12,7 +12,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 
-<div class="navigation-container">
+<div class="navigation-containere" style="margin-bottom: 30px;">
 	<?php Navigation_Class::g()->display_toggle( $groupment_id ); ?>
 	<?php Navigation_Class::g()->display_workunit_list( $groupment_id ); ?>
+</div>
+
+
+<div class="navigation-container">
+
+	<div class="society-header">
+		<div class="title">
+			<span class="icon dashicons dashicons-building"></span>
+			Ma société
+		</div>
+	</div>
+
+	<ul class="workunit-list">
+		<li class="unit active">
+			<div class="unit-container">
+				<div class="toggle active"><span class="icon"></span></div>
+				<div class="media no-file">
+					<i class="add animated fa fa-plus-circle"></i>
+					<i class="default-image fa fa-picture-o"></i>
+				</div>
+				<div class="title">
+					<span class="title-container">
+						<span class="ref">GP1</span>
+						<span class="name">Poste 1</span>
+					</span>
+				</div>
+				<div class="add-container">
+					<div class="button w30 blue"><span class="icon dashicons dashicons-admin-multisite"></span></div>
+					<div class="button w30 blue"><span class="icon dashicons dashicons-admin-home"></span></div>
+				</div>
+			</div>
+			<ul class="sub-list">
+				<li class="unit">
+					<div class="unit-container">
+						<div class="toggle"><span class="icon"></span></div>
+						<div class="media no-file">
+							<i class="add animated fa fa-plus-circle"></i>
+							<i class="default-image fa fa-picture-o"></i>
+						</div>
+						<div class="title">
+							<span class="title-container">
+								<span class="ref">GP1</span>
+								<span class="name">Le poste des pompiers</span>
+							</span>
+						</div>
+						<div class="add-container">
+							<div class="button w30 blue"><span class="icon dashicons dashicons-admin-multisite"></span></div>
+							<div class="button w30 blue"><span class="icon dashicons dashicons-admin-home"></span></div>
+						</div>
+					</div>
+				</li>
+			</ul>
+		</li>
+		<li class="unit">
+			<div class="unit-container">
+				<div class="toggle"><span class="icon"></span></div>
+				<div class="media no-file">
+					<i class="add animated fa fa-plus-circle"></i>
+					<i class="default-image fa fa-picture-o"></i>
+				</div>
+				<div class="title">
+					<span class="title-container">
+						<span class="ref">GP1</span>
+						<span class="name">Poste 1</span>
+					</span>
+				</div>
+				<div class="add-container">
+					<div class="button w30 blue"><span class="icon dashicons dashicons-admin-multisite"></span></div>
+					<div class="button w30 blue"><span class="icon dashicons dashicons-admin-home"></span></div>
+				</div>
+			</div>
+		</li>
+	</ul>
+
 </div>
