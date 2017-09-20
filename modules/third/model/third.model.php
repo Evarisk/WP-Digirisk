13725aabf37f5575aff556146453a3066d8d735f
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-09-20 15:26:31 +0200

    6.3.0 Tiers -Mise à jour du modèle

--- modules/third/model/third.model.php
+++ modules/third/model/third.model.php
@@ -1,37 +1,66 @@
-<?php namespace digi;
+<?php
+/**
+ * Définition des champs d'un tier.
+ *
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.0.0
+ * @version 6.3.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
+ */
 
-if ( !defined( 'ABSPATH' ) ) exit;
+namespace digi;
 
-class third_model extends \eoxia\post_model {
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
+/**
+ * Définition d'un tier
+ */
+class Third_Model extends \eoxia\post_model {
+
+	/**
+	 * Définition des champs
+	 *
+	 * @param Object $object La définition des champs.
+	 *
+	 * @since 6.0.0
+	 * @version 6.3.0
+	 */
 	public function __construct( $object ) {
 		$this->model = array_merge( $this->model, array(
 			'full_name' => array(
-				'type' 			=> 'string',
+				'type' => 'string',
 				'meta_type' => 'multiple',
-				'bydefault' => ''
+				'bydefault' => '',
 			),
 			'contact' => array(
 				'meta_type' => 'multiple',
-				'type'			=> 'array',
+				'type' => 'array',
 				'child' => array(
 					'phone' => array(
-						'type'	=> 'string',
+						'type' => 'string',
+						'bydefault' => '',
+						'meta_type' => 'multiple',
+					),
+					'email' => array(
+						'type' => 'string',
 						'bydefault' => '',
 						'meta_type' => 'multiple',
 					),
 					'address_id' => array(
-						'type'	=> 'integer',
-						'bydefault'	=> 0,
+						'type' => 'integer',
+						'bydefault' => 0,
 						'meta_type' => 'multiple',
-					)
-				)
+					),
+				),
 			),
 			'opening_time' => array(
 				'type' => 'string',
 				'bydefault' => '',
 				'meta_type' => 'multiple',
-			)
+			),
 		) );
 		parent::__construct( $object );
 	}
