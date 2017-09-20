02f7bb3a81c80fac21c563a658671bc782d23a77
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-09-20 15:28:57 +0200

    Mise à jour 6.3.0 -Créer l'entité société depuis le GP1

---
+++ modules/update_manager/data/update-6300-data.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Les données pour la MAJ 6.3.0
+ *
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.3.0
+ * @version 6.3.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
+ */
+
+$datas = array(
+	array(
+		'action' => 'digirisk_update_create_society',
+		'description' => __( 'Créer la société à partir du GP1.', 'digirisk' ),
+	),
+);
