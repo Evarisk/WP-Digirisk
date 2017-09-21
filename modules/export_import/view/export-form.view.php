18f2d557fadc1c415a2564e2dab655618ae1fffc
Jimmy <jimmy@eoxia.com>
2017-09-21 15:46:16 +0200

    6.3.0 Passes le module en 'indisponible'

--- modules/export_import/view/export-form.view.php
+++ modules/export_import/view/export-form.view.php
@@ -1,21 +1,24 @@
-<?php namespace digi;
-
-if ( !defined( 'ABSPATH' ) ) exit;
+<?php
 /**
- * Fichier du squelette du formulaire permettant de lancer l'export des données / Template file for the form allowing to export datas
+ * Vue permettant d'exporter un modèle DigiRisk.
  *
- * @author Alexandre Techer <dev@evarisk.com>
- * @version 6.1.5.5
- * @copyright 2015-2016 Evarisk
- * @package export_import
- * @subpackage view
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.1.9
+ * @version 6.3.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
  */
-?>
+
+namespace digi;
+
+if ( ! defined( 'ABSPATH' ) ) exit; ?>
+
 <form action="<?php echo admin_url( 'admin-ajax.php' ); ?>" method="POST" id="digi-export-form" >
 	<h3><?php _e( 'Export', 'digirisk' ); ?></h3>
 
 	<div class="content">
-		<input type="hidden" name="action" value="digi_export_data" />
+		<?php echo esc_html_e( 'Cette fonctionnalitée est indisponible.', 'digirisk' ); ?>
+		<!--<input type="hidden" name="action" value="digi_export_data" />
 		<?php /** Crtéation d'un nonce de sécurité pour le formulaire / Create a security nonce for the form */ wp_nonce_field( 'digi_export_data' ); ?>
 		<input type="hidden" name="element_id" value="<?php echo $element_id; ?>" />
 
@@ -23,16 +26,16 @@ if ( !defined( 'ABSPATH' ) ) exit;
 
 		<ul class="hidden">
 			<li>
-				<label><input type="checkbox" name="type_to_export[]" value="<?php echo group_class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Groupements', 'digirisk' ); ?></label>
+				<label><input type="checkbox" name="type_to_export[]" value="<?php echo Group_Class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Groupements', 'digirisk' ); ?></label>
 			</li>
 			<li>
-				<label><input type="checkbox" name="type_to_export[]" value="<?php echo workunit_class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Work units', 'digirisk' ); ?></label>
+				<label><input type="checkbox" name="type_to_export[]" value="<?php echo Workunit_Class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Work units', 'digirisk' ); ?></label>
 			</li>
 			<li>
-				<label><input type="checkbox" name="type_to_export[]" value="<?php echo risk_class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Risks', 'digirisk' ); ?></label>
+				<label><input type="checkbox" name="type_to_export[]" value="<?php echo Risk_Class::g()->get_post_type(); ?>" checked="true" /><?php _e( 'Risks', 'digirisk' ); ?></label>
 			</li>
-		</ul>
+		</ul>-->
 	</div>
 
-	<button class="button blue" id="digi-export-button" ><?php _e( 'Export my Digirisk datas', 'digirisk' ); ?></button>
+	<!--<button class="button blue" id="digi-export-button" ><?php _e( 'Export my Digirisk datas', 'digirisk' ); ?></button>-->
 </form>
--- modules/export_import/view/import-form.view.php
+++ modules/export_import/view/import-form.view.php
@@ -1,30 +1,35 @@
-<?php namespace digi;
-
-if ( !defined( 'ABSPATH' ) ) exit;
+<?php
 /**
- * Fichier du squelette du formulaire permettant de lancer l'export des données / Template file for the form allowing to export datas
+ * Vue permettant d'impoter un modèle DigiRisk.
  *
- * @author Alexandre Techer <dev@evarisk.com>
- * @version 6.1.5.5
- * @copyright 2015-2016 Evarisk
- * @package export_import
- * @subpackage view
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.1.9
+ * @version 6.3.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
  */
-?>
+
+namespace digi;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
+
 <form action="<?php echo admin_url( 'admin-ajax.php' ); ?>" method="POST" id="digi-import-form" >
 	<h3><?php _e( 'Import', 'digirisk' ); ?></h3>
 
 	<div class="content">
-		<input type="hidden" name="action" value="digi_import_data" />
+		<?php echo esc_html_e( 'Cette fonctionnalitée est indisponible.', 'digirisk' ); ?>
+		<!--<input type="hidden" name="action" value="digi_import_data" />
 		<?php /** Crtéation d'un nonce de sécurité pour le formulaire / Create a security nonce for the form */ wp_nonce_field( 'digi_import_data' ); ?>
 		<input type="hidden" name="element_id" value="<?php echo $element_id; ?>" />
 
 		<span class="digi-import-explanation" ><?php _e( 'Don\'t start from scratch. Use a predefined template by importing it with the button below', 'digirisk' ); ?></span>
 		<progress value="0" max="100">0%</progress>
 		<input type="file" name="file" id="file" />
-		<span class="digi-import-detail"></span>
+		<span class="digi-import-detail"></span>-->
 	</div>
 
-	<label for="file" class="button blue" ><?php _e( 'Import Digirisk model (.ZIP)', 'digirisk' ); ?></label><br />
+	<!--<label for="file" class="button blue" ><?php esc_html_e( 'Import Digirisk model (.ZIP)', 'digirisk' ); ?></label><br />-->
 
 </form>
