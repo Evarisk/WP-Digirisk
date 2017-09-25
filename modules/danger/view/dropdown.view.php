ef2a68a6a90d426b735b8fffc6540698ee607afa
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-09-25 10:22:55 +0200

    6.3.0 - Renommes 'Choisir un danger' en 'Choisir un risque'

--- modules/danger/view/dropdown.view.php
+++ modules/danger/view/dropdown.view.php
@@ -3,16 +3,17 @@
  * La liste des danger
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
- * @since 6.2.1.0
- * @version 6.2.9.0
+ * @since 6.2.1
+ * @version 6.3.0
  * @copyright 2015-2017 Evarisk
- * @package danger
- * @subpackage view
+ * @package DigiRisk
  */
 
 namespace digi;
 
-if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <input class="input-hidden-danger" type="hidden" name="risk[danger_id]" value='<?php echo ! empty( $preset ) ? esc_attr( $selected_danger->id ) : '-1'; ?>' />
 
@@ -20,10 +21,10 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 			data-parent="categorie-container"
 			data-target="content"
 			data-nonce="<?php echo esc_attr( wp_create_nonce( 'check_predefined_danger' ) ); ?>"
-			aria-label="<?php esc_html_e( 'Vous devez choisir un danger.', 'digirisk' ); ?>">
+			aria-label="<?php esc_html_e( 'Vous devez choisir un risque.', 'digirisk' ); ?>">
 
 	<div class="action">
-		<span class="<?php echo ! empty( $preset ) && ! empty( $selected_danger ) ? 'hidden' : ''; ?>"><?php esc_html_e( 'Choisir un danger', 'digirisk' ); ?></span>
+		<span class="<?php echo ! empty( $preset ) && ! empty( $selected_danger ) ? 'hidden' : ''; ?>"><?php esc_html_e( 'Choisir un risque', 'digirisk' ); ?></span>
 		<img class="<?php echo ! empty( $preset ) ? '' : 'hidden'; ?> tooltip hover" src="<?php echo ! empty( $selected_danger ) ? esc_attr( wp_get_attachment_url( $selected_danger->thumbnail_id ) ) : ''; ?>" aria-label="" />
 		<i class="icon animated fa fa-angle-down"></i>
 	</div>
