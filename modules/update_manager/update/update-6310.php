07a65f98ad735d0189cd9918f162c983b50df27f
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-06 16:02:16 +0200

    6.3.1: Passes l'équivalence

---
+++ modules/update_manager/update/update-6310.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Mise à jour des données pour la version 6.3.1
+ *
+ * @author Evarisk
+ * @since 6.3.1
+ * @version 6.3.1
+ * @copyright 2017
+ * @package DigiRisk
+ */
+
+namespace digi;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Mise à jour des données pour la version 6.3.1
+ */
+class Update_631 {
+	private $limit_update_risk = 10;
+
+	/**
+	 * Initialise les actions nécessaire pour les mises à jour de la 6.3.1
+	 * Ses actions sont définis dans le fichier data/update-6310-data.php
+	 *
+	 * @since 6.3.1
+	 * @version 6.3.1
+	 */
+	public function __construct() {
+		add_action( 'wp_ajax_digirisk_update_risk_equivalence', array( $this, 'callback_digirisk_update_risk_equivalence' ) );
+	}
+
+	/**
+	 *
+	 * @since 6.3.1
+	 * @version 6.3.1
+	 */
+	public function callback_digirisk_update_risk_equivalence() {
+		$done = false;
+
+		$index = ! empty( $_POST['args']['index'] ) ? (int) $_POST['args']['index'] : 0;
+		$count = ! empty( $_POST['args']['count'] ) ? (int) $_POST['args']['count'] : count( get_posts( array(
+			'posts_per_page' => -1,
+			'post_type' => Risk_Class::g()->get_post_type(),
+			'meta_key' => '_wpdigi_preset',
+			'meta_value' => 1,
+			'meta_compare' => '!=',
+		) ) );
+
+		$risks = Risk_Class::g()->get( array(
+			'posts_per_page' => $this->limit_update_risk,
+			'offset' => $index,
+			'meta_key' => '_wpdigi_preset',
+			'meta_value' => 1,
+			'meta_compare' => '!=',
+		) );
+
+		if ( ! empty( $risks ) ) {
+			foreach ( $risks as $risk ) {
+
+				$risk->evaluation->equivalence = $risk->evaluation->risk_level['equivalence'];
+				Risk_Evaluation_Class::g()->update( $risk->evaluation );
+
+				$risk->current_equivalence = $risk->evaluation->equivalence;
+				Risk_Class::g()->update( $risk );
+			}
+		}
+
+		$index += $this->limit_update_risk;
+
+		if ( $index >= $count ) {
+			$done = true;
+		}
+
+		wp_send_json_success( array(
+			'done' => $done,
+			'args' => array(
+				'more' => true,
+				'index' => $index,
+				'count' => $count,
+				'moreDescription' => ' (' . $index . '/' . $count . ')',
+			),
+		) );
+	}
+}
+
+new Update_631();
