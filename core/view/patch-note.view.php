aaf7f9d34fef3c8af6762f18a877ee8760901035
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-10 11:46:40 +0200

    6.3.1: Résolution de l'erreur du patchnote

--- core/view/patch-note.view.php
+++ core/view/patch-note.view.php
@@ -19,11 +19,11 @@ $result = Digirisk_Class::g()->get_patch_note(); ?>
 
 <div class="notification patch-note active">
 	<span class="thumbnail"><img src="<?php echo esc_attr( PLUGIN_DIGIRISK_URL . 'core/assets/images/favicon_hd.png' ); ?>" /></span>
-	<span class="title">Note de mise à jour de la <a href="#">version <?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->version ); ?></a></span>
+	<span class="title">Note de mise à jour de la <a href="#">version <?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->major_version ); ?></a></span>
 	<span class="close action-attribute"
 				data-action="close_change_log"
 				data-nonce="<?php echo esc_attr( wp_create_nonce( 'close_change_log' ) ); ?>"
-				data-version="<?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->version ); ?>"><i class="icon fa fa-times-circle"></i></span>
+				data-version="<?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->major_version ); ?>"><i class="icon fa fa-times-circle"></i></span>
 </div>
 
 <div class="popup patch-note">
@@ -41,9 +41,9 @@ $result = Digirisk_Class::g()->get_patch_note(); ?>
 						<div class="entry-title"><?php echo esc_html( $element->numero_de_suivi ); ?></div>
 						<div class="entry-content"><?php echo $element->description; ?></div>
 							<?php
-							if ( ! empty( $element->illustration ) ) :
+							if ( ! empty( $element->illustration ) && ! empty( $element->illustration->url ) ) :
 								?>
-								<img src="<?php echo esc_attr( $element->illustration ); ?>" alt="<?php echo esc_attr( $element->numero_de_suivi ); ?>" />
+								<img src="<?php echo esc_attr( $element->illustration->url ); ?>" alt="<?php echo esc_attr( $element->numero_de_suivi ); ?>" />
 								<?php
 							endif;
 							?>
