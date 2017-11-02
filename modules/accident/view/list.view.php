584f499fc5040f5530c71b81e44c3bf58ae6d924
Amandine Eoxia <amandine@eoxia.com>
2017-11-02 16:47:09 +0100

    6.4.0: Ajout du fichier css Flex Table

--- modules/accident/view/list.view.php
+++ modules/accident/view/list.view.php
@@ -42,3 +42,81 @@ if ( ! defined( 'ABSPATH' ) ) {
 	) );
 	?>
 </div>
+
+<!-- flex table -->
+
+
+<div class="flex-table accident">
+	<div class="table-header">
+		<div class="col">
+			<div class="header-cell padding">Colonne 1</div>
+			<div class="header-cell padding">Colonne 2</div>
+			<div class="header-cell padding">Colonne 3</div>
+			<div class="header-cell padding w100"></div>
+		</div>
+	</div>
+
+	<div class="table-body">
+		<div class="col">
+			<div data-title="Colonne 1" class="cell padding">Contenu 1</div>
+			<div data-title="Colonne 2" class="cell padding">Contenu 2</div>
+			<div data-title="Colonne 3" class="cell padding">Contenu 3</div>
+			<div data-title="action" class="cell w100">
+				<div class="action grid-layout w3">
+					<div 	class="button light w50 edit action-attribute"><i class="icon fa fa-pencil"></i></div>
+					<div 	class="button light w50 delete action-delete"><i class="icon fa fa-times"></i></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="col advanced">
+			<div class="col">
+				<div data-title="Colonne 1" class="cell padding">Contenu 1</div>
+				<div data-title="Colonne 2" class="cell padding">Contenu 2</div>
+				<div data-title="Colonne 3" class="cell padding">Contenu 3</div>
+				<div data-title="action" class="cell w100">
+					<div class="action grid-layout w3">
+						<div data-parent="accident-row" data-loader="table" data-namespace="digirisk" data-module="accident" data-before-method="saveSignature" class="button w50 green save action-input"><i class="icon fa fa-floppy-o"></i></div>
+					</div>
+				</div>
+			</div>
+			<div class="advanced">
+				<form class="form">
+					<div class="grid-layout padding w3">
+						<div>
+							<label for="champs1">Label</label>
+							<input id="champs1" />
+						</div>
+						<div>
+							<label for="champs2">Label</label>
+							<input id="champs2" />
+						</div>
+						<div>
+							<label for="champs3">Label</label>
+							<input id="champs3" />
+						</div>
+					</div>
+					<label for="textarea">Label</label>
+					<textarea id="textarea">
+
+					</textarea>
+				</form>
+			</div>
+		</div>
+	</div class="table-body">
+
+	<div class="table-footer">
+		<div class="col">
+			<div class="cell" ></div>
+			<div class="cell" ></div>
+			<div class="cell" ></div>
+			<div class="cell w100" data-title="action">
+				<div class="action grid-layout w3">
+					<div 	data-loader="table"
+								data-parent="risk-row"
+								class="button w50 blue add"><i class="icon fa fa-plus"></i></div>
+				</div>
+			</div>
+		</div>
+	</div class="table-footer">
+</div>
