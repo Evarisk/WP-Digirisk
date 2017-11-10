a250fae8594f23b8e22761b29ed2ea33f2ab7b85
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-10 15:50:22 +0100

    6.4.0: Mise à la norme WordPress

--- modules/evaluation_method/view/view-evaluation-method.view.php
+++ modules/evaluation_method/view/view-evaluation-method.view.php
@@ -3,17 +3,17 @@
  * Affiches la cotation d'un risque
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
- * @since 6.2.3.0
- * @version 6.2.3.0
+ * @since 6.2.3
+ * @version 6.4.0
  * @copyright 2015-2017 Evarisk
- * @package method_evaluation
- * @subpackage view
+ * @package DigiRisk
  */
 
 namespace digi;
 
-
-if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <div 	class="cotation-container grid open-popup-ajax tooltip hover"
 			data-id="<?php echo esc_attr( $risk->id ); ?>"
@@ -22,10 +22,11 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
 			data-nonce="<?php echo esc_attr( wp_create_nonce( 'historic_risk' ) ); ?>"
 			data-parent="risk-row"
 			data-target="popup"
+			data-class="historic-risk"
 			aria-label="<?php esc_html_e( 'Afficher l\'historique des cotations', 'digirisk' ); ?>">
 
 	<div class="action cotation default-cotation level<?php echo esc_attr( $risk->evaluation->scale ); ?>">
-		<i class="icon fa fa-line-chart" style="<?php echo ( 0 !== $risk->evaluation->scale ) ? 'display: none;': ''; ?>"></i>
+		<i class="icon fa fa-line-chart" style="<?php echo ( 0 !== $risk->evaluation->scale ) ? 'display: none;' : ''; ?>"></i>
 		<span><?php echo esc_html( $risk->evaluation->risk_level['equivalence'] ); ?></span>
 	</div>
 
