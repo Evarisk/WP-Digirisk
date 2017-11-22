eb709b1ed6c3f45df352625053f082cbfa63cc3e
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-22 15:19:51 +0100

    6.4.0: Ajout de l'onglet "capacité"

--- modules/setting/view/main.view.php
+++ modules/setting/view/main.view.php
@@ -1,27 +1,50 @@
-<?php namespace digi;
+<?php
+/**
+ * Gestion des onglets dans la page "digirisk-setting".
+ *
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.0.0
+ * @version 6.4.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
+ */
 
-if ( ! defined( 'ABSPATH' ) ) { exit; } ?>
+namespace digi;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <div class="wrap">
 	<h1><?php esc_html_e( 'Digirisk settings', 'digirisk' ); ?></h1>
 
 	<div class="digi-tools-main-container">
 		<h2 class="nav-tab-wrapper">
-			<a class="nav-tab nav-tab-active" href="#" data-id="digi-accronym" ><?php esc_html_e( 'Accronymes', 'digirisk' ); ?></a>
-			<a class="nav-tab" href="#" data-id="digi-danger-preset" ><?php esc_html_e( 'Danger preset', 'digirisk' ); ?></a>
+			<a class="nav-tab <?php echo ( 'digi-capability' === $default_tab ) ? 'nav-tab-active' : ''; ?>" href="#" data-id="digi-capability" ><?php esc_html_e( 'Capacités', 'digirisk' ); ?></a>
+			<a class="nav-tab <?php echo ( 'digi-accronym' === $default_tab ) ? 'nav-tab-active' : ''; ?>" href="#" data-id="digi-accronym" ><?php esc_html_e( 'Accronymes', 'digirisk' ); ?></a>
+			<a class="nav-tab <?php echo ( 'digi-danger-preset' === $default_tab ) ? 'nav-tab-active' : ''; ?>" href="#" data-id="digi-danger-preset" ><?php esc_html_e( 'Danger preset', 'digirisk' ); ?></a>
 		</h2>
 
 		<div class="digirisk-wrap">
-			<div id="digi-accronym" class="tab-content">
-				<?php \eoxia\View_Util::exec( 'digirisk', 'setting', 'accronym/form', array(
-					'list_accronym' => $list_accronym,
-				) ); ?>
+
+			<div id="digi-capability" class="tab-content <?php echo ( 'digi-capability' === $default_tab ) ? '' : 'hidden'; ?>">
+				<?php \eoxia\View_Util::exec( 'digirisk', 'setting', 'capability/main' ); ?>
 			</div>
 
-			<div id="digi-danger-preset" class="tab-content hidden">
-					<?php \eoxia\View_Util::exec( 'digirisk', 'setting', 'preset/main', array(
+			<div id="digi-accronym" class="tab-content <?php echo ( 'digi-accronym' === $default_tab ) ? '' : 'hidden'; ?>">
+				<?php
+				\eoxia\View_Util::exec( 'digirisk', 'setting', 'accronym/form', array(
+					'list_accronym' => $list_accronym,
+				) );
+				?>
+			</div>
+
+			<div id="digi-danger-preset" class="tab-content <?php echo ( 'digi-danger-preset' === $default_tab ) ? '' : 'hidden'; ?>">
+					<?php
+					\eoxia\View_Util::exec( 'digirisk', 'setting', 'preset/main', array(
 						'dangers_preset' => $dangers_preset,
-					) ); ?>
+					) );
+					?>
 			</div>
 		</div>
 	</div>
