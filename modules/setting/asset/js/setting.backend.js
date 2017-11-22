5609be4ed59612d41b72dba89af9fe7c43c4b371
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-22 15:20:21 +0100

    6.4.0: Gestion de la page pour régler les capacités des utilisateurs

--- modules/setting/asset/js/setting.backend.js
+++ modules/setting/asset/js/setting.backend.js
@@ -1,8 +1,8 @@
 /**
  * Initialise l'objet "setting" ainsi que la méthode "init" obligatoire pour la bibliothèque EoxiaJS.
  *
- * @since 1.0
- * @version 6.2.9.0
+ * @since 6.0.0
+ * @version 6.4.0
  */
 window.eoxiaJS.digirisk.setting = {};
 
@@ -13,6 +13,7 @@ window.eoxiaJS.digirisk.setting.init = function() {
 window.eoxiaJS.digirisk.setting.event = function() {
 	jQuery( document ).on( 'click', '#digi-danger-preset .save-all:not(.grey)', window.eoxiaJS.digirisk.setting.savePresetRisks );
 	jQuery( document ).on( 'click', '#digi-danger-preset table tr input:not(input[type="checkbox"]), #digi-danger-preset tr .toggle, #digi-danger-preset tr textarea, #digi-danger-preset tr .popup, #digi-danger-preset tr .action', window.eoxiaJS.digirisk.setting.checkTheCheckbox );
+	jQuery( document ).on( 'click', '.settings_page_digirisk-setting .list-users .wp-digi-pagination a', window.eoxiaJS.digirisk.setting.pagination );
 };
 
 window.eoxiaJS.digirisk.setting.savePresetRisks = function( event ) {
@@ -23,6 +24,36 @@ window.eoxiaJS.digirisk.setting.savePresetRisks = function( event ) {
 	jQuery( '#digi-danger-preset tr  .edit-risk.checked:first' ).click();
 	jQuery( '#digi-danger-preset .save-all' ).removeClass( 'green' ).addClass( 'disable' );
 };
+
+
+/**
+ * Gestion de la pagination des utilisateurs.
+ *
+ * @param  {ClickEvent} event [description]
+ * @return {void}
+ *
+ * @since 6.4.0
+ * @version 6.4.0
+ */
+window.eoxiaJS.digirisk.setting.pagination = function( event ) {
+	var href = jQuery( this ).attr( 'href' ).split( '&' );
+	var nextPage = href[2].replace( 'current_page=', '' );
+
+	jQuery( '.list-users' ).addClass( 'loading' );
+
+	var data = {
+		action: 'paginate_setting_page_user',
+		next_page: nextPage
+	};
+
+	event.preventDefault();
+
+	jQuery.post( window.ajaxurl, data, function( view ) {
+		jQuery( '.list-users' ).replaceWith( view );
+		window.eoxiaJS.digirisk.search.renderChanged();
+	} );
+};
+
 
 /**
  * Coches la case à cocher lors de l'action dans une ligne du tableau.
@@ -42,4 +73,19 @@ window.eoxiaJS.digirisk.setting.checkTheCheckbox = function( event ) {
 window.eoxiaJS.digirisk.setting.savedRiskSuccess = function( element, response ) {
 	jQuery( element ).closest( 'tr' ).replaceWith( response.data.template );
 	// window.eoxiaJS.digirisk.setting.savePresetRisks( undefined );
+};
+
+
+/**
+ * Le callback en cas de réussite à la requête Ajax "save_capacity".
+ * Affiches le message de "success".
+ *
+ * @param  {HTMLDivElement} triggeredElement  L'élement HTML déclenchant la requête Ajax.
+ * @param  {Object}         response          Les données renvoyées par la requête Ajax.
+ * @return {void}
+ *
+ * @since 6.4.0
+ * @version 6.4.0
+ */
+window.eoxiaJS.digirisk.setting.savedCapability = function( triggeredElement, response ) {
 };
--- modules/setting/class/setting.class.php
+++ modules/setting/class/setting.class.php
@@ -23,12 +23,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 class Setting_Class extends \eoxia\Singleton_Util {
 
 	/**
+	 * La limite des utilisateurs de la page ="digirisk-setting"
+	 *
+	 * @var integer
+	 */
+	private $limit_user = 20;
+
+	/**
 	 * Le constructeur
 	 *
 	 * @return void
 	 *
-	 * @since 1.0
-	 * @version 6.2.9.0
+	 * @since 6.0.0
+	 * @version 6.2.9
 	 */
 	protected function construct() {
 		add_action( 'admin_enqueue_scripts', array( $this, 'init_option' ) );
@@ -40,8 +47,8 @@ class Setting_Class extends \eoxia\Singleton_Util {
 	 *
 	 * @return void
 	 *
-	 * @since 1.0
-	 * @version 6.2.9.0
+	 * @since 6.0.0
+	 * @version 6.2.9
 	 */
 	public function init_option() {
 		$file_content = file_get_contents( \eoxia\Config_Util::$init['digirisk']->setting->path . 'asset/json/default.json' );
@@ -86,6 +93,72 @@ class Setting_Class extends \eoxia\Singleton_Util {
 				update_option( \eoxia\Config_Util::$init['digirisk']->setting->key_preset_danger, true );
 			}
 		}
+	}
+
+	/**
+	 * Récupère le role "subscriber" et appel la vue "capability/has-cap".
+	 *
+	 * @since 6.4.0
+	 * @version 6.4.0
+	 *
+	 * @return void
+	 */
+	public function display_role_has_cap() {
+		$role_subscriber = get_role( 'subscriber' );
+
+		\eoxia\View_Util::exec( 'digirisk', 'setting', 'capability/has-cap', array(
+			'role_subscriber' => $role_subscriber,
+		) );
+	}
+
+	/**
+	 * Récupères la liste des utilisateurs pour les afficher dans la vue "capability/list".
+	 *
+	 * @since 6.4.0
+	 * @version 6.4.0
+	 *
+	 * @param array $list_user_id La liste des utilisateurs à afficher. Peut être vide pour récupérer tous les utilisateurs.
+	 *
+	 * @return void
+	 */
+	public function display_user_list_capacity( $list_user_id = array() ) {
+		$current_page = ! empty( $_POST['next_page'] ) ? (int) $_POST['next_page'] : 1;
+		$args_user = array(
+			'exclude' => array( 1 ),
+			'offset' => ( $current_page - 1 ) * $this->limit_user,
+			'number' => $this->limit_user,
+		);
+
+		if ( ! empty( $list_user_id ) ) {
+			$args_user['include'] = $list_user_id;
+		}
+
+		$users = User_Digi_Class::g()->get( $args_user );
+
+		unset( $args_user['offset'] );
+		unset( $args_user['number'] );
+		unset( $args_user['include'] );
+		$args_user['fields'] = array( 'ID' );
+
+		$count_user = count( User_Digi_Class::g()->get( $args_user ) );
+		$number_page = ceil( $count_user / $this->limit_user );
+
+		$role_subscriber = get_role( 'subscriber' );
+		$has_capacity_in_role = ! empty( $role_subscriber->capabilities['manage_digirisk'] ) ? true : false;
+
+		if ( ! empty( $users ) ) {
+			foreach ( $users as &$user ) {
+				$user->wordpress_user = new \WP_User( $user->id );
+			}
+		}
+
+		\eoxia\View_Util::exec( 'digirisk', 'setting', 'capability/list', array(
+			'users' => $users,
+			'has_capacity_in_role' => $has_capacity_in_role,
+			'number_page' => $number_page,
+			'count_user' => $count_user,
+			'current_page' => $current_page,
+		) );
 	}
 }
 
