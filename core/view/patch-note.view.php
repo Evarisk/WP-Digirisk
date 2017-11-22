1c9203c67acd4bb2ad3daaaa9f78bfd372b13fb2
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-22 15:15:40 +0100

    6.4.0: Nouvelle gestion du changelog

--- core/view/patch-note.view.php
+++ core/view/patch-note.view.php
@@ -4,7 +4,7 @@
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
  * @since 6.3.0
- * @version 6.3.0
+ * @version 6.4.0
  * @copyright 2015-2017 Evarisk
  * @package DigiRisk
  */
@@ -17,41 +17,44 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $result = Digirisk_Class::g()->get_patch_note(); ?>
 
-<div class="notification patch-note active">
-	<span class="thumbnail"><img src="<?php echo esc_attr( PLUGIN_DIGIRISK_URL . 'core/assets/images/favicon_hd.png' ); ?>" /></span>
-	<span class="title">Note de mise à jour de la <a href="#">version <?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->major_version ); ?></a></span>
-	<span class="close action-attribute"
-				data-action="close_change_log"
-				data-nonce="<?php echo esc_attr( wp_create_nonce( 'close_change_log' ) ); ?>"
-				data-version="<?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->major_version ); ?>"><i class="icon fa fa-times-circle"></i></span>
-</div>
+<?php if ( $result ) : ?>
+	<div class="notification patch-note active">
+		<span class="thumbnail"><img src="<?php echo esc_attr( PLUGIN_DIGIRISK_URL . 'core/assets/images/favicon_hd.png' ); ?>" /></span>
+		<span class="title">Note de mise à jour de la <a href="#">version <?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->version ); ?></a></span>
+		<span class="close action-attribute"
+					data-action="close_change_log"
+					data-nonce="<?php echo esc_attr( wp_create_nonce( 'close_change_log' ) ); ?>"
+					data-version="<?php echo esc_attr( \eoxia\Config_Util::$init['digirisk']->version ); ?>"><i class="icon fa fa-times-circle"></i></span>
+	</div>
 
-<div class="popup patch-note">
-	<div class="container">
-		<div class="header">
-			<h2 class="title"><?php echo esc_html( 'Note de version: ' . $result->acf->numero_de_version ); ?></h2>
-			<i class="close fa fa-times"></i>
-		</div>
-		<div class="content">
-			<?php
-			if ( ! empty( $result->acf->note_de_version ) ) :
-				foreach ( $result->acf->note_de_version as $element ) :
-					?>
-					<div class="note">
-						<div class="entry-title"><?php echo esc_html( $element->numero_de_suivi ); ?></div>
-						<div class="entry-content"><?php echo $element->description; ?></div>
-							<?php
-							if ( ! empty( $element->illustration ) && ! empty( $element->illustration->url ) ) :
-								?>
-								<img src="<?php echo esc_attr( $element->illustration->url ); ?>" alt="<?php echo esc_attr( $element->numero_de_suivi ); ?>" />
+	<div class="popup patch-note">
+		<div class="container">
+			<div class="header">
+				<h2 class="title"><?php echo esc_html( 'Note de version: ' . $result->numero_de_version ); ?></h2>
+				<i class="close fa fa-times"></i>
+			</div>
+			<div class="content">
+				<?php
+				if ( ! empty( $result->note_de_version ) ) :
+					foreach ( $result->note_de_version as $element ) :
+						?>
+						<div class="note">
+							<div class="entry-title"><?php echo esc_html( $element->numero_de_suivi ); ?></div>
+							<div class="entry-content"><?php echo $element->description; ?></div>
 								<?php
-							endif;
-							?>
-					</div>
-					<?php
-				endforeach;
-			endif;
-			?>
+								if ( ! empty( $element->illustration ) && ! empty( $element->illustration->url ) ) :
+									?>
+									<img src="<?php echo esc_attr( $element->illustration->url ); ?>" alt="<?php echo esc_attr( $element->numero_de_suivi ); ?>" />
+									<?php
+								endif;
+								?>
+						</div>
+						<?php
+					endforeach;
+				endif;
+				?>
+			</div>
 		</div>
 	</div>
-</div>
+<?php
+endif;
