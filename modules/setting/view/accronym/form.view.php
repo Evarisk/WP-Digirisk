829d980c72a054b52688bd4a7c27925401739db3
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-22 15:20:08 +0100

    6.4.0: Debug le bouton

--- modules/setting/view/accronym/form.view.php
+++ modules/setting/view/accronym/form.view.php
@@ -1,10 +1,25 @@
-<?php namespace digi; ?>
+<?php
+/**
+ * Affichage principale du formulaire pour gérer les accronymes.
+ *
+ * @author Jimmy Latour <jimmy@evarisk.com>
+ * @since 6.4.0
+ * @version 6.4.0
+ * @copyright 2015-2017 Evarisk
+ * @package DigiRisk
+ */
 
+namespace digi; ?>
 
-<form method="post" action="<?php echo admin_url( 'admin-post.php' ); ?>">
+<form method="post" action="<?php echo esc_attr( admin_url( 'admin-post.php' ) ); ?>">
 	<input type="hidden" name="action" value="update_accronym" />
 	<?php wp_nonce_field( 'update_accronym' ); ?>
 
-	<?php \eoxia\View_Util::exec( 'digirisk', 'setting', 'accronym/list-item', array( 'list_accronym' => $list_accronym ) ); ?>
-	<?php echo submit_button(); ?>
+	<?php
+	\eoxia\View_Util::exec( 'digirisk', 'setting', 'accronym/list-item', array(
+		'list_accronym' => $list_accronym,
+	) );
+	?>
+
+	<?php echo submit_button( 'Save', 'primary blue' ); ?>
 </form>
