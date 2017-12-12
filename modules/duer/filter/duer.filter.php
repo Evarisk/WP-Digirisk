de53bb3b2b727de7220634377752cf73a7cffebc
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-12-12 11:55:16 +0100

    6.4.4: Ajout de l'onglet "Liste des risques" dans les GP

--- modules/duer/filter/duer.filter.php
+++ modules/duer/filter/duer.filter.php
@@ -28,10 +28,11 @@ class DUER_Filter {
 	 */
 	public function __construct() {
 		add_filter( 'digi_tab', array( $this, 'callback_digi_tab' ), 5, 2 );
+		add_filter( 'digi_tab', array( $this, 'callback_digi_tab_group' ), 8, 2 );
 	}
 
 	/**
-	 * Ajoutes une entrée dans le tableau $list_tab
+	 * Ajoutes une entrée dans le tableau $list_tab pour la société.
 	 *
 	 * @param  array   $list_tab  La liste des filtres.
 	 * @param  integer $id        L'ID de la société.
@@ -47,6 +48,20 @@ class DUER_Filter {
 			'title' => __( 'DUER', 'digirisk' ),
 		);
 
+		return $list_tab;
+	}
+
+	/**
+	 * Ajoutes une entrée dans le tableau $list_tab pour les groupements
+	 *
+	 * @param  array   $list_tab  La liste des filtres.
+	 * @param  integer $id        L'ID de la société.
+	 * @return array              La liste des filtres + le filtre ajouté par cette méthode.
+	 *
+	 * @since 6.4.4
+	 * @version 6.4.4
+	 */
+	public function callback_digi_tab_group( $list_tab, $id ) {
 		$list_tab['digi-group']['list-duer'] = array(
 			'type'  => 'text',
 			'text'  => __( 'Liste des risques ', 'digirisk' ),
