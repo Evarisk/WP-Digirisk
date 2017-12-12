991faa786f7a0441d97c452f61a8a2e09f53eb7e
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-12-12 11:53:33 +0100

    6.4.4: #25800 => 13746

--- modules/corrective_task/asset/js/corrective-task.backend.js
+++ modules/corrective_task/asset/js/corrective-task.backend.js
@@ -1,24 +1,96 @@
 /**
  * Initialise l'objet "correctiveTask" ainsi que la méthode "init" obligatoire pour la bibliothèque EoxiaJS.
  *
- * @since 1.0
- * @version 6.2.4.0
+ * @since 6.0.0
+ * @version 6.4.4
  */
 
 window.eoxiaJS.digirisk.correctiveTask = {};
 
-window.eoxiaJS.digirisk.correctiveTask.init = function() {};
+window.eoxiaJS.digirisk.correctiveTask.init = function() {
+	window.eoxiaJS.digirisk.correctiveTask.event();
+};
+
+/**
+ * Les évènements
+ *
+ * @since 6.0.0
+ * @version 6.4.4
+ *
+ * @return {void}
+ *
+ * @todo: En attente de la livraison 1.6.0 de Task Manager pour décommenter le code.
+ */
+window.eoxiaJS.digirisk.correctiveTask.event = function() {
+	// jQuery( document ).on( 'change', '.popup.corrective-task .point[data-id="0"] input[name="content"]', window.eoxiaJS.digirisk.correctiveTask.listenAddSafeExit );
+	// jQuery( document ).on( 'addedPointSuccess', '.popup.corrective-task .point[data-id="0"] .action-input', window.eoxiaJS.digirisk.correctiveTask.addedPointSuccess );
+};
+
+/**
+ * Si le contenu du point à ajouter change, on ajoutes le "safeExit".
+ *
+ * @since 6.4.4
+ * @version 6.4.4
+ *
+ * @param  {CustomEvent} event Evenement spécial envoyé par TaskManager lors de la modification d'un point.
+ *
+ * @return {void}
+ */
+window.eoxiaJS.digirisk.correctiveTask.listenAddSafeExit = function( event ) {
+	if ( '' !== jQuery( this ).val() ) {
+		jQuery( '.popup.corrective-task' ).addClass( 'no-close' );
+		jQuery( '.popup.corrective-task .message' ).removeClass( 'hidden' );
+		window.addEventListener( 'beforeunload', window.eoxiaJS.digirisk.correctiveTask.safeExit );
+	} else {
+		jQuery( '.popup.corrective-task' ).removeClass( 'no-close' );
+		jQuery( '.popup.corrective-task .message' ).addClass( 'hidden' );
+		window.removeEventListener( 'beforeunload', window.eoxiaJS.digirisk.correctiveTask.safeExit );
+	}
+};
+
+/**
+ * Evenement envoyé par Task Manager lorsqu'un point est ajouté correctement.
+ *
+ * @since 6.4.4
+ * @version 6.4.4
+ *
+ * @param  {CustomEvent} event Envoyé par Task Manager.
+ *
+ * @return {void}
+ */
+window.eoxiaJS.digirisk.correctiveTask.addedPointSuccess = function( event ) {
+	jQuery( '.popup.corrective-task' ).removeClass( 'no-close' );
+	jQuery( '.popup.corrective-task .message' ).addClass( 'hidden' );
+	window.removeEventListener( 'beforeunload', window.eoxiaJS.digirisk.correctiveTask.safeExit );
+};
+
+/**
+ * Vérification avant la fermeture de la page si toutes les données sont enregistrées.
+ *
+ * @since 6.4.4
+ * @version 6.4.4
+ *
+ * @param  {WindowEventHandlers} event L'évènement de la fenêtre.
+ * @return {string}
+ */
+window.eoxiaJS.digirisk.correctiveTask.safeExit = function( event ) {
+	var confirmationMessage = 'Vos données sont en attentes d\'enregistrement';
+
+	event.returnValue = confirmationMessage;
+	return confirmationMessage;
+};
 
 /**
  * Le callback en cas de réussite à la requête Ajax "open_task".
  * Remplaces le contenu de la popup "corrective-task" par la vue renvoyée par la réponse Ajax.
  *
+ * @since 6.0.0
+ * @version 6.4.4
+ *
  * @param  {HTMLDivElement} triggeredElement  L'élement HTML déclenchant la requête Ajax.
  * @param  {Object}         response          Les données renvoyées par la requête Ajax.
- * @return {void}
  *
- * @since 1.0
- * @version 6.2.5.0
+ * @return {void}
  */
 window.eoxiaJS.digirisk.correctiveTask.openedTaskPopup = function( triggeredElement, response ) {
 	jQuery( '.popup.corrective-task .content' ).html( response.data.view );
--- modules/corrective_task/view/need-task-manager.view.php
+++ modules/corrective_task/view/need-task-manager.view.php
@@ -3,15 +3,16 @@
  * Affiches le message comme quoi il faut installer Task Manager
  *
  * @author Jimmy Latour <jimmy@evarisk.com>
- * @since 0.1
- * @version 6.2.5.0
+ * @since 6.0.0
+ * @version 6.2.5
  * @copyright 2015-2017 Evarisk
- * @package corrective_task
- * @subpackage view
+ * @package DigiRisk
  */
 
 namespace digi;
 
-if ( ! defined( 'ABSPATH' ) ) { exit; }
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 _e( 'Il faut installer l\'extension <a href="https://wordpress.org/plugins/task-manager/" target="_blank">Task Manager</a> pour créer des tâches correctives.', 'digirisk' ); // WPCS: XSS is ok.
