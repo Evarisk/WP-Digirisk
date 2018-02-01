ad4ebb5675b59134494694c50941803b343ee13c
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2018-02-01 09:58:37 +0100

    6.4.5: 14750 - Association des risques aux catégories suite MAJ INRS Possibilité d'affecter une catégorie de risque à un risque "corrompu"

--- modules/risk/shortcode/risk-category.shortcode.php
+++ modules/risk/shortcode/risk-category.shortcode.php
@@ -29,8 +29,6 @@ class Risk_Category_Shortcode {
 	 */
 	public function __construct() {
 		add_shortcode( 'digi-dropdown-categories-risk', array( $this, 'callback_dropdown_categories_risk' ) );
-
-		add_shortcode( 'digi-fix-risk-categories', array( $this, 'callback_fix_risk_categories' ) );
 	}
 
 	/**
@@ -56,6 +54,16 @@ class Risk_Category_Shortcode {
 		$category_risk_id = ! empty( $param ) && ! empty( $param['category_risk_id'] ) ? (int) $param['category_risk_id'] : 0;
 		$display = ! empty( $param ) && ! empty( $param['display'] ) ? $param['display'] : 'edit';
 		$preset = ! empty( $param ) && ! empty( $param['preset'] ) ? (int) $param['preset'] : 0;
+
+		$risk = null;
+		if ( 0 !== $id ) {
+			$risk = Risk_Class::g()->get( array(
+				'id' => $id,
+			), true );
+			if ( 0 === $risk->risk_category->id ) {
+				$display = 'edit';
+			}
+		}
 
 		if ( 'edit' === $display ) {
 			$risks_categories_preset = Risk_Class::g()->get( array(
@@ -101,66 +109,14 @@ class Risk_Category_Shortcode {
 				'risks_categories' => $risks_categories,
 				'preset' => $preset,
 				'selected_risk_category' => $selected_risk_category,
+				'risk' => $risk,
 			) );
 		} else {
-			$risk = Risk_Class::g()->get( array(
-				'id' => $id,
-			), true );
-
 			\eoxia\View_Util::exec( 'digirisk', 'risk', 'dropdown/item', array(
 				'id' => $id,
 				'risk' => $risk,
 			) );
 		}
-	}
-
-	/**
-	 * Shortcode permettant l'affichage de l'interterface de correction d'affectation des catégories de danger aux risques ayant été affectés lors du transfert en version 6.4.à
-	 *
-	 * @param array $param La liste des arguments passés au shortcode.
-	 *
-	 * @since 6.4.5
-	 * @version 6.4.5
-	 *
-	 * @return void
-	 */
-	public function callback_fix_risk_categories( $param ) {
-		global $wpdb;
-
-		// Récupération de la correspondance entre les anciennes catégories de danger et les nouvelles catégories de risques.
-		$json_matching = array();
-		$json_data_file_url_path_et_pif = \eoxia\Config_Util::$init['digirisk']->update_manager->url . 'asset/json/risk-danger-6400.json';
-		$request = wp_remote_get( $json_data_file_url_path_et_pif );
-		if ( ! is_wp_error( $request ) ) {
-			$data = wp_remote_retrieve_body( $request );
-			if ( ! is_wp_error( $data ) ) {
-				$json_matching = json_decode( $data, true );
-			}
-		}
-
-		// Récupération de la liste des catégories de danger qui sont affectées à au moins un risque.
-		$digi_danger_category_list = $wpdb->get_results( $wpdb->prepare(
-			"SELECT T.*
-		 	FROM {$wpdb->term_relationships} AS TR
-				JOIN {$wpdb->term_taxonomy} AS TT ON ( TT.term_taxonomy_id = TR.term_taxonomy_id )
-				JOIN {$wpdb->terms} AS T ON ( T.term_id = TT.term_id )
-			WHERE TT.taxonomy = %s
-			GROUP BY T.term_id", 'digi-danger-category' ) );
-
-		// Récuération de la liste des catégories de risques existantes.
-		$digi_category_risk_list = $wpdb->get_results( $wpdb->prepare(
-			"SELECT T.*
-		 	FROM {$wpdb->term_taxonomy} AS TT
-				JOIN {$wpdb->terms} AS T ON ( T.term_id = TT.term_id )
-			WHERE TT.taxonomy = %s
-			GROUP BY T.term_id", 'digi-category-risk' ) );
-
-		// Affichage de l'interface.
-		\eoxia\View_Util::exec( 'digirisk', 'risk', 'tools/main', array(
-			'digi_danger_category_list' => $digi_danger_category_list,
-			'digi_category_risk_list'   => $digi_category_risk_list,
-			'json_matching'             => $json_matching,
-		) );
 	}
 
 }
