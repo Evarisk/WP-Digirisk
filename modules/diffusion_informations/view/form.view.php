9c704b26b9cc00c94ac115d08a92f1769cd2083f
Amandine Eoxia <amandine@eoxia.com>
2018-09-06 17:52:48 +0200

    maj CSS Diffusions informations

--- modules/diffusion_informations/view/form.view.php
+++ modules/diffusion_informations/view/form.view.php
@@ -21,54 +21,57 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<input type="hidden" name="parent_id" value="<?php echo esc_attr( $element_id ); ?>" />
 
 	<div class="wpeo-gridlayout padding grid-2">
-		<ul class="form">
+		<ul class="wpeo-form">
 			<li><h2><?php esc_html_e( 'Délégués du personnel', 'digirisk' ); ?></h2></li>
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['delegues_du_personnels_date']['raw'] ) ? 'active' : ''; ?>">
-				<div class="group-date form-element <?php echo esc_attr( ! empty( $element->date ) ? 'active' : '' ); ?>">
-					<input type="text" class="mysql-date" style="width: 0px; padding: 0px; border: none;" name="delegues_du_personnels_date" value="<?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_date']['raw'] ); ?>" />
-					<input type="text" class="date" placeholder="04/01/2017" value="<?php echo esc_html( $diffusion_information->data['delegues_du_personnels_date']['rendered']['date'] ); ?>" />
-					<label><?php esc_html_e( 'Date d\'élection', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</div>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Date d\'élection', 'digirisk' ); ?></span>
+				<label class="group-date form-field-container">
+					<input type="hidden" class="mysql-date" name="delegues_du_personnels_date" value="<?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_date']['raw'] ); ?>" />
+					<input type="text" class="date form-field" placeholder="04/01/2017" value="<?php echo esc_html( $diffusion_information->data['delegues_du_personnels_date']['rendered']['date'] ); ?>" />
+				</label>
 			</li>
 
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['delegues_du_personnels_titulaires'] ) ? 'active' : ''; ?>">
-				<textarea name="delegues_du_personnels_titulaires"><?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_titulaires'] ); ?></textarea>
-				<label><?php esc_html_e( 'Titulaires', 'digirisk' ); ?></label>
-				<span class="bar"></span>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Titulaires', 'digirisk' ); ?></span>
+				<label class="form-field-container">
+					<textarea class="form-field" name="delegues_du_personnels_titulaires"><?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_titulaires'] ); ?></textarea>
+				</label>
 			</li>
 
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['delegues_du_personnels_suppleants'] ) ? 'active' : ''; ?>">
-				<textarea name="delegues_du_personnels_suppleants"><?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_suppleants'] ); ?></textarea>
-				<label><?php esc_html_e( 'Suppléants', 'digirisk' ); ?></label>
-				<span class="bar"></span>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Suppléants', 'digirisk' ); ?></span>
+				<label class="form-field-container">
+					<textarea class="form-field" name="delegues_du_personnels_suppleants"><?php echo esc_attr( $diffusion_information->data['delegues_du_personnels_suppleants'] ); ?></textarea>
+				</label>
 			</li>
 		</ul>
 
-		<ul class="form">
+		<ul class="wpeo-form">
 			<li><h2><?php esc_html_e( 'Membres du comité d\'entreprise', 'digirisk' ); ?></h2></li>
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['membres_du_comite_entreprise_date']['raw'] ) ? 'active' : ''; ?>">
-				<div class="group-date form-element <?php echo esc_attr( ! empty( $diffusion_information->data['membres_du_comite_entreprise_date']['raw'] ) ? 'active' : '' ); ?>">
-					<input type="text" class="mysql-date" style="width: 0px; padding: 0px; border: none;" name="membres_du_comite_entreprise_date" value="<?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_date']['raw'] ); ?>" />
-					<input type="text" class="date" placeholder="04/01/2017" value="<?php echo esc_html( $diffusion_information->data['membres_du_comite_entreprise_date']['rendered']['date'] ); ?>" />
-					<label><?php esc_html_e( 'Date d\'élection', 'digirisk' ); ?></label>
-					<span class="bar"></span>
-				</div>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Date d\'élection', 'digirisk' ); ?></span>
+				<label class="group-date form-field-container">
+					<input type="hidden" class="mysql-date" name="membres_du_comite_entreprise_date" value="<?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_date']['raw'] ); ?>" />
+					<input type="text" class="date form-field" placeholder="04/01/2017" value="<?php echo esc_html( $diffusion_information->data['membres_du_comite_entreprise_date']['rendered']['date'] ); ?>" />
+					</label>
+				</label>
 			</li>
 
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['membres_du_comite_entreprise_titulaires'] ) ? 'active' : ''; ?>">
-				<textarea name="membres_du_comite_entreprise_titulaires"><?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_titulaires'] ); ?></textarea>
-				<label><?php esc_html_e( 'Titulaires', 'digirisk' ); ?></label>
-				<span class="bar"></span>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Titulaires', 'digirisk' ); ?></span>
+				<label class="form-field-container">
+					<textarea class="form-field" name="membres_du_comite_entreprise_titulaires"><?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_titulaires'] ); ?></textarea>
+				</label>
 			</li>
 
-			<li class="form-element <?php echo ! empty( $diffusion_information->data['membres_du_comite_entreprise_suppleants'] ) ? 'active' : ''; ?>">
-				<textarea name="membres_du_comite_entreprise_suppleants"><?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_suppleants'] ); ?></textarea>
-				<label><?php esc_html_e( 'Suppléants', 'digirisk' ); ?></label>
-				<span class="bar"></span>
+			<li class="form-element">
+				<span class="form-label"><?php esc_html_e( 'Suppléants', 'digirisk' ); ?></span>
+				<label class="form-field-container">
+					<textarea class="form-field" name="membres_du_comite_entreprise_suppleants"><?php echo esc_attr( $diffusion_information->data['membres_du_comite_entreprise_suppleants'] ); ?></textarea>
+				</label>
 			</li>
 		</ul>
 	</div>
 
-	<button class="button blue action-input float right" data-parent="form-generate"><i class="icon far fa-sync-alt"></i><span><?php esc_html_e( 'Générer les diffusions d\'informations A3 et A4', 'digirisk' ); ?></span></button>
+	<div><button class="wpeo-button button-main button-margin action-input alignright" data-parent="form-generate"><i class="button-icon far fa-sync-alt"></i><span><?php esc_html_e( 'Générer les diffusions d\'informations A3 et A4', 'digirisk' ); ?></span></button></div>
 </form>
