0231a901a5ac1a05109aa5d801144c140f4f71e2
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-09-21 10:46:24 +0200

    Gestion Multisite Mise à jour

--- modules/update_manager/action/class-update-manager-action.php
+++ modules/update_manager/action/class-update-manager-action.php
@@ -33,7 +33,7 @@ class Update_Manager_Action extends \eoxia\Update_Manager_Action {
 			parent::__construct();
 			add_action( 'admin_menu', array( $this, 'callback_admin_menu' ), 12 );
 			add_action( 'wp_loaded', array( $this, 'automatic_update_redirect' ) );
-			add_action( 'wp_ajax_digi_redirect_to_dashboard', array( $this, 'callback_fp_redirect_to_dashboard' ) );
+			add_action( 'wp_ajax_digi_redirect_to_dashboard', array( $this, 'callback_digi_redirect_to_dashboard' ) );
 		}
 	}
 
@@ -42,7 +42,7 @@ class Update_Manager_Action extends \eoxia\Update_Manager_Action {
 	 *
 	 * @since 7.0.0
 	 */
-	public function callback_fp_redirect_to_dashboard() {
+	public function callback_digi_redirect_to_dashboard() {
 		$version = (int) str_replace( '.', '', \eoxia\Config_Util::$init['digirisk']->version );
 		if ( 3 === strlen( $version ) ) {
 			$version *= 10;
@@ -50,11 +50,31 @@ class Update_Manager_Action extends \eoxia\Update_Manager_Action {
 		update_option( \eoxia\Config_Util::$init['digirisk']->key_last_update_version, $version );
 		delete_option( \eoxia\Config_Util::$init['digirisk']->key_waiting_updates );
 
+		$url = admin_url( 'admin.php?page=' . \eoxia\Config_Util::$init['digirisk']->dashboard_page_url );
+
+		if ( is_multisite() ) {
+			$sites = get_sites();
+
+			if ( ! empty( $sites ) ) {
+				foreach ( $sites as $site ) {
+					switch_to_blog( $site->blog_id );
+
+					$last_update_version = get_option( '_digirisk_last_update_version', true );
+					if ( (int) $version > (int) $last_update_version ) {
+						$url = admin_url( 'admin.php?page=digirisk-update' );
+						break;
+					}
+				}
+
+				restore_current_blog();
+			}
+		}
+
 		wp_send_json_success( array(
 			'updateComplete'  => true,
 			// Translators: 1. Start of link to dashboard 2. End of link to dashboard.
 			'doneDescription' => sprintf( __( 'Vous allez être redirigé vers DigiRisk.. %1$sCliquez ici si rien ne se passe.%2$s', 'digirisk' ), '<a href="" >', '</a>' ),
-			'url'             => admin_url( 'admin.php?page=' . \eoxia\Config_Util::$init['digirisk']->dashboard_page_url ),
+			'url'             => $url,
 		) );
 	}
 
