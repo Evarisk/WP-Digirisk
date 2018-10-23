0f9afe88d3c7b1b91a68f3e80c404714b499e866
laygendev <latour.jimmy@gmail.com>
2018-10-23 09:38:12 +0200

    Gestion du conflit avec Task Manager

--- digirisk.php
+++ digirisk.php
@@ -26,6 +26,22 @@ DEFINE( 'PLUGIN_DIGIRISK_DIR', basename( __DIR__ ) );
 
 require_once 'core/external/eo-framework/eo-framework.php';
 
-\eoxia\Init_Util::g()->exec( PLUGIN_DIGIRISK_PATH, basename( __FILE__, '.php' ) );
+$active_plugins   = get_option( 'active_plugins' );
+$key_task_manager = false;
 
-register_activation_hook( __FILE__, array( Digirisk::g(), 'activation' ) );
+if ( ! empty( $active_plugins ) ) {
+	foreach ( $active_plugins as $key => $active_plugin ) {
+		if ( strpos( $active_plugin, 'task-manager.php' ) != 0 ) {
+			$key_task_manager = $key;
+		}
+	}
+}
+
+if ( false !== $key_task_manager ) {
+	array_splice( $active_plugins, $key, 1 );
+	update_option( 'active_plugins', $active_plugins );
+} else {
+	\eoxia\Init_Util::g()->exec( PLUGIN_DIGIRISK_PATH, basename( __FILE__, '.php' ) );
+
+	register_activation_hook( __FILE__, array( Digirisk::g(), 'activation' ) );
+}
