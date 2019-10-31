4a5583f3eb1744c85f87d8c03f8859d4cfce7900
Jimmy <jimmy.eoxia@gmail.com>
2019-10-31 10:16:30 +0100

    Désactive temporairement le nonce qui n'existe plus. Récupères automatiquement la société.

--- modules/diffusion_informations/action/diffusion-informations.action.php
+++ modules/diffusion_informations/action/diffusion-informations.action.php
@@ -63,20 +63,16 @@ class Diffusion_Informations_Action {
 	 * L'action appelant la fonction pour générer la diffusion d'informations.
 	 *
 	 * @since 6.2.10
+	 *
+	 * @todo: nonce
 	 */
 	public function callback_generate_diffusion_information() {
-		check_ajax_referer( 'generate_diffusion_information' );
+		// check_ajax_referer( 'generate_diffusion_information' );
 
-		$parent_id = ! empty( $_POST['parent_id'] ) ? (int) $_POST['parent_id'] : 0; // WPCS: input var ok.
-
-		if ( empty( $parent_id ) ) {
-			wp_send_json_error();
-		}
-
-		$element = Society_Class::g()->show_by_type( $parent_id );
+		$element = Society_Class::g()->get( array( 'posts_per_page' => 1 ), true );
 
 		$data = array(
-			'parent_id'                               => $parent_id,
+			'parent_id'                               => $element->data['id'],
 			'status'                                  => 'inherit',
 			'delegues_du_personnels_date'             => ! empty( $_POST['delegues_du_personnels_date'] ) ? sanitize_text_field( $_POST['delegues_du_personnels_date'] ) : '',
 			'delegues_du_personnels_titulaires'       => ! empty( $_POST['delegues_du_personnels_titulaires'] ) ? sanitize_text_field( $_POST['delegues_du_personnels_titulaires'] ) : '',
