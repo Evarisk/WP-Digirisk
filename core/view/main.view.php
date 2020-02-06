1d628414ae64c3653f31c09d00f81b6ebd601a37
eoxia-amandine <amandine@eoxia.com>
2020-02-06 17:34:57 +0100

    Amélioration du responsive

--- core/view/main.view.php
+++ core/view/main.view.php
@@ -21,83 +21,85 @@ namespace digi;
 defined( 'ABSPATH' ) || exit; ?>
 
 
-<div class="digirisk-wrap wpeo-wrap wpeo-box" style="clear: both;">
-	<?php
-		if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['digirisk']->update_page_url ) ) :
-			\eoxia\Update_Manager_Class::g()->display_say_to_update( 'digirisk', __( 'Need to update DigiRisk data', 'digirisk' ) );
-		endif;
-	?>
-	<h2>GÉREZ VOTRE DOCUMENT UNIQUE, AFFICHAGE LÉGAL, ET BIEN PLUS...</h2>
-
-	<div class="wpeo-gridlayout grid-3 top-content">
-		<div>
-			<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/ressources-humaines.png" /></div>
-			<h3>GESTION DES RISQUES</h3>
-			<p>Les risques sont traités comme suit :<br />
-				Selon la société et/ou groupement<br />
-				Selon unité de travail<br />
-				Selon photo<br />
-			</p>
-		</div>
-
-		<div>
-			<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/document-unique.png" /></div>
-			<h3>DOCUMENT UNIQUE</h3>
-			<p>Impression en un clic de votre Document Unique<br />
-				Personnalisation des fiches de poste<br />
-				Impression des fiches de poste<br />
-				Export natif au format Open Office<br />
-				Personnalisable facilement avec Open Office
-			</p>
-		</div>
-
-		<div>
-			<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/gestion-des-risques.png" /></div>
-			<h3>GESTION AFFICHAGE LÉGAL</h3>
-			<p>Impression de l'affichage légal<br />Impression des fiches de postes
-			</p>
-		</div>
-	</div>
-
-	<?php
-	if ( $ask_auto_connect ) :
+<div class="digirisk-wrap wpeo-wrap" style="clear: both;">
+	<div class="wpeo-box">
+		<?php
+			if ( ! empty( $waiting_updates ) && strpos( $_SERVER['REQUEST_URI'], 'admin.php' ) && ! strpos( $_SERVER['REQUEST_URI'], 'admin.php?page=' . \eoxia\Config_Util::$init['digirisk']->update_page_url ) ) :
+				\eoxia\Update_Manager_Class::g()->display_say_to_update( 'digirisk', __( 'Need to update DigiRisk data', 'digirisk' ) );
+			endif;
 		?>
-		<div class="wpeo-modal modal-active modal-interface">
-			<div class="modal-container">
+		<h2>GÉREZ VOTRE DOCUMENT UNIQUE, AFFICHAGE LÉGAL, ET BIEN PLUS...</h2>
 
-				<!-- Entête -->
-				<div class="modal-header">
-					<h2 class="modal-title">Interface par défaut</h2>
-					<div class="modal-close"><i class="fas fa-times"></i></div>
-				</div>
+		<div class="wpeo-gridlayout grid-3 top-content">
+			<div>
+				<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/ressources-humaines.png" /></div>
+				<h3>GESTION DES RISQUES</h3>
+				<p>Les risques sont traités comme suit :<br />
+					Selon la société et/ou groupement<br />
+					Selon unité de travail<br />
+					Selon photo<br />
+				</p>
+			</div>
 
-				<!-- Corps -->
-				<div class="modal-content">
-					<p>DigiRisk n'est pas votre interface par défaut. Voulez-vous la définir en tant qu'interface par défaut ?</p>
-					<div class="wpeo-form">
-						<div class="form-element">
-							<div class="form-field-inline">
-								<input type="checkbox" id="no-ask-again" class="form-field" checked="checked" name="ask_again" />
-								<label for="no-ask-again">Toujours effectuer cette vérification au démarrage de DigiRisk.</label>
+			<div>
+				<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/document-unique.png" /></div>
+				<h3>DOCUMENT UNIQUE</h3>
+				<p>Impression en un clic de votre Document Unique<br />
+					Personnalisation des fiches de poste<br />
+					Impression des fiches de poste<br />
+					Export natif au format Open Office<br />
+					Personnalisable facilement avec Open Office
+				</p>
+			</div>
+
+			<div>
+				<div><img src="https://www.digirisk.com/wp-content/uploads/2017/11/gestion-des-risques.png" /></div>
+				<h3>GESTION AFFICHAGE LÉGAL</h3>
+				<p>Impression de l'affichage légal<br />Impression des fiches de postes
+				</p>
+			</div>
+		</div>
+
+		<?php
+		if ( $ask_auto_connect ) :
+			?>
+			<div class="wpeo-modal modal-active modal-interface">
+				<div class="modal-container">
+
+					<!-- Entête -->
+					<div class="modal-header">
+						<h2 class="modal-title">Interface par défaut</h2>
+						<div class="modal-close"><i class="fas fa-times"></i></div>
+					</div>
+
+					<!-- Corps -->
+					<div class="modal-content">
+						<p>DigiRisk n'est pas votre interface par défaut. Voulez-vous la définir en tant qu'interface par défaut ?</p>
+						<div class="wpeo-form">
+							<div class="form-element">
+								<div class="form-field-inline">
+									<input type="checkbox" id="no-ask-again" class="form-field" checked="checked" name="ask_again" />
+									<label for="no-ask-again">Toujours effectuer cette vérification au démarrage de DigiRisk.</label>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>
 
-				<!-- Footer -->
-				<div class="modal-footer">
-					<a class="action-input wpeo-button button-main button-uppercase"
-						data-parent="wpeo-modal"
-					   data-set-default="true"
-					   data-action="set_default_app"><span>Faire de DigiRisk mon interface par défaut.</span></a>
-					<a class="action-input wpeo-button button-grey button-uppercase"
-						data-parent="wpeo-modal"
-					   	data-set-default="false"
-						data-action="set_default_app"><span>Plus tard</span></a>
+					<!-- Footer -->
+					<div class="modal-footer">
+						<a class="action-input wpeo-button button-main button-uppercase"
+							data-parent="wpeo-modal"
+						   data-set-default="true"
+						   data-action="set_default_app"><span>Faire de DigiRisk mon interface par défaut.</span></a>
+						<a class="action-input wpeo-button button-grey button-uppercase"
+							data-parent="wpeo-modal"
+						    data-set-default="false"
+							data-action="set_default_app"><span>Plus tard</span></a>
+					</div>
 				</div>
 			</div>
-		</div>
-		<?php
-		endif;
-	?>
+			<?php
+			endif;
+		?>
+	</div>
 </div>
