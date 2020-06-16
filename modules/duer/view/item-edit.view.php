2451e57b07f591f03c604488de9fb6552cfad9df
Amandine Robillard <amandine@eoxia.com>
2020-06-16 14:56:34 +0200

    Changement du tableau DUER

--- modules/duer/view/item-edit.view.php
+++ modules/duer/view/item-edit.view.php
@@ -13,100 +13,64 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }  ?>
 
-<tr class="edit">
+<div class="table-row table-edit duer-row">
 	<input type="hidden" name="element_id" value="<?php echo esc_attr( $element_id ); ?>" />
-
-	<td class="padding"></td>
-	<td class="padding">
+	<div class="table-cell table-75"></div>
+	<div class="table-cell table-75">
 		<div class="group-date form-element">
 			<input type="hidden" class="mysql-date" name="dateDebutAudit" value="<?php echo esc_attr( current_time( 'mysql' ) ); ?>" />
 			<input type="text" class="date" value="<?php echo esc_attr( current_time( 'd/m/Y' ) ); ?>" />
 		</div>
-	</td>
-	<td class="padding">
+	</div>
+	<div class="table-cell table-75">
 		<div class="group-date form-element">
 			<input type="hidden" class="mysql-date" name="dateFinAudit" value="<?php echo esc_attr( current_time( 'mysql' ) ); ?>" />
 			<input type="text" class="date" value="<?php echo esc_attr( current_time( 'd/m/Y' ) ); ?>" />
 		</div>
-	</td>
-
-	<td class="padding">
+	</div>
+	<div class="table-cell table-75">
 		<textarea class="hidden textarea-content-destinataire-duer" name="destinataireDUER"><?php echo esc_html( $element->data['document_meta']['destinataireDUER'] ); ?></textarea>
-
-		<span data-parent="main-container"
-				data-target="duer-modal"
-				data-title="Édition du destinataire"
-				data-src="destinataire-duer"
-				class="wpeo-modal-event wpeo-button-pulse span-content-destinataire-duer">
-
+		<span data-parent="main-container" data-target="duer-modal" data-title="Édition du destinataire" data-src="destinataire-duer" class="wpeo-modal-event wpeo-button-pulse span-content-destinataire-duer">
 			<i class="button-icon fas fa-user"></i>
 			<span class="button-float-icon animated"><i class="fas fa-pencil-alt"></i></span>
 		</span>
-	</td>
-
-	<td class="padding">
+	</div>
+	<div class="table-cell table-100">
 		<textarea class="hidden textarea-content-methodology" name="methodologie"><?php echo esc_html( $element->data['document_meta']['methodologie'] ); ?></textarea>
-		<span data-parent="main-container"
-					data-target="duer-modal"
-					data-title="Édition de la méthodologie"
-					data-src="methodology"
-					class="wpeo-modal-event wpeo-button-pulse span-content-methodology">
-
+		<span data-parent="main-container" data-target="duer-modal" data-title="Édition de la méthodologie" data-src="methodology" class="wpeo-modal-event wpeo-button-pulse span-content-methodology">
 			<i class="button-icon fas fa-search"></i>
 			<span class="button-float-icon animated"><i class="fas fa-pencil-alt"></i></span>
 		</span>
-	</td>
-
-	<td class="padding">
+	</div>
+	<div class="table-cell table-75">
 		<textarea class="hidden textarea-content-sources" name="sources"><?php echo esc_html( $element->data['document_meta']['sources'] ); ?></textarea>
-		<span data-parent="main-container"
-					data-target="duer-modal"
-					data-title="Édition de la source"
-					data-src="sources"
-					class="wpeo-modal-event wpeo-button-pulse span-content-sources">
-
+		<span data-parent="main-container" data-target="duer-modal" data-title="Édition de la source" data-src="sources" class="wpeo-modal-event wpeo-button-pulse span-content-sources">
 			<i class="button-icon fas fa-link"></i>
 			<span class="button-float-icon animated"><i class="fas fa-pencil-alt"></i></span>
 		</span>
-	</td>
-
-	<td class="padding">
+	</div>
+	<div class="table-cell table-75">
 		<textarea class="hidden textarea-content-dispo-des-plans" name="dispoDesPlans"><?php echo esc_html( $element->data['document_meta']['dispoDesPlans'] ); ?></textarea>
-		<span data-parent="main-container"
-					data-target="duer-modal"
-					data-title="Édition de la localisation"
-					data-src="dispo-des-plans"
-					class="wpeo-modal-event wpeo-button-pulse span-content-dispo-des-plans">
-
+		<span data-parent="main-container" data-target="duer-modal" data-title="Édition de la localisation" data-src="dispo-des-plans" class="wpeo-modal-event wpeo-button-pulse span-content-dispo-des-plans">
 			<i class="button-icon fas fa-map-marker-alt"></i>
 			<span class="button-float-icon animated"><i class="fas fa-pencil-alt"></i></span>
 		</span>
-	</td>
-
-	<td class="padding">
+	</div>
+	<div class="table-cell table-75">
 		<textarea class="hidden textarea-content-notes-importantes" name="remarqueImportante"><?php echo esc_html( $element->data['document_meta']['remarqueImportante'] ); ?></textarea>
-		<span data-parent="main-container"
-					data-target="duer-modal"
-					data-title="Édition de la note importante"
-					data-src="notes-importantes"
-					class="wpeo-modal-event wpeo-button-pulse span-content-notes-importantes">
-
+		<span data-parent="main-container" data-target="duer-modal" data-title="Édition de la note importante" data-src="notes-importantes" class="wpeo-modal-event wpeo-button-pulse span-content-notes-importantes">
 			<i class="button-icon fas fa-file"></i>
 			<span class="button-float-icon animated"><i class="fas fa-pencil-alt"></i></span>
 		</span>
-	</td>
-
-	<td>
-		<div class="action w50">
-			<div class="wpeo-modal-event add wpeo-button button-square-50"
-					data-id="<?php echo esc_attr( $element_id ); ?>"
-					data-nonce="<?php echo esc_attr( wp_create_nonce( 'display_societies_duer' ) ); ?>"
-					data-action="display_societies_duer"
-					data-class="generate-duer-modal modal-force-display"
-					data-title="Génération du DUER">
-					<i class="button-icon fas fa-plus"></i>
-				</div>
+	</div>
+	<div class="table-cell cell-action table-100 table-end" data-title="Action">
+		<div class="wpeo-modal-event add wpeo-button button-square-50"
+			 data-id="<?php echo esc_attr( $element_id ); ?>"
+			 data-nonce="<?php echo esc_attr( wp_create_nonce( 'display_societies_duer' ) ); ?>"
+			 data-action="display_societies_duer"
+			 data-class="generate-duer-modal modal-force-display"
+			 data-title="Génération du DUER">
+			<i class="button-icon fas fa-plus"></i>
 		</div>
-	</td>
-
-</tr>
+	</div>
+</div>
--- modules/duer/view/list-item.view.php
+++ modules/duer/view/list-item.view.php
@@ -16,102 +16,66 @@ namespace digi;
 
 defined( 'ABSPATH' ) || exit; ?>
 
-<tr>
-	<td class="padding w50"><strong><?php echo esc_html( $document->data['unique_identifier'] ); ?></strong></td>
-	<td class="padding"><?php echo esc_html( $document->data['document_meta']['dateDebutAudit']['rendered']['date'] ); ?></td>
-	<td class="padding"><?php echo esc_html( $document->data['document_meta']['dateFinAudit']['rendered']['date'] ); ?></td>
-
-	<td class="padding padding text-center">
+<div class="table-row duer-row">
+	<div class="table-cell table-75"><strong><?php echo esc_html( $document->data['unique_identifier'] ); ?></strong></div>
+	<div class="table-cell table-75"><?php echo esc_html( $document->data['document_meta']['dateDebutAudit']['rendered']['date'] ); ?></div>
+	<div class="table-cell table-75"><?php echo esc_html( $document->data['document_meta']['dateFinAudit']['rendered']['date'] ); ?></div>
+	<div class="table-cell table-75">
 		<span class="hidden text-content-destinataire-duer"><?php echo nl2br( $document->data['document_meta']['destinataireDUER'] ); // WPCS: XSS is ok. ?></span>
-		<span data-parent="main-container"
-					data-type="view"
-					data-target="duer-modal"
-					data-title="Destinataire"
-					data-src="destinataire-duer"
-					class="wpeo-modal-event wpeo-button-pulse">
-
+		<span data-parent="main-container" data-type="view" data-target="duer-modal" data-title="Destinataire" data-src="destinataire-duer" class="wpeo-modal-event wpeo-button-pulse">
 			<i class="button-icon fas fa-user"></i>
 			<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
 		</span>
-	</td>
-
-	<td class="padding text-center">
+	</div>
+	<div class="table-cell table-100">
 		<span class="hidden text-content-methodology"><?php echo nl2br( $document->data['document_meta']['methodologie'] ); // WPCS: XSS is ok. ?></span>
-		<span data-parent="main-container"
-					data-type="view"
-					data-target="duer-modal"
-					data-title="Méthodologie"
-					data-src="methodology"
-					class="wpeo-modal-event wpeo-button-pulse">
-
+		<span data-parent="main-container" data-type="view" data-target="duer-modal" data-title="Méthodologie" data-src="methodology" class="wpeo-modal-event wpeo-button-pulse">
 			<i class="button-icon fas fa-search"></i>
 			<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
 		</span>
-	</td>
-
-	<td class="padding text-center">
+	</div>
+	<div class="table-cell table-75">
 		<span class="hidden text-content-sources"><?php echo nl2br( $document->data['document_meta']['sources'] ); // WPCS: XSS is ok. ?></span>
-		<span data-parent="main-container"
-					data-type="view"
-					data-target="duer-modal"
-					data-title="Source"
-					data-src="sources"
-					class="wpeo-modal-event wpeo-button-pulse">
-
+		<span data-parent="main-container" data-type="view" data-target="duer-modal" data-title="Source" data-src="sources" class="wpeo-modal-event wpeo-button-pulse">
 			<i class="button-icon fas fa-link"></i>
 			<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
 		</span>
-		</td>
-
-	<td class="padding text-center">
+	</div>
+	<div class="table-cell table-75">
 		<span class="hidden text-content-dispo-des-plans"><?php echo nl2br( $document->data['document_meta']['dispoDesPlans'] ); // WPCS: XSS is ok. ?></span>
-		<span data-parent="main-container"
-					data-type="view"
-					data-target="duer-modal"
-					data-title="Disponibilité des plans"
-					data-src="dispo-des-plans"
-					class="wpeo-modal-event wpeo-button-pulse">
-
+		<span data-parent="main-container" data-type="view" data-target="duer-modal" data-title="Disponibilité des plans" data-src="dispo-des-plans" class="wpeo-modal-event wpeo-button-pulse">
 			<i class="button-icon fas fa-map-marker-alt"></i>
 			<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
 		</span>
-	</td>
-
-		<td class="padding text-center">
-			<span class="hidden text-content-notes-importantes"><?php echo nl2br( $document->data['document_meta']['remarqueImportante'] ); // WPCS: XSS is ok. ?></span>
-			<span data-parent="main-container"
-						data-type="view"
-						data-target="duer-modal"
-						data-title="Note importante"
-						data-src="notes-importantes"
-						class="wpeo-modal-event wpeo-button-pulse">
-
-				<i class="button-icon fas fa-file"></i>
-				<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
-			</span>
-		</td>
-
-	<td>
+	</div>
+	<div class="table-cell table-75">
+		<span class="hidden text-content-notes-importantes"><?php echo nl2br( $document->data['document_meta']['remarqueImportante'] ); // WPCS: XSS is ok. ?></span>
+		<span data-parent="main-container" data-type="view" data-target="duer-modal" data-title="Note importante" data-src="notes-importantes" class="wpeo-modal-event wpeo-button-pulse">
+			<i class="button-icon fas fa-file"></i>
+			<span class="button-float-icon animated"><i class="fas fa-eye"></i></span>
+		</span>
+	</div>
+	<div class="table-cell cell-action table-100 table-end" data-title="Action">
 		<div class="action wpeo-gridlayout grid-2 grid-gap-0">
 			<?php if ( ! empty( $document->data['file_generated'] ) ) : ?>
 				<a class="wpeo-button button-purple button-square-50 wpeo-tooltip-event"
-					aria-label="<?php echo esc_attr_e( 'DUER', 'digirisk' ); ?>"
-					href="<?php echo esc_attr( $document->data['link'] ); ?>">
+				   aria-label="<?php echo esc_attr_e( 'DUER', 'digirisk' ); ?>"
+				   href="<?php echo esc_attr( $document->data['link'] ); ?>">
 					<i class="icon fas fa-file-alt"></i>
 				</a>
 			<?php else : ?>
 				<span class="action-attribute wpeo-button button-grey button-square-50 wpeo-tooltip-event"
-					data-id="<?php echo esc_attr( $document->data['id'] ); ?>"
-					data-model="<?php echo esc_attr( $document->get_class() ); ?>"
-					data-action="generate_document"
-					data-color="red"
-					data-direction="left"
-					aria-label="<?php echo esc_attr_e( 'Corrompu. Cliquer pour regénérer.', 'digirisk' ); ?>">
+					  data-id="<?php echo esc_attr( $document->data['id'] ); ?>"
+					  data-model="<?php echo esc_attr( $document->get_class() ); ?>"
+					  data-action="generate_document"
+					  data-color="red"
+					  data-direction="left"
+					  aria-label="<?php echo esc_attr_e( 'Corrompu. Cliquer pour regénérer.', 'digirisk' ); ?>">
 					<i class="fas fa-times icon" aria-hidden="true"></i>
 				</span>
 			<?php endif; ?>
 
 			<?php echo apply_filters( 'digi_list_duer_single_item_action_end', '', $document ); ?>
 		</div>
-	</td>
-</tr>
+	</div>
+</div>
