44007647d896747e9605361fe2da83ace608e95b
Amandine Robillard <amandine@eoxia.com>
2020-06-17 11:06:16 +0200

    Changement du tableau des accidents

--- modules/accident/view/list.view.php
+++ modules/accident/view/list.view.php
@@ -15,37 +15,31 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 } ?>
 
-<div class="flex-table accident">
-	<div class="table-header">
-		<div class="col">
-			<div class="header-cell padding w150"><?php esc_html_e( 'Ref.', 'digirisk' ); ?></div>
-			<div class="header-cell padding w200"><?php esc_html_e( 'Nom., Prénom.. victime', 'digirisk' ); ?><span class="red">*</span></div>
-			<div class="header-cell padding w150"><?php esc_html_e( 'Date et heure', 'digirisk' ); ?><span class="red">*</span></div>
-			<div class="header-cell padding w200"><?php esc_html_e( 'Lieu', 'digirisk' ); ?><span class="red">*</span></div>
-			<div class="header-cell padding"><?php esc_html_e( 'Circonstances', 'digirisk' ); ?><span class="red">*</span></div>
-			<div class="header-cell padding w70"><?php esc_html_e( 'Indicateurs', 'digirisk' ); ?></div>
-			<div class="header-cell padding w150"></div>
-		</div>
+<div class="wpeo-table table-flex table-accident">
+	<div class="table-row table-header">
+		<div class="table-cell table-150"><?php esc_html_e( 'Ref.', 'digirisk' ); ?></div>
+		<div class="table-cell table-150"><?php esc_html_e( 'Nom., Prénom.. victime', 'digirisk' ); ?><span class="red">*</span></div>
+		<div class="table-cell table-100"><?php esc_html_e( 'Date et heure', 'digirisk' ); ?><span class="red">*</span></div>
+		<div class="table-cell table-150"><?php esc_html_e( 'Lieu', 'digirisk' ); ?><span class="red">*</span></div>
+		<div class="table-cell"><?php esc_html_e( 'Circonstances', 'digirisk' ); ?><span class="red">*</span></div>
+		<div class="table-cell table-75"><?php esc_html_e( 'Indicateurs', 'digirisk' ); ?></div>
+		<div class="table-cell table-150"></div>
 	</div>
 
-	<div class="table-body">
-		<?php
-		if ( ! empty( $accidents ) ) :
-			foreach ( $accidents as $accident ) :
-				\eoxia\View_Util::exec( 'digirisk', 'accident', 'list-item', array(
-					'accident' => $accident,
-				) );
-			endforeach;
-		endif;
-		?>
-	</div>
+	<?php
+	if ( ! empty( $accidents ) ) :
+		foreach ( $accidents as $accident ) :
+			\eoxia\View_Util::exec( 'digirisk', 'accident', 'list-item', array(
+				'accident' => $accident,
+			) );
+		endforeach;
+	endif;
+	?>
 
-	<div class="table-footer">
-		<?php
-		\eoxia\View_Util::exec( 'digirisk', 'accident', 'item-add', array(
-			'accident'     => $accident_schema,
-			'main_society' => $main_society,
-		) );
-		?>
-	</div>
+	<?php
+	\eoxia\View_Util::exec( 'digirisk', 'accident', 'item-add', array(
+		'accident'     => $accident_schema,
+		'main_society' => $main_society,
+	) );
+	?>
 </div>
