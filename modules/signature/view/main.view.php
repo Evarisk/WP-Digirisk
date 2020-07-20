b82f051751a4a46f8fc715cbcbc5a6e981579fb9
theodavidigirisk <theo.digirisk@gmail.com>
2020-07-20 17:18:19 +0200

    debugging comment accident

--- modules/signature/view/main.view.php
+++ modules/signature/view/main.view.php
@@ -50,7 +50,7 @@ defined( 'ABSPATH' ) || exit;
 			    data-url="<?php echo wp_get_attachment_url( $signature_id ); ?>"
 			    aria-label="Modifier la signature"
 			    data-action="load_modal_signature"
-			    data-title="<?php echo $title_modal; ?>">
+			    data-title="<?php esc_html_e( 'Signature de l\'utilisateur: ' , 'task-manager' ); ?>">
 				<input type="hidden" name="user_signature" value="<?php echo wp_get_attachment_url( $signature_id ); ?>" />
 				<img class="signature" src="<?php echo esc_attr( wp_get_attachment_url( $signature_id ) ); ?>">
 				<span class="button-float-icon animated wpeo-tooltip-event" aria-label="Modifier la signature"><i class="fas fa-pencil-alt"></i></span>
